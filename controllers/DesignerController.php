5a0285a1ab511bc7f830898d4a22c61b9489df42
hj <1534120192@qq.com>
2017-04-24 09:49:01 +0800

    add controllers

---
+++ controllers/DesignerController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace app\controllers;
+
+use app\services\ExceptionHandleService;
+use yii\filters\AccessControl;
+use yii\filters\VerbFilter;
+use yii\web\Controller;
+
+class DesignController extends Controller
+{
+    /**
+     * @inheritdoc
+     */
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'denyCallback' => function ($rule, $action) {
+                    $code = 403;
+                    new ExceptionHandleService($code);
+                    exit;
+                },
+                'only' => ['logout', 'about'],
+                'rules' => [
+                    [
+                        'actions' => ['logout', 'about'],
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'logout' => ['post',],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function actions()
+    {
+        return [
+            'error' => [
+                'class' => 'yii\web\ErrorAction',
+            ],
+            'captcha' => [
+                'class' => 'yii\captcha\CaptchaAction',
+                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
+            ],
+        ];
+    }
+}
