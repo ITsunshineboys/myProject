e33f8767be0886df9da2e393ac166b4f71ddb207
hj <1534120192@qq.com>
2017-04-25 12:01:42 +0800

    modify table user

--- scripts/2017.4.25.sql
+++ scripts/2017.4.25.sql
@@ -1,16 +1,16 @@
 CREATE TABLE `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(25) DEFAULT '',
-  `password` varchar(100),
+  `password` varchar(100) DEFAULT '',
   `authKey` varchar(25) DEFAULT '',
   `accessToken` varchar(255) DEFAULT '',
-  `mobile` bigint,
+  `mobile` bigint DEFAULT 0,
   `nickname` varchar(20) DEFAULT '',
   `identity_no` varchar(18) DEFAULT '',
-  `aite_cube_no` bigint,
+  `aite_cube_no` bigint DEFAULT 0,
   `icon` varchar(100) DEFAULT '',
-  `create_time` int,
-  `login_time` int,
-  `login_role_id` int,
+  `create_time` int DEFAULT 0,
+  `login_time` int DEFAULT 0,
+  `login_role_id` int DEFAULT 0,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
