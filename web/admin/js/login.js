ef94e058779737fc6ed1a827176eb9aa6aa6da7e
xiexueying <837241830@qq.com>
2017-04-26 10:28:50 +0800

    登录接口调试

--- web/admin/js/login.js
+++ web/admin/js/login.js
@@ -1,4 +1,5 @@
 var url="http://test.cdlhzz.cn:888/";
+//var url="http://localhost:888/";
 var role="site/all-roles";
 var logout="site/logout"
 app.controller("admin_login",function($scope,$http){
@@ -26,15 +27,30 @@ app.controller("admin_login",function($scope,$http){
             $(".warm").text("请输入6-25位的密码！");
         }
     };
-    $scope.login=function(){
-        $scope.name=$("input[name='name1']").val();
-        $scope.psw=$("input[name='psw']").val();
-        //$http.get(url+logout)
-        //    .success(function(data){
-        //        $scope.loginout=data;
-        //    })
-        alert("你点击了登录按钮")
 
-    }
+    $scope.$on('ngRepeatFinished', function (data) { //接收广播，一旦repeat结束就会执行
+        $scope.login=function(){
+            $scope.name=$("input[name='name1']").val();
+            $scope.psw=$("input[name='psw']").val();
+            $scope.myrole=$('#select1').val();
+            console.log("$scope.myrole=="+$scope.myrole)
+            $http.post(url+logout+"?role_id="+$scope.myrole+"&username="+$scope.name+"&password="+$scope.psw)
+                .success(function(data){
+                    $scope.loginout=data;
+                    if($scope.loginout.code==200){
+                        alert("登录成功！")
+                    }
 
+                });
+            //alert("你点击了登录按钮")
+
+        }
+    });
 });
+app.controller('itemReaptCtrl', ['$scope', function ($scope) {
+    $scope.$watch($scope.$last, function () {
+        if($scope.$last){   //$scope.$last是来判断是否是最后一个ng-repeat对象， 如果是则$scope.$last的值为true ,反之则为false
+            setTimeout(function(){$scope.$emit('ngRepeatFinished')},1); // 由于是向父控制器中发布广播，所有用$emit
+        }
+    })
+}]);
