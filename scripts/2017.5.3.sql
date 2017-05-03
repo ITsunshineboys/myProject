62715ef24c4c7985feda2a4da3978622a00fb722
hj <1534120192@qq.com>
2017-05-03 16:42:34 +0800

    add carousel api

---
+++ scripts/2017.5.3.sql
@@ -0,0 +1,6 @@
+create table carousel (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `image` varchar(100) NOT NULL DEFAULT '',
+  `link` varchar(100) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
