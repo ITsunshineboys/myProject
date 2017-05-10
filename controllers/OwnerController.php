3b0e6495d4f32a71267c717c54547b153a0af3f5
weichenghui <362224550@qq.com>
2017-05-10 18:02:00 +0800

    智能报价！无资料页面

--- controllers/OwnerController.php
+++ controllers/OwnerController.php
@@ -62,10 +62,14 @@ class OwnerController extends Controller
         ];
     }
 
-    public function actionSearch()
+    /**
+     * 有资料
+     * @return string
+     */
+    public function actionHave_information()
     {
         // 搜索框
-        $post = '末日路';
+        $post = \Yii::$app->request->post();
         $search_condition = new Effect();
         $lists = $search_condition->districtSearch($post);
         foreach ($lists as $list){
@@ -92,10 +96,67 @@ class OwnerController extends Controller
                 'list_picture' => $list_picture,
                 'series_list' => $series_list,
                 'style_list' => $style_list,
-                'style_picture_list' => $style_picture_list
-
+                'style_picture_list' => $style_picture_list,
             ]
         ]);
     }
 
+    /**
+     * 无资料
+     * @return string
+     */
+    public function actionNull_information()
+    {
+        $post = \Yii::$app->request->post();
+        $null_information = new Effect();
+        if($null_information->load($post) && $null_information->validate())
+        {
+            if(!$null_information->save()){
+                $errors = $null_information->errors;
+                return Json::encode([
+                            'code'=> 500,
+                            'msg' => '填写有误',
+                            'data' =>[
+                                'errors' => $errors
+                            ]
+                ]);
+            }
+        }else{
+            $errors = $null_information->errors;
+            return Json::encode([
+                    'code' => 500,
+                    'msg' => '填写有误',
+                    'data' => [
+                        'errors'=> $errors
+                        ]
+            ]);
+        }
+        $list = $null_information->find()->where(['and','toponymy' => $post['toponymy'],'street' => $post['street']])->one();
+        // 系列列表
+        $series = new Series();
+        $series_list = $series->find()->all();
+
+        // 风格列表
+        $style = new Style();
+        $style_list = $style ->find()->all();
+        foreach ($style_list as $s){
+            $style_picture = new StylePicture();
+            $style_picture_list = $style_picture->find()->where(['style_id'=>$s['id']])->all();
+        }
+
+        return Json::encode([
+           'code' => 200,
+            'msg' => '成功',
+            'data' => [
+                'series_list' => $series_list,
+                'style_picture_list' => $style_picture_list,
+                'list' => $list
+            ]
+        ]);
+    }
+
+    public function actionBasis_list()
+    {
+
+    }
 }
--- models/Effect.php
+++ models/Effect.php
@@ -11,6 +11,18 @@ use yii\db\ActiveRecord;
 
 class Effect extends ActiveRecord
 {
+    public $province;
+    public $city;
+    public $district;
+    public $street;
+    public $toponymy;
+    public $area;
+    public $high;
+    public $room;
+    public $hall;
+    public $toilet;
+    public $kitchen;
+    public $window;
     /**
      * @return string 返回该AR类关联的数据表名
      */
@@ -19,6 +31,14 @@ class Effect extends ActiveRecord
         return 'effect';
     }
 
+    public function rules()
+    {
+        return [
+            [['province', 'city','district','street','toponymy','area','high','room','hall','toilet','kitchen','window'], 'required'],
+            [['province', 'city','district','street','toponymy'],'string'],
+            [['area','high','room','hall','toilet','kitchen','window'],'number']
+        ];
+    }
     /**
      * @param $toponymy
      * @param $street
@@ -29,7 +49,11 @@ class Effect extends ActiveRecord
         if (!empty($search))
         {
             $detail = $this->find()->where( ['or',['like','toponymy',$search],['like','street',$search]])->all() ;
+        }else{
+            echo '传入的值有错';
+            exit;
         }
+
         return $detail;
     }
 }
