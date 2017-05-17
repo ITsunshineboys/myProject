84592935c397128de541869b71b736dbcc429f46
hj <1534120192@qq.com>
2017-05-17 11:26:17 +0800

    add mall/recommend-click-record api

--- models/GoodsRecommendViewLog.php
+++ models/GoodsRecommendViewLog.php
@@ -19,4 +19,50 @@ class GoodsRecommendViewLog extends ActiveRecord
     {
         return 'goods_recommend_view_log';
     }
+
+    /**
+     * @return array the validation rules.
+     */
+    public function rules()
+    {
+        return [
+            [['recommend_id'], 'required'],
+            ['recommend_id', 'number', 'integerOnly' => true, 'min' => 1],
+            ['recommend_id', 'validateRecommendId', 'skipOnEmpty' => false],
+        ];
+    }
+
+    /**
+     * Validates recommend id
+     *
+     * @param string $attribute recommend id to validate
+     * @return bool
+     */
+    public function validateRecommendId($attribute)
+    {
+        if (!GoodsRecommend::findOne($this->$attribute)) {
+            $this->addError($attribute);
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Do some ops before insertion
+     *
+     * @param bool $insert if is a new record
+     * @return bool
+     */
+    public function beforeSave($insert)
+    {
+        if (parent::beforeSave($insert)) {
+            if ($insert) {
+                $this->create_time = time();
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
