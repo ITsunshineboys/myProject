f0b1bc63cb792aee6896cf22c8065d46ae8d6735
hj <1534120192@qq.com>
2017-06-02 09:50:51 +0800

    modify sortFields method of ModelService class

--- services/ModelService.php
+++ services/ModelService.php
@@ -14,8 +14,8 @@ class ModelService
 {
     const SEPARATOR_SORT = ':';
     const SORT_DIRECTIONS = [
-        'DESC',
-        'ASC',
+        SORT_DESC => 'DESC',
+        SORT_ASC => 'ASC',
     ];
 
     /**
@@ -25,22 +25,24 @@ class ModelService
      * @param  array        $sort  sorting fields with direction
      * @return bool|string
      */
-    public static function sortFields(ActiveRecord $model, array $sort = ['id:' . self::SORT_DIRECTIONS[0]])
+    public static function sortFields(ActiveRecord $model, array $sort = ['id:' . self::SORT_DIRECTIONS[SORT_DESC]])
     {
         $attributes = $model->getAttributes();
+        $orderBy = [];
+
         foreach ($sort as $v) {
             list($field, $direction) = explode(self::SEPARATOR_SORT, $v);
-            !$direction && $direction = self::SORT_DIRECTIONS[0];
+            !$direction && $direction = SORT_DESC;
             if (!$field) {
                 return false;
             }
 
-            $direction = strtoupper($direction);
+            if (!isset(self::SORT_DIRECTIONS[$direction])) {
+                return false;
+            }
 
-            if (in_array($field, array_keys($attributes))
-                && in_array($direction, self::SORT_DIRECTIONS)
-            ) {
-                $orderBy[$field] = $direction;
+            if (in_array($field, array_keys($attributes))) {
+                $orderBy[$field] = self::SORT_DIRECTIONS[$direction];
             } else {
                 return false;
             }
