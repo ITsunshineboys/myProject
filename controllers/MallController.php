f76d7201426797a026d8ba690fcec4e4fffbbd11
hj <1534120192@qq.com>
2017-06-19 15:30:28 +0800

    add style and series filters for category goods api

--- controllers/MallController.php
+++ controllers/MallController.php
@@ -329,11 +329,15 @@ class MallController extends Controller
         $platformPriceMin = (int)Yii::$app->request->get('platform_price_min', 0);
         $platformPriceMax = (int)Yii::$app->request->get('platform_price_max', 0);
         $brandId = (int)Yii::$app->request->get('brand_id', 0);
+        $styleId = (int)Yii::$app->request->get('style_id', 0);
+        $seriesId = (int)Yii::$app->request->get('series_id', 0);
 
         $where =  "category_id = {$categoryId}";
         $platformPriceMin && $where .= " and platform_price >= {$platformPriceMin}";
         $platformPriceMax && $where .= " and platform_price <= {$platformPriceMax}";
         $brandId && $where .= " and brand_id = {$brandId}";
+        $styleId && $where .= " and style_id = {$styleId}";
+        $seriesId && $where .= " and series_id = {$seriesId}";
 
         $page = (int)Yii::$app->request->get('page', 1);
         $size = (int)Yii::$app->request->get('size', Goods::PAGE_SIZE_DEFAULT);
