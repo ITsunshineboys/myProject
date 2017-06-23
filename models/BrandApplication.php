bc34df352be45c7df44af906826e0410ee4820c3
hj <1534120192@qq.com>
2017-06-23 17:43:55 +0800

    modify brand application model

--- models/BrandApplication.php
+++ models/BrandApplication.php
@@ -19,4 +19,58 @@ class BrandApplication extends ActiveRecord
     {
         return 'brand_application';
     }
+
+    /**
+     * @return array the validation rules.
+     */
+    public function rules()
+    {
+        return [
+            [['category_id', 'brand_id', 'authorization_start', 'authorization_end'], 'required'],
+            [['brand_id'], 'validateBrandId'],
+            [['category_id'], 'validateCategoryId'],
+        ];
+    }
+
+    /**
+     * Validates brand_id
+     *
+     * @param string $attribute brand_id to validate
+     * @return bool
+     */
+    public function validateBrandId($attribute)
+    {
+        if ($this->$attribute > 0
+            && GoodsBrand::find()->where(['id' => $this->$attribute, 'status' => GoodsBrand::STATUS_ONLINE])->exists()
+        ) {
+            return true;
+        }
+
+        $this->addError($attribute);
+        return false;
+    }
+
+    /**
+     * Validates category_id
+     *
+     * @param string $attribute category_id to validate
+     * @return bool
+     */
+    public function validateCategoryId($attribute)
+    {
+        $where = [
+            'id' => $this->$attribute,
+            'deleted' => GoodsCategory::STATUS_OFFLINE,
+            'level' => GoodsCategory::LEVEL3
+        ];
+
+        if ($this->$attribute > 0
+            && GoodsCategory::find()->where($where)->exists()
+        ) {
+            return true;
+        }
+
+        $this->addError($attribute);
+        return false;
+    }
 }
