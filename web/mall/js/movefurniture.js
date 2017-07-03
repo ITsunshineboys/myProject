b01ef4afa029b4cf2f48cef538f899fc2361b1fb
xiexueying <837241830@qq.com>
2017-07-03 18:26:08 +0800

    111

--- web/mall/js/movefurniture.js
+++ web/mall/js/movefurniture.js
@@ -1,33 +1,33 @@
 /**
  * Created by xl on 2017/6/20 0020.
  */
-//var myapp=angular.module('myapp',[]);
-//myapp.controller("commentCtrl",function ($scope,$http) {
-//    $http({
-//        method: 'post',
-//        url:url+"http://local.test.cdlhzz.cn/mall/categories"
-//    }).then(function successCallback(data) {
-//        $scope.message = data.data.data.categories;
-//        //alert(message);
-//    }, function errorCallback(data) {
-//
-//        //alert(data);
-//
-//    });
-//});
-$(function () {
-    $.ajax({
-        method:"post",
-        url:"http://local.test.cdlhzz.cn/mall/categories",
-        dataType:"json",
-        data:{},
-        success:function (data){
-            alert("11111");
-            //console.log(res.data.data.categories)
-        },
-        error:function(data){
-            //console.log(res)
-            alert(2222);
-        }
-    })
+var myapp=angular.module('myapp',[]);
+myapp.controller("commentCtrl",function ($scope,$http) {
+    $http({
+        method: 'post',
+        url:url+"categories"
+    }).then(function successCallback(data) {
+        $scope.message = data.data.data.categories;
+        //alert(message);
+    }, function errorCallback(data) {
+
+        //alert(data);
+
+    });
 });
+//$(function () {
+//    $.ajax({
+//        method:"post",
+//        url:"http://local.test.cdlhzz.cn/mall/categories",
+//        dataType:"json",
+//        data:{},
+//        success:function (data){
+//            alert("11111");
+//            //console.log(res.data.data.categories)
+//        },
+//        error:function(data){
+//            //console.log(res)
+//            alert(2222);
+//        }
+//    })
+//});
