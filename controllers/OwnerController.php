90daac8466bfd25744c8902279c2c776c098ffec
Wch <362224550@qq.com>
2017-07-06 09:54:43 +0800

    强电

--- controllers/OwnerController.php
+++ controllers/OwnerController.php
@@ -282,7 +282,7 @@ class OwnerController extends Controller
 //        $receive = \Yii::$app->request->post();
 //        $post = Json::decode($receive);
         $post = [
-//            'effect_id' => 1,
+            'effect_id' => 1,
             'master_bedroom' => 1,
             'secondary_bedroom' => 1,
             'sitting_room' => 1,
