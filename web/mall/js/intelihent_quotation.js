78a7608a12410b4010daac8688681077e72135f5
xiexueying <837241830@qq.com>
2017-07-07 16:45:44 +0800

    2222222222222222222

--- web/mall/js/intelihent_quotation.js
+++ web/mall/js/intelihent_quotation.js
@@ -3,15 +3,28 @@
  */
 angular.module('app',[])
     .controller("Intelligent_quotation",function ($scope,$http) {
+        $scope.nowSeries ='齐家';
+        $scope.nowStyle = '现代简约';
         $http({
-            method:"post",
-            url:url+"/mall/owner/series-and-style"
+            method:"get",
+            url:url+"owner/series-and-style"
         }).then(function successCallback (resp) {
-            $scope.message = resp.data.show;
-            console.log($scope.message);
+            $scope.message = resp.data.data.show.stairs_details;
+            $scope.style = resp.data.data.show.series;
+            $scope.me=resp.data.data.show.style;
+            console.log($scope.me);
         },function errorCallback () {
 
-        })
+        });
+        //切换系列
+        $scope.toggleSeries = function (item) {
+            $scope.nowSeries = item;
+        };
+
+        //切换风格
+        $scope.toggleStyle = function (item) {
+            $scope.nowStyle = item;
+        }
     });
 
 
