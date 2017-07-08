c81f4d094b29b2794bbd045434d30673aed94c5e
xiexueying <837241830@qq.com>
2017-07-08 18:22:45 +0800

    new   2017-7-8

--- web/owner/js/controller.js
+++ web/owner/js/controller.js
@@ -1,10 +1,16 @@
 angular.module("all_controller", [])
     .controller("cell_search_ctrl", function ($scope, $http) {//小区搜索控制器
-        $scope.data = ''
+        $scope.data = '';
         $scope.getData = function () {
-            let arr = []
-            $http.get("/owner/search").then(function (response) {
-                console.log(response)
+            let arr = [];
+            $http({
+                method:"post",
+                url:"/owner/search",
+                data:{
+                    str:$scope.data
+                }
+            }).then(function (response) {
+                console.log(response);
                 for (let item of response.data.data.effect) {
                     if (item.toponymy.indexOf($scope.data) != -1 && $scope.data != '') {
                         arr.push({"toponymy": item.toponymy, "site_particulars": item.site_particulars})
@@ -19,24 +25,23 @@ angular.module("all_controller", [])
     .controller("intelligent_index_ctrl", function ($scope,$http) {//主页控制器
         // $scope.get_quotation = function () {
             $http({
-                url:"/owner/search",
-                method:"get",
-                data:{'id':'111'}
+                url:"/owner/search?id=1",
+                method:"get"
             }).then(function (response) {
                 console.log(response)
             },function (response) {
 
-            })
+            });
         // }
     })
     .controller("intelligent_nodata_ctrl", function ($scope,$stateParams, $http) { //无数据控制器
-        $scope.message = ''
-        $scope.nowStyle = '现代简约'
-        $scope.nowStairs = '实木结构'
-        $scope.nowSeries = '齐家'
-        $scope.toponymy = $stateParams.toponymy
+        $scope.message = '';
+        $scope.nowStyle = '现代简约';
+        $scope.nowStairs = '实木结构';
+        $scope.nowSeries = '齐家';
+        $scope.toponymy = $stateParams.toponymy;
         $scope.choose_stairs = true;
-        console.log($stateParams)
+        console.log($stateParams);
 
         $scope.$watch('message', function (newVal, oldVal) {
             if (newVal && newVal != oldVal) {
--- web/owner/js/global.js
+++ web/owner/js/global.js
@@ -1,5 +1,6 @@
-var url="http://test.cdlhzz.cn:888/";
-// var url="http://local.test.cdlhzz.cn/";
+//var url="http://test.cdlhzz.cn:888/";
+var url="http://local.test.cdlhzz.cn/";
+ //var url="http://local.test.cdlhzz.cn/";
 var app=angular.module("app",[]);
 //获取url中的？号后面的内容
 function GetQueryString(name) {
