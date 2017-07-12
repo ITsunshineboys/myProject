6e282ad4eef25babb43df083a5883e5dca602d22
hj <1534120192@qq.com>
2017-07-12 15:33:40 +0800

    modify goods model

--- models/Goods.php
+++ models/Goods.php
@@ -12,7 +12,6 @@ use app\services\StringService;
 use Yii;
 use yii\db\ActiveRecord;
 use yii\helpers\HtmlPurifier;
-use yii\helpers\Url;
 
 class Goods extends ActiveRecord
 {
@@ -361,23 +360,11 @@ class Goods extends ActiveRecord
         if (empty($level) && empty($title)) {
             echo '请正确输入值';
             exit;
-        } else
-        {
-            $str = implode(',',$title);
+        } else {
+            $str = implode('\',\'', $title);
             $db = Yii::$app->db;
-
-            $sql = "SELECT goods.id,goods.platform_price,goods.supplier_price,goods.purchase_price_decoration_company,goods_attr.name,goods_attr.value,goods_brand.name,goods_category.title,logistics_district.district_name FROM goods LEFT JOIN goods_attr ON goods_attr.goods_id = goods.id LEFT JOIN goods_brand ON goods.brand_id = goods_brand.id LEFT JOIN goods_category ON goods.category_id = goods_category.id LEFT JOIN logistics_template ON goods.supplier_id = logistics_template.supplier_id LEFT JOIN logistics_district ON logistics_template.id = logistics_district.template_id WHERE logistics_district.district_code = " . $city . " AND goods_category.`level` = " . $level . " AND goods_category.title in ('". $str ."')";
+            $sql = "SELECT goods.id,goods.platform_price,goods.supplier_price,goods.purchase_price_decoration_company,goods_attr.name,goods_attr.value,goods_brand.name,goods_category.title,logistics_district.district_name FROM goods LEFT JOIN goods_attr ON goods_attr.goods_id = goods.id LEFT JOIN goods_brand ON goods.brand_id = goods_brand.id LEFT JOIN goods_category ON goods.category_id = goods_category.id LEFT JOIN logistics_template ON goods.supplier_id = logistics_template.supplier_id LEFT JOIN logistics_district ON logistics_template.id = logistics_district.template_id WHERE logistics_district.district_code = " . $city . " AND goods_category.`level` = " . $level . " AND goods_category.title in ('" . $str . "')";
             $all = $db->createCommand($sql)->queryAll();
-
-            $sql = "SELECT goods.id,goods.platform_price,goods.supplier_price,goods_attr.name,goods_attr.value,goods_brand.name,goods_category.title,logistics_district.district_name FROM goods LEFT JOIN goods_attr ON goods_attr.goods_id = goods.id LEFT JOIN goods_brand ON goods.brand_id = goods_brand.id LEFT JOIN goods_category ON goods.category_id = goods_category.id LEFT JOIN logistics_template ON goods.supplier_id = logistics_template.supplier_id LEFT JOIN logistics_district ON logistics_template.id = logistics_district.template_id WHERE logistics_district.district_code = " . $city . " AND goods_category.`level` = " . $level . " AND goods_category.title LIKE '" . $title . "'";
-            $a = $db->createCommand($sql)->queryAll();
-        }
-        if (!empty($a)) {
-            foreach ($a as $v => $k) {
-                $c [] = ($k['platform_price'] - $k['supplier_price']) / $k['supplier_price'];
-                $max = array_search(max($c), $c);
-            }
-            return $a[$max];
         }
         return $all;
     }
@@ -446,21 +433,10 @@ AND goods.id IN (" . $id . ")";
             $db = Yii::$app->db;
             $sql = "SELECT goods.id,goods.platform_price,goods.supplier_price,goods_attr. name,goods_attr.value,goods_brand. name,goods_category.title,logistics_district.district_name,goods.series_id,goods.style_id FROM goods LEFT JOIN goods_attr ON goods_attr.goods_id = goods.id LEFT JOIN goods_brand ON goods.brand_id = goods_brand.id LEFT JOIN goods_category ON goods.category_id = goods_category.id LEFT JOIN logistics_template ON goods.supplier_id = logistics_template.supplier_id LEFT JOIN logistics_district ON logistics_template.id = logistics_district.template_id WHERE logistics_district.district_code = " . $city . " AND goods_category.title in ('" . $id . "')";
             $all_goods = $db->createCommand($sql)->queryAll();
-
-            $all  = [];
-            foreach ($all_goods as $k)
-            {
-                foreach ($material as $one_material)
-                {
-                    if ($k['title'] == $one_material)
-                    {
-
-//            var_dump($all_goods);exit;
             $all = [];
             foreach ($all_goods as $k) {
                 foreach ($material as $one_material) {
                     if ($k['title'] == $one_material) {
-
                         $c [] = ($k['platform_price'] - $k['supplier_price']) / $k['supplier_price'];
                         $max = array_search(max($c), $c);
                         $all [] = $all_goods[$max];
@@ -468,11 +444,6 @@ AND goods.id IN (" . $id . ")";
                     }
                 }
             }
-
-
-            var_dump($k);
-            exit;
-
             return $all;
         }
     }
