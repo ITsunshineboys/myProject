68dd2d240aba72257c83950f2432d7b4ea3dd035
Wch <362224550@qq.com>
2017-07-15 16:42:43 +0800

    123123

--- controllers/OwnerController.php
+++ controllers/OwnerController.php
@@ -245,7 +245,7 @@ class OwnerController extends Controller
         //人工总费用
         $labor_all_cost = BasisDecorationService::laborFormula($weak_points,$workers);
         $labor_all_cost['worker_kind'] = $workers['worker_kind'];
-        
+
         //材料总费用
         $material_price = BasisDecorationService::quantity($weak_points,$weak_current,$craft);
         $material = [];
@@ -330,20 +330,20 @@ class OwnerController extends Controller
      */
     public function actionStrongCurrent()
     {
-//        $post = \Yii::$app->request->post();
-        $post = [
-            'area'=>60,
-            'bedroom'=>3,
-            'hall'=>1,
-            'toilet'=>1,
-            'kitchen'=>1,
-            'stairs_details_id'=>1,
-            'series'=>60,
-            'style'=>60,
-            'window'=>60,
-            'province'=>510000,
-            'city'=>510100,
-        ];
+        $post = \Yii::$app->request->post();
+//        $post = [
+//            'area'=>60,
+//            'bedroom'=>3,
+//            'hall'=>1,
+//            'toilet'=>1,
+//            'kitchen'=>1,
+//            'stairs_details_id'=>1,
+//            'series'=>60,
+//            'style'=>60,
+//            'window'=>60,
+//            'province'=>510000,
+//            'city'=>510100,
+//        ];
         $workers = LaborCost::profession($post,'强电');
 
         //点位查询
