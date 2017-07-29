7f7f77cd82679aa827abbaa5ef8a2313ca7599ac
hj <1534120192@qq.com>
2017-07-29 11:41:02 +0800

    modify goods model

--- models/Goods.php
+++ models/Goods.php
@@ -676,7 +676,7 @@ AND goods.id IN (" . $id . ")";
         return [
             [['title', 'subtitle', 'category_id', 'brand_id', 'cover_image', 'supplier_price', 'platform_price', 'market_price', 'purchase_price_decoration_company', 'purchase_price_manager', 'purchase_price_designer', 'left_number', 'logistics_template_id', 'after_sale_services'], 'required', 'on' => self::SCENARIO_REVIEW],
             [['title', 'subtitle', 'category_id', 'brand_id', 'cover_image', 'supplier_price', 'platform_price', 'market_price', 'left_number', 'logistics_template_id', 'after_sale_services'], 'required', 'on' => self::SCENARIO_ADD],
-            [['title'], 'string', 'length' => [1, 38]],
+            [['title'], 'string', 'length' => [1, 32]],
             [['subtitle'], 'string', 'length' => [1, 16]],
             [['cover_image', 'offline_reason', 'reason'], 'string'],
             [['category_id', 'brand_id', 'supplier_price', 'platform_price', 'market_price', 'purchase_price_decoration_company', 'purchase_price_manager', 'purchase_price_designer', 'left_number', 'logistics_template_id', 'style_id', 'series_id'], 'number', 'min' => 0],
