a89bcecfc9a492ccda96ba47aea5ed70d1bea8ae
hj <1534120192@qq.com>
2017-07-29 13:51:47 +0800

    modify index.php

--- web/index.php
+++ web/index.php
@@ -8,5 +8,10 @@ require(__DIR__ . '/../vendor/autoload.php');
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
 $config = require(__DIR__ . '/../config/web.php');
-YII_DEBUG && header('Access-Control-Allow-Origin:*');
+
+if (YII_DEBUG) {
+    header('Access-Control-Allow-Origin: *');
+    header("Access-Control-Allow-Credentials: true");
+}
+
 (new yii\web\Application($config))->run();
