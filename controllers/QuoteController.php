1c1515297154017cf43b4cd7849c990440d95725
Wch <362224550@qq.com>
2017-08-03 17:12:17 +0800

    list

--- controllers/QuoteController.php
+++ controllers/QuoteController.php
@@ -361,7 +361,6 @@ class QuoteController extends Controller
     public function actionPlotList()
     {
         $post = \Yii::$app->request->post();
-        var_dump($post['post']);exit;
 //        $post = 510100;
         $effect = Effect::find()->where(['city'=>$post['post']]);
         $pages = new Pagination(['totalCount'=>$effect->count(),'pageSize'=>10]);
