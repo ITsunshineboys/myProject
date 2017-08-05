252fa5be1f50db9a78d1d82dea60db2746a5e267
hyz123 <1248705443@qq.com>
2017-08-05 11:43:25 +0800

    7

--- models/Wxpay.php
+++ models/Wxpay.php
@@ -75,20 +75,21 @@ class Wxpay  extends ActiveRecord
             //、获取用户openid
             $tools = new PayService();
             $openId = $tools->GetOpenid();
+            echo $openId;
             //②、统一下单
-            $input = new WxPayUnifiedOrder();
-            $input->SetBody("test");
-            $input->SetAttach("1");
-            $input->SetOut_trade_no(WxPayConfig::MCHID.date("YmdHis"));
-            $input->SetTotal_fee("1");
-            $input->SetTime_start(date("YmdHis"));
-            $input->SetTime_expire(date("YmdHis", time() + 600));
-            $input->SetGoods_tag("test");
-            $input->SetNotify_url('http://www.cdlhzz.cn/');
-            $input->SetTrade_type("JSAPI");
-            $input->SetOpenid($openId);
-            $order = WxPayApi::unifiedOrder($input);
-            $jsApiParameters = $tools->GetJsApiParameters($order);
-            $editAddress = $tools->GetEditAddressParameters();
+            // $input = new WxPayUnifiedOrder();
+            // $input->SetBody("test");
+            // $input->SetAttach("1");
+            // $input->SetOut_trade_no(WxPayConfig::MCHID.date("YmdHis"));
+            // $input->SetTotal_fee("1");
+            // $input->SetTime_start(date("YmdHis"));
+            // $input->SetTime_expire(date("YmdHis", time() + 600));
+            // $input->SetGoods_tag("test");
+            // $input->SetNotify_url('http://www.cdlhzz.cn/');
+            // $input->SetTrade_type("JSAPI");
+            // $input->SetOpenid($openId);
+            // $order = WxPayApi::unifiedOrder($input);
+            // $jsApiParameters = $tools->GetJsApiParameters($order);
+            // $editAddress = $tools->GetEditAddressParameters();
         }
 }
