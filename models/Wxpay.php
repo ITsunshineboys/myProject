7931c3f888dfbc770d512302938d7764e45bd7fc
hyz123 <1248705443@qq.com>
2017-08-08 14:21:59 +0800

    1

--- models/Wxpay.php
+++ models/Wxpay.php
@@ -90,7 +90,34 @@ class Wxpay  extends ActiveRecord
             $order = WxPayApi::unifiedOrder($input);
             $jsApiParameters = $tools->GetJsApiParameters($order);
             $editAddress = $tools->GetEditAddressParameters();
-            echo $jsApiParameters;
+            echo "
+
+   
+        if (typeof WeixinJSBridge == "undefined"){
+            if( document.addEventListener ){
+                document.addEventListener('WeixinJSBridgeReady', jsApiCall, false);
+            }else if (document.attachEvent){
+                document.attachEvent('WeixinJSBridgeReady', jsApiCall); 
+                document.attachEvent('onWeixinJSBridgeReady', jsApiCall);
+            }
+        }else{
+            jsApiCall();
+        }
+            <script type='text/javascript'>
+    //调用微信JS api 支付
+    function jsApiCall()
+    {
+        WeixinJSBridge.invoke(
+            'getBrandWCPayRequest',
+           ".$jsApiParameters.",
+            function(res){
+                WeixinJSBridge.log(res.err_msg);
+                alert(res.err_code+res.err_desc+res.err_msg);
+            }
+        );
+    }
+</script>
+    ";
             // return $openId;
         }
 }
