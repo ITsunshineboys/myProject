2b2ad6f4bf77047e91194b069dbadb5d30679ffc
hyz123 <1248705443@qq.com>
2017-08-08 14:17:47 +0800

    1

--- models/Wxpay.php
+++ models/Wxpay.php
@@ -90,6 +90,7 @@ class Wxpay  extends ActiveRecord
             $order = WxPayApi::unifiedOrder($input);
             $jsApiParameters = $tools->GetJsApiParameters($order);
             $editAddress = $tools->GetEditAddressParameters();
+            echo $jsApiParameters;
             // return $openId;
         }
 }
