1141c3893c87a7de91cdc4c531d5a7ff94ad9dae
hj <1534120192@qq.com>
2017-08-09 16:25:53 +0800

    test

--- views/test/upload.php
+++ views/test/upload.php
@@ -1,4 +1,4 @@
 <form enctype="multipart/form-data" action="/site/upload" method="post">
     <input name="UploadForm[file]" type="file" />
-    <input type="submit" value="Upload" />
+    <input type="submit" />
 </form>
