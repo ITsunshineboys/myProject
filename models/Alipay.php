82c20bb69b27af16e69c6ab553fc2a31a542cc50
hyz123 <1248705443@qq.com>
2017-08-16 10:57:46 +0800

    1

--- models/Alipay.php
+++ models/Alipay.php
@@ -51,26 +51,7 @@ class Alipay extends  ActiveRecord
         $result=$payResponse->wapPay($payRequestBuilder,'http://test.cdlhzz.cn:888/order/alipayeffect_earnstnotify','http://test.cdlhzz.cn:888/line/effect_earnstsuccess_pay');
     }
 
-    public  static function  effect_earnstsubmit($effect_id,$name,$phone,$out_trade_no)
-    {
-        $config=(new Alipayconfig())->alipayconfig();
-        $str=$effect_id.'&'.$name.'&'.$phone;
-        $total_amount=0.01;
-        $passback_params=urlencode($str);
-        $notify_url='http://test.cdlhzz.cn:888/order/alipayeffect_earnstnotify';
-        $return_url='http://test.cdlhzz.cn:888/line/effect_earnstsuccess_pay';
-        //超时时间
-        $timeout_express="1m";
-        $payRequestBuilder = new AlipayTradeWapPayContentBuilder();
-        $payRequestBuilder->setBody('此笔订单为样板间定金');
-        $payRequestBuilder->setSubject('样板间申请费');
-        $payRequestBuilder->setOutTradeNo($out_trade_no);
-        $payRequestBuilder->setTotalAmount($total_amount);
-        $payRequestBuilder->setTimeExpress($timeout_express);
-        $payRequestBuilder->setPassback_params($passback_params);
-        $payResponse = new AlipayTradeService($config);
-        $result=$payResponse->wapPay($payRequestBuilder,$notify_url,$return_url);
-    }
+    
     public function Alipaylinenotify(){
         $config=(new AlipayConfig())->alipayconfig();
         $alipaySevice = new AlipayTradeService($config);
