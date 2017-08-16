55a49321217b1cf0ff6b86fe5c7946cb530fed3f
hyz123 <1248705443@qq.com>
2017-08-16 11:17:31 +0800

    1

--- controllers/OrderController.php
+++ controllers/OrderController.php
@@ -16,6 +16,7 @@ use app\models\Lhzz;
 use app\services\SmValidationService;
 use app\services\AlipayTradeService;
 use app\services\ExceptionHandleService;
+use yii\db\Query;
 use yii\filters\AccessControl;
 use yii\filters\VerbFilter;
 use yii\helpers\Json;
@@ -1767,6 +1768,13 @@ class OrderController extends Controller
                 'msg' => '修改失败，参数未做任何修改',
             ]);
         }
+    }
+    public function actionTesteffecf(){
+        $data=(new Query())->from('effect_earnst')->all();
+        return Json::encode([
+            'code' => 200,
+            'data' =>  $data,
+        ]);
     }
 
     /**
