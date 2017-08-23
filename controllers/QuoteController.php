d698cf6745b15dce9458979ac857baafce59749a
Wch <362224550@qq.com>
2017-08-23 13:55:26 +0800

    图片添加

--- controllers/QuoteController.php
+++ controllers/QuoteController.php
@@ -592,13 +592,16 @@ class QuoteController extends Controller
                 $effect_id = \Yii::$app->db->getLastInsertID();
                 (new DecorationParticulars())->plotAdd($effect_id,$hall_area,$hall_perimeter,$bedroom_area,$bedroom_perimeter,$toilet_area,$toilet_perimeter,$kitchen_area,$kitchen_perimeter,$modelling_length,$flat_area,$balcony_area);
 
-                foreach ($house['drawing_list'] as $images){
-                    $effect_images = $images['all_drawing'];
-                    $series_id     = $images['series'];
-                    $style_id      = $images['style'];
-                    $images_user   = $images['drawing_name'];
-                    ( new EffectPicture())->plotAdd($effect_id,$effect_images,$series_id,$style_id,$images_user);
+                if (empty($house['drawing_list'])){
+                    foreach ($house['drawing_list'] as $images){
+                        $effect_images = $images['all_drawing'];
+                        $series_id     = $images['series'];
+                        $style_id      = $images['style'];
+                        $images_user   = $images['drawing_name'];
+                        ( new EffectPicture())->plotAdd($effect_id,$effect_images,$series_id,$style_id,$images_user);
+                    }
                 }
+
             }
             else {
                 $bedroom = $house['cur_room'];
