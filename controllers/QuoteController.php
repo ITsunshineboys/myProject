1e4a229a25b080df2b2f4ba4bc04b0273058e261
Wch <362224550@qq.com>
2017-08-26 11:37:18 +0800

    1111

--- controllers/QuoteController.php
+++ controllers/QuoteController.php
@@ -619,6 +619,7 @@ class QuoteController extends Controller
         foreach ($request['house_informations'] as $house) {
             //添加功能
             if (!in_array("id",$house)) {
+                echo 1111;exit;
                 if ($house['is_ordinary'] != 1 ){
                     //普通户型添加
                     $bedroom = $house['cur_room'];
@@ -735,6 +736,7 @@ class QuoteController extends Controller
                     }
                 }
             } else{
+                echo 2222;exit;
                 if ($house['is_ordinary'] != 1 ){
                     //普通户型修改
                     $house_id = $house['id'];
