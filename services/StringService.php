23fe8431db04ab05751bf2983e8bc3f93478fb9a
hyz123 <1248705443@qq.com>
2017-09-12 17:46:40 +0800

    add get client ip  function in StringService

--- services/StringService.php
+++ services/StringService.php
@@ -400,4 +400,20 @@ class StringService
             return -1;
         }
     }
+
+    /**get  client ip
+     * @return array|false|string
+     */
+    public  static  function  getClientIP()
+    {
+        global $ip;
+        if (getenv("HTTP_CLIENT_IP"))
+            $ip = getenv("HTTP_CLIENT_IP");
+        else if(getenv("HTTP_X_FORWARDED_FOR"))
+            $ip = getenv("HTTP_X_FORWARDED_FOR");
+        else if(getenv("REMOTE_ADDR"))
+            $ip = getenv("REMOTE_ADDR");
+        else $ip = "Unknow";
+        return $ip;
+    }
 }
