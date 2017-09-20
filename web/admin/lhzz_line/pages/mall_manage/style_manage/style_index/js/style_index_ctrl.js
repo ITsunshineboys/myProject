3f3858d9bfe7090f93d9f72a4bf28add0edbf15c
tiger <tiger@163.com>
2017-09-20 15:44:08 +0800

    tiger

--- web/admin/lhzz_line/pages/mall_manage/style_manage/style_index/js/style_index_ctrl.js
+++ web/admin/lhzz_line/pages/mall_manage/style_manage/style_index/js/style_index_ctrl.js
@@ -307,8 +307,8 @@ style_index.controller("style_index",function ($scope,$http,$stateParams) {
             $http.get('http://test.cdlhzz.cn:888/mall/goods-attr-list-admin',{
                 params: {pid:+newVal},
             }).then(function (res) {
-                console.log(res);
                 $scope.proptable = res.data.data.goods_attr_list_admin.details;
+                console.log(res);
             },function (err) {
                 console.log(err);
             })
