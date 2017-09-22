fbced56c1e65405b79da055778571f1044f78e72
hyz123 <1248705443@qq.com>
2017-09-22 13:44:10 +0800

    modify

--- models/GoodsOrder.php
+++ models/GoodsOrder.php
@@ -1952,6 +1952,11 @@ class GoodsOrder extends ActiveRecord
                 $user->login_role_id=7;
                 $arr[$key]['availableamount']=self::switchMoney($user->availableamount*0.01);
             }else{
+                if ($user->login_role_id==7)
+                {
+                    $user->login_role_id=7;
+                    $arr[$key]['availableamount']=self::switchMoney($user->availableamount*0.01);
+                }
                 $arr[$key]['availableamount']=self::switchMoney(Role::CheckUserRole($user->login_role_id)->where(['uid'=>$user->id])->one()->availableamount*0.01);
             }
             $create_time[$key]  = $arr[$key]['create_time'];
