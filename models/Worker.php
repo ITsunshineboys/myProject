151f4bb8cc62f42d89608f6aea1e8223dd67c910
yangr <yr@qq.com>
2017-09-27 11:39:01 +0800

    edit

--- models/Worker.php
+++ models/Worker.php
@@ -193,7 +193,7 @@ class Worker extends \yii\db\ActiveRecord
         $query=new Query();
         $array=$query
             ->from('worker as w')
-            ->select('w.examine_status,w.icon,w.nickname,lc.rank,u.aite_cube_no')
+            ->select('w.examine_status,w.icon,w.nickname,lc.rank,lc.worker_kind,u.aite_cube_no')
             ->leftJoin('user as u','w.uid=u.id')
             ->leftJoin('labor_cost as lc','w.labor_cost_id=lc.id')
             ->where(['w.uid'=>$uid])
@@ -234,7 +234,6 @@ class Worker extends \yii\db\ActiveRecord
                 return $code;
             }
             $user=User::find()->where(['id'=>$uid])->one();
-
             $user->identity_no=$post['identity_no'];
             $user->identity_card_front_image=$post['identity_card_front_image'];
             $user->identity_card_back_image=$post['identity_card_back_image'];
