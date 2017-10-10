ae28c02a75f87383f053778c69adddb101ba7b1f
hyz123 <1248705443@qq.com>
2017-10-10 14:56:19 +0800

    1

--- controllers/OrderController.php
+++ controllers/OrderController.php
@@ -1694,7 +1694,6 @@ class OrderController extends Controller
             ->asArray()->one();
 
         $supplier=Supplier::find()->where(['id'=>$order['supplier_id']])->one();
-var_dump($supplier);exit;
         $u=User::find()->where(['id'=>$supplier->uid])->asArray()->one();
         var_dump($u);exit;
         $order=GoodsOrder::find()->select('id')->where(['order_no'=>$order_no,'supplier_id'=>$supplier->id])->one();
