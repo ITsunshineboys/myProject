ea38b840977d155a1be36af5100435e7c21630a5
hyz123 <1248705443@qq.com>
2017-10-12 11:17:26 +0800

    1

--- controllers/OrderController.php
+++ controllers/OrderController.php
@@ -305,7 +305,7 @@ class OrderController extends Controller
                     'msg'  => Yii::$app->params['errorCodes'][$code]
                 ]);
             }
-             $Goods=Goods::findOne($goods_id)->toArray();
+             $Goods=Goods::findOne($goods_id);
             var_dump($Goods);exit;
             $data=GoodsOrder::getlinegoodsdata($goods_id,$goods_num);
            if (is_numeric($data))
