e878bd2e99773f06227ecd8cf4f4638fc644bd41
yangr <yr@qq.com>
2017-10-14 17:25:58 +0800

    test

--- models/SupplierCashManager.php
+++ models/SupplierCashManager.php
@@ -104,6 +104,7 @@ class SupplierCashManager extends ActiveRecord
         }
         $bankcard = self::GetBankcard($arr['bank_log_id']);
         $supplier = self::GetSupplier($supplier_id);
+        var_dump($bankcard,$supplier);die;
         if (!$bankcard || !$supplier) {
             return null;
         }
