7a1340d1966c3277563326db3471e1d7c50e8d25
yangr <yr@qq.com>
2017-10-14 16:10:08 +0800

    11

--- models/SupplierCashManager.php
+++ models/SupplierCashManager.php
@@ -322,7 +322,8 @@ class SupplierCashManager extends ActiveRecord
             ->from(self::SUP_CASHREGISTER . ' as g')
             ->where(['g.role_id' => self::ROLE_ID])
             ->leftJoin(self::SUPPLIER . ' s', 'g.uid = s.id')
-            ->select(['g.id', 'g.cash_money', 'g.apply_time', 's.shop_name', 's.shop_no', 'g.uid', 'g.status', 'g.real_money']);
+            ->select(['g.id', 'g.cash_money', 'g.apply_time', 's.shop_name', 's.shop_no', 'g.uid', 'g.status', 'g.real_money'])
+            ->orderBy('g.apply_time Desc');
 
         if ($status) {
             $query->andWhere(['g.status' => $status]);
