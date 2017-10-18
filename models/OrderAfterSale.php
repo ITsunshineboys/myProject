3aa3fd7393734fe374ba7de684f271940a93cf30
hyz123 <1248705443@qq.com>
2017-10-18 11:19:23 +0800

    1

--- models/OrderAfterSale.php
+++ models/OrderAfterSale.php
@@ -702,7 +702,8 @@ class OrderAfterSale extends ActiveRecord
             'code'=>'',
             'status'=>''
         ];
-
+        $OrderGoods=OrderGoods::find()->where(['order_no'=>$OrderAfterSale->order_no,'sku'=>$OrderAfterSale->sku])->one()->customer_service;
+        echo $OrderGoods;exit;
         if ($OrderAfterSale->buyer_confirm!=1){
             switch ($role)
             {
