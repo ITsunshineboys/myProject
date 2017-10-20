46f000c90e7354ad6d8a203cbb6e8e12688b725a
xiexueying <837241830@qq.com>
2017-10-20 16:21:00 +0800

    11254

--- web/line/js/controller.js
+++ web/line/js/controller.js
@@ -964,9 +964,9 @@ angular.module("all_controller", [])
                             if($scope.codeWX == 200){  // 微信支付
                                 alert('调用微信接口');
                                 // 微信接口
-                                $http.get({'http://common.cdlhzz.cn/order/lineplaceorder?goods_name='+$scope.title+'&order_price='+$scope.allCost+'&goods_num='+$scope.shopNum+'&goods_id='+$scope.mall_id+'&address_id='+$scope.address_id+'&invoice_id='+$scope.invoice_id+'&supplier_id='+$scope.supplier_id+'&freight='+$scope.freight
+                                $http.get('http://common.cdlhzz.cn/order/lineplaceorder?goods_name='+$scope.title+'&order_price='+$scope.allCost+'&goods_num='+$scope.shopNum+'&goods_id='+$scope.mall_id+'&address_id='+$scope.address_id+'&invoice_id='+$scope.invoice_id+'&supplier_id='+$scope.supplier_id+'&freight='+$scope.freight
                                     // buyer_message: $scope.leaveMessage
-                                }).then(function (response) {
+                                ).then(function (response) {
                                     // console.log(response);
                                     alert($scope.mall_id +'商品ID');
                                     alert($scope.address_id+'地址id');
