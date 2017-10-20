8ce17d86e879d82e320f9e9bb908e267a4cdcd89
yangr <yr@qq.com>
2017-10-20 14:57:48 +0800

    测试 极光推送

--- controllers/JpushController.php
+++ controllers/JpushController.php
@@ -5,163 +5,35 @@ namespace app\controllers;
 
 
 
-use JPush\Client;
-use JPush\Exceptions\APIConnectionException;
-use JPush\Exceptions\APIRequestException;
-use JPush\PushPayload;
-use yii\web\Response;
-use yii;
-class JpushController extends \yii\web\Controller
+
+use app\models\Jpush;
+
+use yii\web\Controller;
+
+class JpushController extends Controller
 {
+    public function actionPush()
+    {
 
-
-
-//    public function actionIndex(){
-//        if(Yii::$app->request->isAjax){
-//            $client = new \JPush\Client('af59e43d6324a2a9d995f72f','44c54839bd576b9a2a476275');
-//            $data=array(
-//                'act'=>'',
-//                'data'=>array(
-//                    'id'=>2,
-//                    'orderid'=>'101728225126629786',
-//                    'poi_name'=>'湘菜龙虾湘菜龙虾',
-//                    'poi_lat'=>'4530397.424610',
-//                    'poi_lng'=>'12535784.223957',
-//                )
-//            );
-//
-////            var_dump($re);exit;
-//            $response = $client->push()
-//                ->setPlatform('all')
-//                ->addAllAudience()
-//                ->setNotificationAlert('Hi, JPush');
-//
-//
-//
-//            if(true){
-//                Yii::$app->response->format = Response::FORMAT_JSON;
-//                return ['code'=>1,'message'=>'发生成功'];
-//            }else{
-//                Yii::$app->response->format = Response::FORMAT_JSON;
-//                return ['code'=>2,'message'=>'发生失败'];
-//            }
-//        }
-//        return $this->render('index');
-//    }
-
-public function actionPu(){
-    $client = new \JPush\Client(' af59e43d6324a2a9d995f72f','44c54839bd576b9a2a476275');
-
-    $pusher = $client->push();
-    $pusher->setPlatform('all');
-    $pusher->addAllAudience();
-    $pusher->setNotificationAlert('Hello, JPush');
-    try {
-        $pusher->send();
-    } catch (\JPush\Exceptions\JPushException $e) {
-        // try something else here
-        print $e;
+        $push = new Jpush();
+        $extras = ['url'=>['www.baidu.com']];//推送附加字段的类型
+        $m_time = '86400';//离线保留时间
+        $receive = ['registration_id'=>['190e35f7e07d39f4c5c']];//设备的id标识
+        $title='订单发货了!';
+        $content = '你的订单已经发货,点击详情查看!';
+        $result = $push->push($receive,$title,$content,$extras, $m_time);
+        if($result){
+            return json_encode([
+                'code'=>200,
+                'msg'=>'ok'
+            ]);
+        }else{
+            return json_encode([
+               'code'=>500,
+                'msg'=>'失败'
+            ]);
+        }
     }
-
-//   $a= $push->message(['1'=>'2'],['a'=>2]);
-
-//    $a= $push->setNotificationAlert('alert');
-
-//    $a= $push->iosNotification();
-// OR
-//    $b= $push->iosNotification('hello');
-//// OR
-//    $a=$push->iosNotification('hello', [
-//        'sound' => 'sound',
-//        'badge' => '+1',
-//        'extras' => [
-//            'key' => 'value'
-//        ]
-//    ]);
-//    var_dump($a);
-//$res=new PushPayload($client);
-//   $a= $res
-//        ->setPlatform('ios')
-//        ->setAudience('all')
-//        ->iosNotification('ater')
-//        ->send();
-//    $a=$push->getCid('1','push');
-//    $a= $push->addAllAudience();
-
-//    $push->addTag('tag1');
-//// OR
-//    $push->addTag(['tag1', 'tag2']);
-    //获取cid
-//    $cid=$push->getCid('1','push');
-////    return json_encode($cid['body']['cidlist']);
-//
-//   var_dump($cid['body']['cidlist']);
-//    $a=$push->getCid();
-//    var_dump($a);
-
-//    $audience=$push->setAudience('all');
-//   $pusher = $client->push();
-//    $pusher->setPlatform('all');
-//    $pusher->addAllAudience();
-//    $pusher->setNotificationAlert('yangru, ddddd');
-//    try {
-//        $pusher->send();
-//    } catch (\JPush\Exceptions\JPushException $e) {
-//        // try something else here
-//        print $e;
-//    }
-//    $app_key = "7b528331738ec719165798fd";
-//    $master_secret = "32da4e2c06ac7b55da2c9228";
-//    $client = new Client($app_key, $master_secret);
-
-//    //简单的推送样例
-//    $auth=base64_decode("7973f8b4ad32cb15c10ea2b0:7cf2502eba69046f73bebb39");
-//
-//
-//
-//    $headers=[
-//        "Content-Type: application/json",
-//        "Authorization: Basic $auth"
-//    ];
-//
-//    $url="https://api.jpush.cn/v3/push";
-//    //初始化
-//    $curl = curl_init();
-//    //设置抓取的url
-//    curl_setopt($curl, CURLOPT_URL, $url);
-//    //设置头文件的信息作为数据流输出
-//    curl_setopt($curl, CURLOPT_HEADER, 0);
-//
-//    //设置获取的信息以文件流的形式返回，而不是直接输出。
-//    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 0);
-//
-//    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-//    //执行命令
-//    $data = curl_exec($curl);
-//    //关闭URL请求
-//    curl_close($curl);
-//    //显示获得的数据
-//
-//    var_dump($data);
-
-
-// $push_payload=$client->push()->setPlatform('all')->addAllAudience()->setNotificationAlert('alert');
-//    try{
-//        $response=$push_payload->send();
-//        var_dump($response);
-//
-//
-//    }catch (APIConnectionException $e){
-//
-//        echo $e;
-//
-//    }catch (APIRequestException $e){
-//
-//        echo $e;
-//    }
-
-}
-
 
 
 }
