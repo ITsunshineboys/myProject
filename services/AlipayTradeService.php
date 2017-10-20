fb60344b131ef0381d0b09039ec8143d813fde72
hyz123 <1248705443@qq.com>
2017-10-20 15:06:04 +0800

    1

--- services/AlipayTradeService.php
+++ services/AlipayTradeService.php
@@ -79,30 +79,25 @@ class AlipayTradeService {
      * @return $response 支付宝返回的信息
      */
     function appPay($builder,$return_url,$notify_url) {
-        $aop = new AopClient;
-        $aop->gatewayUrl = "https://openapi.alipay.com/gateway.do";
-        $aop->appId = "app_id";
-        $aop->rsaPrivateKey =$this->private_key;
+        $aop=new AopClient();
+        $aop->gatewayUrl = $this->gateway_url;
+        $aop->appId = $this->appid;
+        $aop->rsaPrivateKey =  $this->private_key;
         $aop->format = "json";
         $aop->charset = "UTF-8";
         $aop->signType = "RSA2";
         $aop->alipayrsaPublicKey = $this->alipay_public_key;
-//实例化具体API对应的request类,类名称和接口名称对应,当前调用接口名称：alipay.trade.app.pay
+//        $biz_content=$builder->getBizContent();
+        $biz_content=$builder->getBizContentApp();
         $request = new AlipayTradeAppPayRequest();
 //SDK已经封装掉了公共参数，这里只需要传入业务参数
-        $bizcontent = "{\"body\":\"我是测试数据\","
-            . "\"subject\": \"App支付测试\","
-            . "\"out_trade_no\": \"201710201457\","
-            . "\"timeout_express\": \"30m\","
-            . "\"total_amount\": \"0.01\","
-            . "\"product_code\":\"QUICK_MSECURITY_PAY\""
-            . "}";
-        $request->setNotifyUrl("商户外网可以访问的异步地址");
+        $bizcontent =$biz_content;
+        $request->setNotifyUrl($notify_url);
         $request->setBizContent($bizcontent);
-//这里和普通的接口调用不同，使用的是sdkExecute
+        //这里和普通的接口调用不同，使用的是sdkExecute
         $response = $aop->sdkExecute($request);
-//htmlspecialchars是为了输出到页面时防止被浏览器将关键参数html转义，实际打印到日志以及http传输不会有这个问题
-       return htmlspecialchars($response);//就是orderString 可以直接给客户端请求，无需再做处理。
+        echo  htmlspecialchars($response);
+        exit;
     }
     /**
      * alipay.trade.wap.pay
