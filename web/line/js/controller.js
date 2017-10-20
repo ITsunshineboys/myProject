7775a8d45b611d5b93d09b6e0fb05e9d93551599
xiexueying <837241830@qq.com>
2017-10-20 16:41:08 +0800

    151551

--- web/line/js/controller.js
+++ web/line/js/controller.js
@@ -968,7 +968,6 @@ angular.module("all_controller", [])
                                 $http.get('http://common.cdlhzz.cn/order/lineplaceorder',{
                                     params:{
                                         goods_name: $scope.title,
-                                        
                                         order_price:$scope.allCost,
                                         goods_num:+$scope.shopNum,
                                         goods_id:+$scope.mall_id,
@@ -978,7 +977,8 @@ angular.module("all_controller", [])
                                         freight:+$scope.freight,
                                         // buyer_message: $scope.leaveMessage
                                 }}).then(function (response) {
-                                    // console.log(response);
+                                    $scope.dataWx = response.data;
+                                    alert($scope.dataFram);
                                     alert($scope.mall_id +'商品ID');
                                     alert($scope.address_id+'地址id');
                                     alert($scope.invoice_id+'发票id');
@@ -986,6 +986,10 @@ angular.module("all_controller", [])
                                     alert(JSON.stringify(response));
                                     alert(JSON.stringify(response.data));
                                     alert(JSON.stringify(response.config));
+
+                                    $('body').append($scope.dataWx);
+                                },function (error) {
+                                    alert(error)
                                 })
                             }
                             if($scope.codeWX == 201){  //非微信浏览器 === 支付宝
