ab746ed67c4a5659f0fb0d9d9ee5b3bf31cf2139
afang <1570948949@qq.com>
2017-10-20 17:37:45 +0800

    1111

--- web/admin/lhzz_line/pages/distribution/js/distribution.js
+++ web/admin/lhzz_line/pages/distribution/js/distribution.js
@@ -1,5 +1,5 @@
 angular.module('distribution',[])
-   .controller('distribution_ctrl',function ($scope,$http,$state) {
+   .controller('distribution_ctrl',function ($scope,$http,$state,$uibModal) {
        //post请求配置
        const config = {
            headers: {'Content-Type': 'application/x-www-form-urlencoded'},
@@ -16,6 +16,21 @@ angular.module('distribution',[])
        }, function (error) {
            console.log(error)
        })
+       //跳转分销首页
+       $scope.go_index = function () {
+           $scope.second_title = ''
+           $scope.three_title = ''
+           $scope.four_title = ''
+           $state.go('distribution.index')
+       }
+       //跳转二级页面
+       $scope.go_second = function () {
+           $scope.three_title = ''
+           $scope.four_title = ''
+           if ($scope.second_title == '详情') {
+               $state.go('distribution.detail')
+           }
+       }
        $scope.ctrlScope = $scope
        $scope.time_type = [
            {name:'全部时间',str:'all'},
@@ -156,6 +171,10 @@ angular.module('distribution',[])
            },config).then(function (response) {
                console.log(response)
                $scope.second_title = '详情'
+               $scope.fatherset = response.data.data.fatherset
+               $scope.myself = response.data.data.myself
+               $scope.profit = response.data.data.profit
+               $scope.subset = response.data.data.subset
                $state.go('distribution.detail')
            },function (error) {
                console.log(error)
@@ -163,12 +182,71 @@ angular.module('distribution',[])
        }
        //查看相关联交易订单列表
        $scope.associate = function () {
-           $http.get('/distribution/searchmore',{
+           $http.get('/distribution/correlate-order',{
                params:{
                    mobile:$scope.cur_item.mobile
                }
            }).then(function (response) {
                console.log(response)
+               $scope.three_title = '相关联交易订单'
+               $scope.total_amount = response.data.data.total_amount
+               $scope.total_orders = response.data.data.total_orders
+               $scope.all_order_detail = response.data.data.details
+               $state.go('distribution.associate_list')
+           },function (error) {
+               console.log(error)
+           })
+       }
+       //保存收益
+       $scope.save_profit = function (valid) {
+           let all_modal = function ($scope, $uibModalInstance) {
+               $scope.cur_title = '保存成功'
+               $scope.common_house = function () {
+                   $uibModalInstance.close()
+                   $state.go('distribution.index')
+               }
+           }
+           all_modal.$inject = ['$scope', '$uibModalInstance']
+           if(valid){
+               $http.post('/distribution/add-profit',{
+                   mobile:$scope.myself.mobile,
+                   profit:$scope.profit
+               },config).then(function (res) {
+                   console.log(res)
+                   $uibModal.open({
+                       templateUrl: 'pages/intelligent/cur_model.html',
+                       controller: all_modal
+                   })  
+               },function (error) {
+                   console.log(error)
+               })
+           }else{
+               $scope.submitted = true
+           }
+       }
+       //修改关联列表备注
+       //弹出备注模态框
+       $scope.replace_remarks = function (item) {
+           $scope.cur_associate_item = angular.copy(item)
+       }
+       //修改备注请求
+       $scope.edit_remarks = function () {
+           $http.post('/distribution/add-remarks',{
+               order_no:$scope.cur_associate_item.order_no,
+               remarks:$scope.cur_associate_item.remarks
+           },config).then(function (response) {
+               console.log(response)
+               $http.get('/distribution/correlate-order',{
+                   params:{
+                       mobile:$scope.cur_item.mobile
+                   }
+               }).then(function (response) {
+                   console.log(response)
+                   $scope.all_order_detail = response.data.data.details
+                   $state.go('distribution.associate_list')
+               },function (error) {
+                   console.log(error)
+               })
            },function (error) {
                console.log(error)
            })
