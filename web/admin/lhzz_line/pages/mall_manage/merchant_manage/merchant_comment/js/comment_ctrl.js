1eec2b7643b3b3b74804f55ad63cea78ffc631f8
tiger <tiger@163.com>
2017-10-21 10:25:59 +0800

    delete some useless files & edit store_detail files

--- web/admin/lhzz_line/pages/mall_manage/merchant_manage/merchant_comment/js/comment_ctrl.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by Administrator on 2017/8/8 0008.
- */
-var comment= angular.module("comment",[])
-    .controller("comment_ctrl",function ($scope,$http) {
-        let vm = $scope.vm = {};
-    })
