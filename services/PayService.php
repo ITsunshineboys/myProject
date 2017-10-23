6868100b67a78356f377e5884ca9882041e28da3
hyz123 <1248705443@qq.com>
2017-10-23 11:19:01 +0800

    test

--- services/PayService.php
+++ services/PayService.php
@@ -50,11 +50,11 @@ class PayService
             //通过code获得openid
     //        if (!isset($_GET['code'])){
                 //触发微信返回code码
-                $baseUrl = urlencode('http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'].$_SERVER['QUERY_STRING']);
-                $url = $this->__CreateOauthUrlForCode($baseUrl);
-               return $url;
-                // Header("Location: $url");
-                // exit();
+                $baseUrl = urlencode('http://common.cdlhzz.cn/line');
+            $url = $this->__CreateOauthUrlForCode($baseUrl);
+//            return $url;
+            Header("Location: $url");
+            exit();
     //        } else {
     //            //获取code码，以获取openid
     //            $code = $_GET['code'];
