42b8122dc2d75a034c3c55f23c052a641d819a88
hyz123 <1248705443@qq.com>
2017-10-24 15:29:10 +0800

    1

--- models/Wxpay.php
+++ models/Wxpay.php
@@ -63,6 +63,7 @@ class Wxpay  extends ActiveRecord
         echo "<script type='text/javascript'>if (typeof WeixinJSBridge == 'undefined'){if( document.addEventListener ){document.addEventListener('WeixinJSBridgeReady', jsApiCall, false);}else if (document.attachEvent){document.attachEvent('WeixinJSBridgeReady', jsApiCall);document.attachEvent('onWeixinJSBridgeReady', jsApiCall);}}else{jsApiCall();}//调用微信JS api 支付
  function jsApiCall(){ WeixinJSBridge.invoke('getBrandWCPayRequest',".$jsApiParameters.",function(res){WeixinJSBridge.log(res.err_msg);alert(res.err_code+res.err_desc+res.err_msg);});}
 </script>";
+exit;
     }
         /**
          * 样板间申请支付定金
