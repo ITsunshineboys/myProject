4086d786fd2cba12bc1d65e0aa63b3fd8536a12b
xiexueying <837241830@qq.com>
2017-10-24 11:10:29 +0800

    12121

--- web/line/js/controller.js
+++ web/line/js/controller.js
@@ -1035,6 +1035,7 @@ angular.module("all_controller", [])
                                     }
                                 }).then(function successCallback(response) {
                                     console.log('成功');
+                                    $scope.contentWx = response.data.data;
                                     alert(JSON.stringify(response.data.data));
                                     alert(JSON.stringify(response.config));
                                 },function (error) {
@@ -1042,6 +1043,26 @@ angular.module("all_controller", [])
                                     alert(error);
                                     alert(JSON.stringify(error))
                                 });
+
+                                // 跳转微信支付界面
+                                function onBridgeReady(){
+                                    WeixinJSBridge.invoke(
+                                        'getBrandWCPayRequest', $scope.contentWx ,
+                                        function(res){
+                                            if(res.err_msg == "get_brand_wcpay_request:ok" ) {}     // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。
+                                        }
+                                    );
+                                }
+                                if (typeof WeixinJSBridge == "undefined"){
+                                    if( document.addEventListener ){
+                                        document.addEventListener('WeixinJSBridgeReady', onBridgeReady, false);
+                                    }else if (document.attachEvent){
+                                        document.attachEvent('WeixinJSBridgeReady', onBridgeReady);
+                                        document.attachEvent('onWeixinJSBridgeReady', onBridgeReady);
+                                    }
+                                }else{
+                                    onBridgeReady();
+                                }
                             }
                             if($scope.codeWX == 201){  //非微信浏览器 === 支付宝
                                 // 支付宝接口
