8bc1de07b65574b9878a78ae51cafbec7427cb09
Wch <362224550@qq.com>
2017-10-26 11:39:29 +0800

    OK

--- services/BasisDecorationService.php
+++ services/BasisDecorationService.php
@@ -1081,11 +1081,7 @@ class BasisDecorationService
             return  $cement;
         }
 //        个数：（水泥用量÷抓取的商品的KG）
-        var_dump($new_dosage);
-        var_dump($value);
-        var_dump($new_dosage / $value);
         $cement['quantity'] = ceil($new_dosage / $value);
-        var_dump( $cement['quantity']);exit;
 //        水泥费用：个数×抓取的商品价格
         $cement['cost'] = $cement['quantity'] * $goods['platform_price'];
         return $cement;
