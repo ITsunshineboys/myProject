75b377130663251985a259e52d41964422e3b33e
wangjie <1248136872@qq.com>
2017-10-26 15:01:41 +0800

    2017年10月26日15:01:33

--- web/admin/supplier/pages/supplier_wallet/js/supplier_wallet.js
+++ web/admin/supplier/pages/supplier_wallet/js/supplier_wallet.js
@@ -38,8 +38,6 @@ angular.module('supplier_wallet_module',[])
         status: '0',                      // 类型选择
     };
 
-
-
   $scope.supplier_wallet_list=[];
   //状态
   $scope.status_arr=[{id:0,value:'全部'},{id:1,value:'提现中'},{id:2,value:'已提现'},{id:3,value:'驳回'}];
@@ -51,12 +49,14 @@ angular.module('supplier_wallet_module',[])
   });
   //监听时间
     $scope.time_status_change=function () {
+        $scope.wjConfig.currentPage=1;
         $scope.params.time_start='';
         $scope.params.time_end='';
         tablePages();
     }
   //监听开始和结束时间
     $scope.time_change=function () {
+        $scope.wjConfig.currentPage=1;
         tablePages();
     }
 });
