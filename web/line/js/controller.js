6d458fd92fd05d0fe093ce8c31071a4e77e4a29c
xiexueying <837241830@qq.com>
2017-10-26 16:24:24 +0800

    1212

--- web/line/js/controller.js
+++ web/line/js/controller.js
@@ -523,7 +523,7 @@ angular.module("all_controller", ['ngCookies'])
                     wx.onMenuShareTimeline({
                         title: '艾特魔方极力推荐产品',      // 分享标题
                         link:  'http://common.cdlhzz.cn/line/#!/product_details?mall_id='+$scope.mall_id, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-                        imgUrl: 'http:common.cdlhzz.cn/'+$scope.datailsShop.image, // 分享图标
+                        imgUrl: 'http:common.cdlhzz.cn/'+$scope.datailsShop.cover_image, // 分享图标
                         success: function () {
                             // 用户确认分享后执行的回调函数
 
@@ -537,7 +537,7 @@ angular.module("all_controller", ['ngCookies'])
                         title: '艾特魔方极力推荐产品', // 分享标题
                         desc: '艾特魔方极力推荐产品', // 分享描述
                         link: 'http://common.cdlhzz.cn/line/#!/product_details?mall_id='+$scope.mall_id, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-                        imgUrl: $scope.datailsShop.image, // 分享图标
+                        imgUrl: 'http:common.cdlhzz.cn/'+$scope.datailsShop.cover_image, // 分享图标
                         type: '', // 分享类型,music、video或link，不填默认为link
                         dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
                         success: function () {
