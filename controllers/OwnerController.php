c2203559f83b57afd1299b0d50b1fdfa04a9ad9b
Wch <362224550@qq.com>
2017-10-27 10:57:53 +0800

    1

--- controllers/OwnerController.php
+++ controllers/OwnerController.php
@@ -1545,8 +1545,9 @@ class OwnerController extends Controller
         $style = trim(Yii::$app->request->post('style',''));
         $stairway = trim(Yii::$app->request->post('stairway',''));
         $stair_id = trim(Yii::$app->request->post('stair_id',''));
+        $toponymy = trim(Yii::$app->request->post('toponymy',''));
         if ($stairway == 0){
-            $where = ['and',['effect_picture.series_id'=>$series],['effect_picture.style_id'=>$style],['effect.stairway'=>$stairway],['type'=>1]];
+            $where = ['and',['effect_picture.series_id'=>$series],['effect_picture.style_id'=>$style],['effect.stairway'=>$stairway],['effect.toponymy'=>$toponymy],['type'=>1]];
             $effect = Effect::effectAndEffectPicture('effect.id',$where);
             if ($effect == null) {
                 $code = 1068;
@@ -1557,7 +1558,7 @@ class OwnerController extends Controller
                 ]);
             }
         } elseif ($stairway == 1) {
-            $where = ['and',['effect_picture.series_id'=>$series],['effect_picture.style_id'=>$style],['effect.stairway'=>$stairway],['effect.stair_id'=>$stair_id],['type'=>1]];
+            $where = ['and',['effect_picture.series_id'=>$series],['effect_picture.style_id'=>$style],['effect.stairway'=>$stairway],['effect.stair_id'=>$stair_id],['effect.toponymy'=>$toponymy],['type'=>1]];
             $effect = Effect::effectAndEffectPicture('effect.id',$where);
             if ($effect == null) {
                 $code = 1068;
