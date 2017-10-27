6304588207f710a2687f224fce21c0d97ab22281
hyz123 <1248705443@qq.com>
2017-10-27 18:06:43 +0800

    up

---
+++ models/UserNewsRecord.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hj
+ * Date: 4/27/17
+ * Time: 2:08 PM
+ */
+
+namespace app\models;
+
+use app\services\ModelService;
+use yii\db\ActiveRecord;
+
+class UserNewsRecord extends ActiveRecord
+{
+
+
+    /**
+     * @return string 返回该AR类关联的数据表名
+     */
+    public static function tableName()
+    {
+        return 'user_news_record';
+    }
+
+
+}
