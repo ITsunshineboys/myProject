f9db06a43cb52cbb3b250065d2fb6f1365f2666a
hyz123 <1248705443@qq.com>
2017-10-30 11:07:50 +0800

    test

--- controllers/OrderController.php
+++ controllers/OrderController.php
@@ -1712,8 +1712,9 @@ class OrderController extends Controller
             ]);
         }
         $GoodsOrder=GoodsOrder::FindByOrderNo($order_no);
-        var_dump($GoodsOrder->supplier_id);exit;
+        
         $supplier=Supplier::find()->where(['id'=>$GoodsOrder->supplier_id])->one();
+        var_dump($supplier->uid);exit;
        $supplier_user=User::find()->where(['id'=>$supplier->uid])->one();
          var_dump($supplier_user);exit;
         if ($GoodsOrder->pay_status==0)
