29ebfd47511b9381db92df92bb197014755ba8a7
yangr <yr@qq.com>
2017-11-01 17:58:43 +0800

    test

--- controllers/ChatController.php
+++ controllers/ChatController.php
@@ -472,7 +472,7 @@ class ChatController extends Controller
     }
 
     public function actionTest(){
-        var_dump(UserNewsRecord::deleteAll(['role_id'=>6]));die;
+        var_dump(UserNewsRecord::deleteAll(['order_no'=>0,'sku'=>0]));die;
         $chat=new ChatService();
        $a= $chat->getChatRecord('select+*+where+timestamp>1403164734226');
         return json_encode($a);
