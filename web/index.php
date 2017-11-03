2d022c5f169f51e7aa4ffa60cc9679c6106f9387
hyz123 <1248705443@qq.com>
2017-11-03 16:56:07 +0800

    up

--- web/index.php
+++ web/index.php
@@ -9,15 +9,18 @@ defined('YII_ENV') or define('YII_ENV', $config['params']['online']['env']);
 require(__DIR__ . '/../vendor/autoload.php');
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
+
 $origin = isset($_SERVER['HTTP_ORIGIN'])? $_SERVER['HTTP_ORIGIN'] : '';  
   
 $allow_origin = array(  
-    'http://cross.local.com:808' 
+    'http://cross.local.com:808'  
 );  
   
 if(in_array($origin, $allow_origin)){  
     header('Access-Control-Allow-Origin:'.$origin);       
 }
+    header("Access-Control-Allow-Credentials: true");
+
 // if (YII_DEBUG) {
 //     header('Access-Control-Allow-Origin: http://cross.local.com:808');
 //     header("Access-Control-Allow-Credentials: true");
