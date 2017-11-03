32063fffa8ea32a622e8799648d4eeca025888b0
yangr <yr@qq.com>
2017-11-03 18:29:36 +0800

    add

---
+++ models/EffectMaterial.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "effect_material".
+ *
+ * @property string $id
+ * @property string $effect_id
+ * @property integer $goods_id
+ * @property string $price
+ * @property integer $count
+ */
+class EffectMaterial extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'effect_material';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['effect_id'], 'required'],
+            [['effect_id', 'goods_id', 'count'], 'integer'],
+        ];
+    }
+
+}
