1beec5fd6a4ac2e4679d9964fe6594ca3b4deaf4
Piz <q564146734@163.com>
2017-11-03 10:03:09 +0800

    样板间有数据页面

--- web/owner/mall/js/details.js
+++ web/owner/mall/js/details.js
@@ -29,8 +29,6 @@ $(".pic_tabs li").on("click",function () {
    //$()
 });
 
-console.log("url上带的值=="+decodeURI(GetQueryString('txt')));
-
 app.controller("details",function($http,$scope){
    $http({
         method: 'post',
