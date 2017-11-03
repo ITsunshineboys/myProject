eab8d1a43df715cdef4c928f7e1e2ec400e93769
hj <1534120192@qq.com>
2017-11-03 16:43:23 +0800

    test

--- web/index.php
+++ web/index.php
@@ -10,7 +10,7 @@ require(__DIR__ . '/../vendor/autoload.php');
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
 if (YII_DEBUG) {
-    header('Access-Control-Allow-Origin: http://cross2.local.com:707');
+    header('Access-Control-Allow-Origin: http://cross.local.com:808');
     header("Access-Control-Allow-Credentials: true");
 //    header('Access-Control-Allow-Origin:' . apache_request_headers()['Origin']);
 }
