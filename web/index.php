eafed11f863a21a34eacdcb564d9da43fdb4fb40
hyz123 <1248705443@qq.com>
2017-11-03 16:52:04 +0800

    up

--- web/index.php
+++ web/index.php
@@ -9,10 +9,19 @@ defined('YII_ENV') or define('YII_ENV', $config['params']['online']['env']);
 require(__DIR__ . '/../vendor/autoload.php');
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
-if (YII_DEBUG) {
-    header('Access-Control-Allow-Origin: http://cross.local.com:808');
-    header("Access-Control-Allow-Credentials: true");
-//    header('Access-Control-Allow-Origin:' . apache_request_headers()['Origin']);
+$origin = isset($_SERVER['HTTP_ORIGIN'])? $_SERVER['HTTP_ORIGIN'] : '';  
+  
+$allow_origin = array(  
+    'http://cross.local.com:808' 
+);  
+  
+if(in_array($origin, $allow_origin)){  
+    header('Access-Control-Allow-Origin:'.$origin);       
 }
+// if (YII_DEBUG) {
+//     header('Access-Control-Allow-Origin: http://cross.local.com:808');
+//     header("Access-Control-Allow-Credentials: true");
+// //    header('Access-Control-Allow-Origin:' . apache_request_headers()['Origin']);
+// }
 
 (new yii\web\Application($config))->run();
