23b0f25961c9457f78d38afa702e698b0a76fab0
hj <1534120192@qq.com>
2017-11-04 15:09:15 +0800

    tet

--- web/index.php
+++ web/index.php
@@ -1,33 +1,18 @@
 <?php
 
 $config = require(__DIR__ . '/../config/web.php');
+$paramsOnline = require(__DIR__ . '/../config/params_online.php');
 
 // comment out the following two lines when deployed to production
-//define('YII_DEBUG', $config['params']['online']['env'] == 'dev');
-//define('YII_ENV', $config['params']['online']['env']);
-define('YII_DEBUG', true);
-define('YII_ENV', 'dev');
+define('YII_DEBUG', $paramsOnline['env'] == 'dev');
+define('YII_ENV', $paramsOnline['env']);
 
 require(__DIR__ . '/../vendor/autoload.php');
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
-//$origin = isset($_SERVER['HTTP_ORIGIN']) ? $_SERVER['HTTP_ORIGIN'] : '';
-//
-//$allow_origin = array(
-//    'http://cross.local.com:808',
-//    'http://cross.local.com'
-//);
-//
-//if (in_array($origin, $allow_origin)) {
-//    header('Access-Control-Allow-Origin:' . $origin);
-//}
-header('Access-Control-Allow-Origin: http://cross.local.com:808');
-header("Access-Control-Allow-Credentials: true");
-
-// if (YII_DEBUG) {
-//     header('Access-Control-Allow-Origin: http://cross.local.com:808');
-//     header("Access-Control-Allow-Credentials: true");
-// //    header('Access-Control-Allow-Origin:' . apache_request_headers()['Origin']);
-// }
+ if (YII_DEBUG) {
+     header('Access-Control-Allow-Origin: http://cross.local.com:808');
+     header("Access-Control-Allow-Credentials: true");
+ }
 
 (new yii\web\Application($config))->run();
