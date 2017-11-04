024729c46522d721ac8e26807774f3c9295c80e4
hj <1534120192@qq.com>
2017-11-04 11:25:36 +0800

    test

--- web/index.php
+++ web/index.php
@@ -9,16 +9,17 @@ define('YII_ENV', $config['params']['online']['env']);
 require(__DIR__ . '/../vendor/autoload.php');
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
-$origin = isset($_SERVER['HTTP_ORIGIN']) ? $_SERVER['HTTP_ORIGIN'] : '';
-
-$allow_origin = array(
-    'http://cross.local.com:808',
-    'http://cross.local.com'
-);
-
-if (in_array($origin, $allow_origin)) {
-    header('Access-Control-Allow-Origin:' . $origin);
-}
+//$origin = isset($_SERVER['HTTP_ORIGIN']) ? $_SERVER['HTTP_ORIGIN'] : '';
+//
+//$allow_origin = array(
+//    'http://cross.local.com:808',
+//    'http://cross.local.com'
+//);
+//
+//if (in_array($origin, $allow_origin)) {
+//    header('Access-Control-Allow-Origin:' . $origin);
+//}
+header('Access-Control-Allow-Origin: http://cross.local.com:808');
 header("Access-Control-Allow-Credentials: true");
 
 // if (YII_DEBUG) {
