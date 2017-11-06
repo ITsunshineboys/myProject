ce622377605f5379276dff23dba18079f38aa176
Wch <362224550@qq.com>
2017-11-06 11:54:40 +0800

    1

--- controllers/OwnerController.php
+++ controllers/OwnerController.php
@@ -1710,7 +1710,7 @@ class OwnerController extends Controller
 //        $redis->get('111111');
 //        var_dump($redis->set());
         return Json::encode([
-            'list' => Effect::findAll([]),
+            'list' => Effect::find()->All(),
 //           'goods'=> (new LogisticsTemplate())->find()->where(['id'=>$id])->All(),
         ]);
     }
