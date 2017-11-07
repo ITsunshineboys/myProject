152c8f9bcbc8c6deac5c2ca92e8784b01f522336
yangr <yr@qq.com>
2017-11-07 10:05:13 +0800

    1

--- controllers/ChatController.php
+++ controllers/ChatController.php
@@ -483,7 +483,7 @@ class ChatController extends Controller
         $code=1000;
         $recipient_id=(int)\Yii::$app->request->get('recipient_id');
         $recipient_role_id=(int)\Yii::$app->request->get('recipient_role_id');
-        if(!$recipient_id){
+        if(!$recipient_id || !$recipient_role_id){
             return Json::encode([
                 'code'=>$code,
                 'msg'=>\Yii::$app->params['errorCodes'][$code]
