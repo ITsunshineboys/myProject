2f7dda7e2fbbe3b0b3f04dd8962f9f0ce1d7590f
wangjie <1248136872@qq.com>
2017-11-07 18:31:42 +0800

    2017年11月3日10:04:46

--- web/admin/supplier/pages/shop_data/js/shop_data.js
+++ web/admin/supplier/pages/shop_data/js/shop_data.js
@@ -1,12 +1,6 @@
 angular.module('shop_data_module',[])
-.controller('shop_data_ctrl',function ($scope,$http) {
+.controller('shop_data_ctrl',function ($scope,$http,_ajax) {
   $scope.myng=$scope;
-  let config = {
-    headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-    transformRequest: function (data) {
-      return $.param(data)
-    }
-  };
   $scope.data_list=[];
     /*分页配置*/
     $scope.wjConfig = {
@@ -16,22 +10,6 @@ angular.module('shop_data_module',[])
         onChange: function () {
             tablePages();
         }
-    }
-    let tablePages=function () {
-        $scope.params.page=$scope.wjConfig.currentPage;//点击页数，传对应的参数
-        $http.get(baseUrl+'/mall/shop-data',{
-            params:$scope.params
-        }).then(function (res) {
-            console.log(res);
-            $scope.data_list=res.data.data.shop_data.details;
-            $scope.wjConfig.totalItems = res.data.data.shop_data.total;
-            $scope.total_sold_number=res.data.data.shop_data.total_sold_number;//销量
-            $scope.total_amount_sold=res.data.data.shop_data.total_amount_sold;//销量额
-            $scope.total_ip_number=res.data.data.shop_data.total_ip_number;//游客数
-            $scope.total_viewed_number=res.data.data.shop_data.total_viewed_number;//访问量
-        },function (err) {
-            console.log(err);
-        })
     };
     $scope.params = {
         page: 1,                        // 当前页数
@@ -39,17 +17,25 @@ angular.module('shop_data_module',[])
         start_time: '',                 // 自定义开始时间
         end_time: '',                   // 自定义结束时间
     };
+    let tablePages=function () {
+        $scope.params.page=$scope.wjConfig.currentPage;//点击页数，传对应的参数
+        _ajax.get('/mall/shop-data',$scope.params,function (res) {
+            $scope.data_list=res.data.shop_data.details;
+            $scope.wjConfig.totalItems = res.data.shop_data.total;
+            $scope.total_sold_number=res.data.shop_data.total_sold_number;//销量
+            $scope.total_amount_sold=res.data.shop_data.total_amount_sold;//销量额
+            $scope.total_ip_number=res.data.shop_data.total_ip_number;//游客数
+            $scope.total_viewed_number=res.data.shop_data.total_viewed_number;//访问量
+        });
+    };
   //时间类型
-  $http.get(baseUrl+'/site/time-types').then(function (response) {
-    $scope.time = response.data.data.time_types;
-    $scope.params.time_type = response.data.data.time_types[0].value;
+  _ajax.get('/site/time-types',{},function (res) {
+      $scope.time = res.data.time_types;
+      $scope.params.time_type = res.data.time_types[0].value;
   });
   //监听开始和结束时间
     $scope.time_change=function () {
         $scope.wjConfig.currentPage = 1; //页数跳转到第一页
         tablePages();
     };
-
-
-//    
 });
