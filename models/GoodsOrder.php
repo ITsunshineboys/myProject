54b5f6cc8c6e0cfa4792e166e9574b76893dc4f5
hyz123 <1248705443@qq.com>
2017-11-07 09:26:18 +0800

    up

--- models/GoodsOrder.php
+++ models/GoodsOrder.php
@@ -349,7 +349,7 @@ class GoodsOrder extends ActiveRecord
 
         if (!$sort_time && $sort_money==2)
         {
-            $sort='a.amount_order desc';
+            $sort="  'z.goods_price*z.goods_number' desc";
         }
         if (!$sort_time  && $sort_money==1)
         {
