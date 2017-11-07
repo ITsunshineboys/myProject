b00cd254384c4a22479bf84a3a587d3dcdc4428a
yangr <yr@qq.com>
2017-11-07 09:36:44 +0800

    edit

--- controllers/ChatController.php
+++ controllers/ChatController.php
@@ -372,7 +372,7 @@ class ChatController extends Controller
         if( $res['news']){
             $res['news']['send_time']=date('Y-m-d',$res['news']['send_time']);
         }else{
-            $res['news']='';
+            $res['news']=[];
         }
 
         $data=ChatRecord::userlog($u_id,$role_id);
