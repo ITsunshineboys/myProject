d0a0f03e37db0db82888df8582601cead4d7aeea
yangr <yr@qq.com>
2017-11-08 14:12:34 +0800

    test

--- models/UserChat.php
+++ models/UserChat.php
@@ -309,6 +309,7 @@ class UserChat extends \yii\db\ActiveRecord
                 ->one();
             $hx=User::find()->asArray()->select('hx_pwd_date,username')->where(['id'=>$uid])->one();
             $data['user']['role_id']=$role_id;
+            $data['user']['hx_pwd']=$hx['hx_pwd_date'];
             $data['user']['hx_name']=$hx['username'];
         }elseif($role_id==7){
             $data['user']=User::find()
@@ -318,6 +319,7 @@ class UserChat extends \yii\db\ActiveRecord
                 ->one();
             $hx=User::find()->asArray()->select('hx_pwd_date,username')->where(['id'=>$uid])->one();
             $data['user']['role_id']=$role_id;
+            $data['user']['hx_pwd']=$hx['hx_pwd_date'];
             $data['user']['hx_name']=$hx['username'];
         }
         $data['chat_record']=\Yii::$app->db->createCommand("SELECT * from chat_record where ((send_uid=$uid and to_uid=$recipient_id) or (send_uid=$recipient_id and to_uid=$uid)) and ((send_role_id=$recipient_role_id and to_role_id=$role_id) or (send_role_id=$role_id and to_role_id=$recipient_role_id))")->queryAll();
@@ -330,7 +332,7 @@ class UserChat extends \yii\db\ActiveRecord
                 $v['send_time']=$send_time;
             }
         }
-        if(!$data){
+        if($data){
           return null;
         }
         return $data;
