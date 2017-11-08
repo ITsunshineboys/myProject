c2e89a12cadedeb37b99adb17fa1077615e7347a
hyz123 <1248705443@qq.com>
2017-11-08 15:54:41 +0800

    up

--- controllers/OrderController.php
+++ controllers/OrderController.php
@@ -618,7 +618,7 @@ class OrderController extends Controller
 //        $result = $alipaySevice->check($post);
 //        if ($result){
             if ($post['trade_status'] == 'TRADE_SUCCESS'){
-                $arr=explode('&',urldecode($post['passback_params']));
+                $arr=urldecode($post['passback_params']);
                 $order_no=$post['out_trade_no'];
                 $order=GoodsOrder::find()
                     ->select('order_no')
