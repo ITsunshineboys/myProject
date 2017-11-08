bb9552c899b57eed29bdead9822aef7d25d221c8
wangjie <1248136872@qq.com>
2017-11-08 11:56:10 +0800

    2017年11月3日10:04:46

--- web/admin/supplier/lib/js/common_lh.js
+++ web/admin/supplier/lib/js/common_lh.js
@@ -3,18 +3,19 @@
 
 // 传参：通过url的get参数stage来获取，不传则使用默认的开发域名
 let baseUrl = (function () {
-    let stages = [
-        "http://test.cdlhzz.cn", // 开发接口域名
-        "http://v1.cdlhzz.cn" // 展示接口域名
-    ];
-    let stage = 0;
-    try {
-        let stageParam = location.search.split("&stage=")[1].split("&")[0];
-        if (stages[stageParam])  {
-            stage = stageParam;
-        }
-    } catch (e) {}
-    return stages[stage];
+    // let stages = [
+    //     "http://test.cdlhzz.cn", // 开发接口域名
+    //     "http://v1.cdlhzz.cn" // 展示接口域名
+    // ];
+    // let stage = 0;
+    // try {
+    //     let stageParam = location.search.split("&stage=")[1].split("&")[0];
+    //     if (stages[stageParam])  {
+    //         stage = stageParam;
+    //     }
+    // } catch (e) {}
+    // return stages[stage];
+    return location.origin;
 })();
 
 /**
