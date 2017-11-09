8422a321b841b53fc1ac7718a9b3fe5d6fafe585
Piz <q564146734@163.com>
2017-11-09 15:12:26 +0800

    微信相关

--- web/owner/mall/js/controller.js
+++ web/owner/mall/js/controller.js
@@ -1,23 +1,4 @@
 angular.module('all_controller',[])
-    .controller("indexCtrl", ["$rootScope", "$scope", "$http", "_ajax", function ($rootScope, $scope, _ajax) {
-        // 微信事宜
-        $rootScope.isWxOpen = false;
-        _ajax.get(baseUrl + '/order/iswxlogin', "", function (res) {
-            if (res.code === 200) {
-                $rootScope.isWxOpen = true;
-                if (getUrlParams('code') === "" && sessionStorage.getItem("code") === null) {
-                    let url = location.href;
-                    _ajax.post(baseUrl + '/order/find-open-id', {url: url}, function (res) {
-                        location.href = res.data
-                    })
-                } else {
-                    $rootScope.isWxOpen = false;
-                    let code = getUrlParams('code');
-                    sessionStorage.setItem('code', code);
-                }
-            }
-        });
-    }])
     .controller("intelligent_index_ctrl", function ($scope, $http, _ajax) {//主页控制器
         /*主页操作*/
         sessionStorage.clear();
