67e1682451be1fcf33e952d3bdc102894afa12cf
Piz <q564146734@163.com>
2017-11-09 16:18:39 +0800

    微信相关

--- web/owner/mall/js/index.js
+++ web/owner/mall/js/index.js
@@ -2,7 +2,16 @@ app.controller("indexCtrl", ["$rootScope", "$scope", "_ajax", function ($rootSco
     // 微信事宜
     $rootScope.isWxOpen = false;
     _ajax.get(baseUrl + '/order/iswxlogin', "", function (res) {
-        if (res.code === 200) {
+        if (res.code === 200) { // 是微信浏览器打开
+            let data = res.data;
+            let wxSharConfig = {
+                appId: data.appId,
+                timestamp: data.timestamp,
+                nonceStr: data.nonceStr,
+                signature: data.signature
+            };
+            sessionStorage.setItem('wxSharConfig', JSON.stringify(wxSharConfig));
+
             $rootScope.isWxOpen = true;
             if (getUrlParams('code') === "" && sessionStorage.getItem("code") === null) {
                 let url = location.href;
@@ -12,6 +21,9 @@ app.controller("indexCtrl", ["$rootScope", "$scope", "_ajax", function ($rootSco
             } else {
                 let code = getUrlParams('code');
                 sessionStorage.setItem('code', code);
+                _ajax.post(baseUrl + '/order/get-open-id', {code: code}, function (res) {
+                    console.log(res);
+                })
             }
         }
     });
