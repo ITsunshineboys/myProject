1893dfc5753d1b033eed404728b93ca182e1802e
Piz <q564146734@163.com>
2017-11-09 16:50:09 +0800

    微信相关

--- web/owner/mall/js/deposit.js
+++ web/owner/mall/js/deposit.js
@@ -25,7 +25,32 @@ app.controller('depositCtrl', ['$rootScope', '$scope', '$state', '_ajax', functi
         if ($rootScope.isWxOpen) {
             payParams.wxpayCode = sessionStorage.getItem('openId');
             _ajax.post('/order/wxpay-effect-earnst-sub', payParams, function (res) {
-                console.log(res);
+                callpay();
+
+                //调用微信JS api 支付
+                function jsApiCall() {
+                    WeixinJSBridge.invoke(
+                        'getBrandWCPayRequest',
+                        res.data,
+                        function (res) {
+                            WeixinJSBridge.log(res.err_msg);
+                            alert(res.err_code + res.err_desc + res.err_msg);
+                        }
+                    );
+                }
+
+                function callpay() {
+                    if (typeof WeixinJSBridge == "undefined") {
+                        if (document.addEventListener) {
+                            document.addEventListener('WeixinJSBridgeReady', jsApiCall, false);
+                        } else if (document.attachEvent) {
+                            document.attachEvent('WeixinJSBridgeReady', jsApiCall);
+                            document.attachEvent('onWeixinJSBridgeReady', jsApiCall);
+                        }
+                    } else {
+                        jsApiCall();
+                    }
+                }
             })
         } else {
             _ajax.post('/order/effect-earnst-alipay-sub', payParams, function (res) {
