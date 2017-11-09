4ca5c6bae8205ee0c3b98675e38aa521d719cd14
Piz <q564146734@163.com>
2017-11-09 17:30:44 +0800

    微信相关

--- web/owner/mall/js/deposit.js
+++ web/owner/mall/js/deposit.js
@@ -24,32 +24,33 @@ app.controller('depositCtrl', ['$rootScope', '$scope', '$state', '_ajax', functi
         // $state.go('pay_success');
         if ($rootScope.isWxOpen) {
             payParams.wxpayCode = sessionStorage.getItem('openId');
+            let wxConfig = JSON.parse(sessionStorage.getItem('wxConfig'));
             _ajax.post('/order/wxpay-effect-earnst-sub', payParams, function (res) {
-                callpay();
-
-                //调用微信JS api 支付
-                function jsApiCall() {
+                function onBridgeReady(){
                     WeixinJSBridge.invoke(
-                        'getBrandWCPayRequest',
-                        res.data,
-                        function (res) {
-                            WeixinJSBridge.log(res.err_msg);
-                            alert(res.err_code + res.err_desc + res.err_msg);
+                        'getBrandWCPayRequest', {
+                            "appId": wxConfig.appId,     //公众号名称，由商户传入
+                            "timeStamp": wxConfig.timestamp,         //时间戳，自1970年以来的秒数
+                            "nonceStr": wxConfig.nonceStr, //随机串
+                            "package":"prepay_id=u802345jgfjsdfgsdg888",
+                            "signType":"MD5",         //微信签名方式：
+                            "paySign":"70EA570631E4BB79628FBCA90534C63FF7FADD89" //微信签名
+                        },
+                        function(res){
+                            if(res.err_msg == "get_brand_wcpay_request:ok" ) {}     // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。
                         }
                     );
                 }
 
-                function callpay() {
-                    if (typeof WeixinJSBridge == "undefined") {
-                        if (document.addEventListener) {
-                            document.addEventListener('WeixinJSBridgeReady', jsApiCall, false);
-                        } else if (document.attachEvent) {
-                            document.attachEvent('WeixinJSBridgeReady', jsApiCall);
-                            document.attachEvent('onWeixinJSBridgeReady', jsApiCall);
-                        }
-                    } else {
-                        jsApiCall();
+                if (typeof WeixinJSBridge == "undefined"){
+                    if( document.addEventListener ){
+                        document.addEventListener('WeixinJSBridgeReady', onBridgeReady, false);
+                    }else if (document.attachEvent){
+                        document.attachEvent('WeixinJSBridgeReady', onBridgeReady);
+                        document.attachEvent('onWeixinJSBridgeReady', onBridgeReady);
                     }
+                }else{
+                    onBridgeReady();
                 }
             })
         } else {
