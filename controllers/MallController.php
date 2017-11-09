3585dfdbfd55325e69cf67436cab3c9ee047478f
hj <1534120192@qq.com>
2017-11-09 14:12:34 +0800

    test

--- controllers/MallController.php
+++ controllers/MallController.php
@@ -603,7 +603,7 @@ class MallController extends Controller
 
         $recommend->delete_time = time();
         if (!$recommend->save()) {
-            StringService::writeLog(GoodsRecommend::tableName(), $recommend->id);
+            StringService::writeLog(GoodsRecommend::tableName(), json_encode($recommend->errors));
             $code = 500;
             return Json::encode([
                 'code' => $code,
