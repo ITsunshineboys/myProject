e92aeea3bb192b864463d554bfc207f14c8aefd3
hj <1534120192@qq.com>
2017-11-09 14:48:10 +0800

    revert

--- web/index.php
+++ web/index.php
@@ -9,18 +9,9 @@ define('YII_ENV', $config['params']['online']['env']);
 require(__DIR__ . '/../vendor/autoload.php');
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
- if (YII_DEBUG) {
- 	$origin = isset($_SERVER['HTTP_ORIGIN'])? $_SERVER['HTTP_ORIGIN'] : '';
-
-	$allow_origin = array(
-	    'http://http://cross.local.com:808',
-	    'http://http://cross.local.com'
-	);
-
-	if(in_array($origin, $allow_origin)){
-	    header('Access-Control-Allow-Origin:'.$origin);
-	}
-     header("Access-Control-Allow-Credentials: true");
- }
+if (YII_DEBUG) {
+    header('Access-Control-Allow-Origin: http://cross.local.com:808');
+    header("Access-Control-Allow-Credentials: true");
+}
 
 (new yii\web\Application($config))->run();
