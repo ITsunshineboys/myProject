2f5646bb802e57a86f171051be05a4e235ae04eb
Piz <q564146734@163.com>
2017-11-09 17:29:32 +0800

    微信相关

--- web/owner/mall/js/index.js
+++ web/owner/mall/js/index.js
@@ -8,13 +8,21 @@ app.controller("indexCtrl", ["$rootScope", "$scope", "_ajax", function ($rootSco
     _ajax.get(baseUrl + '/order/iswxlogin', "", function (res) {
         if (res.code === 200) { // 是微信浏览器打开
             let data = res.data;
-            let wxSharConfig = {
+            let wxConfig = {
                 appId: data.appId,
                 timestamp: data.timestamp,
                 nonceStr: data.nonceStr,
                 signature: data.signature
             };
             sessionStorage.setItem('wxSharConfig', JSON.stringify(wxSharConfig));
+            wx.config({
+                debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+                appId: data.appId, // 必填，公众号的唯一标识
+                timestamp: data.timestamp, // 必填，生成签名的时间戳
+                nonceStr: data.nonceStr, // 必填，生成签名的随机串
+                signature: data.signature,// 必填，签名，见附录1
+                jsApiList: ["onMenuShareTimeline", "onMenuShareAppMessage", "chooseWXPay"] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+            });
 
             $rootScope.isWxOpen = true;
 
@@ -27,7 +35,7 @@ app.controller("indexCtrl", ["$rootScope", "$scope", "_ajax", function ($rootSco
                 } else {
                     let code = getUrlParams('code');
                     _ajax.post(baseUrl + '/order/get-open-id', {code: code}, function (res) {
-                        let openId = res.data.openid;
+                        let openId = res.data;
                         sessionStorage.setItem('openId', openId);
                     })
                 }
