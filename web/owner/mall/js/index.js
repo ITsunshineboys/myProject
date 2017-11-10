0d29fa9893d9d8d5cf5ba3957fab4c60694faa8d
Piz <q564146734@163.com>
2017-11-10 17:24:32 +0800

    WX分享

--- web/owner/mall/js/index.js
+++ web/owner/mall/js/index.js
@@ -25,37 +25,70 @@ app.controller("indexCtrl", ["$rootScope", "$scope", "_ajax", function ($rootSco
                 alert(location.hash);
                 // 分享到朋友圈
                 wx.onMenuShareTimeline({
-                    title: 'Demo', // 分享标题
-                    link: location.protocol + '//' + location.host + location.pathname + location.hash, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+                    title: '', // 分享标题
+                    link: window.location.host + window.location.pathname + window.location.hash, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
                     imgUrl: '', // 分享图标
+                    success: function () {
+                        // 用户确认分享后执行的回调函数
+                    },
+                    cancel: function () {
+                        // 用户取消分享后执行的回调函数
+                    }
                 });
                 // 分享给朋友
                 wx.onMenuShareAppMessage({
-                    title: 'Demo', // 分享标题
-                    desc: "", // 分享描述
-                    link: baseUrl + location.pathname + location.hash, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-                    imgUrl: '' // 分享图标
+                    title: '', // 分享标题
+                    desc: '', // 分享描述
+                    link: window.location.host + window.location.pathname + window.location.hash, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+                    imgUrl: '', // 分享图标
+                    type: '', // 分享类型,music、video或link，不填默认为link
+                    dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
+                    success: function () {
+                        // 用户确认分享后执行的回调函数
+                        alert(window.location.host + window.location.pathname + window.location.hash)
+                    },
+                    cancel: function () {
+                        // 用户取消分享后执行的回调函数
+                    }
                 });
                 // 分享到QQ
                 wx.onMenuShareQQ({
                     title: '', // 分享标题
                     desc: '', // 分享描述
-                    link: location.protocol + '//' + location.host + location.pathname + location.hash, // 分享链接
-                    imgUrl: '' // 分享图标
+                    link: '', // 分享链接
+                    imgUrl: '', // 分享图标
+                    success: function () {
+                        // 用户确认分享后执行的回调函数
+                    },
+                    cancel: function () {
+                        // 用户取消分享后执行的回调函数
+                    }
                 });
                 // 腾讯微博
                 wx.onMenuShareWeibo({
                     title: '', // 分享标题
                     desc: '', // 分享描述
-                    link: location.protocol + '//' + location.host + location.pathname + location.hash, // 分享链接
-                    imgUrl: '' // 分享图标
+                    link: '', // 分享链接
+                    imgUrl: '', // 分享图标
+                    success: function () {
+                        // 用户确认分享后执行的回调函数
+                    },
+                    cancel: function () {
+                        // 用户取消分享后执行的回调函数
+                    }
                 });
                 // 分享到QQ空间
                 wx.onMenuShareQZone({
                     title: '', // 分享标题
                     desc: '', // 分享描述
-                    link: location.protocol + '//' + location.host + location.pathname + location.hash, // 分享链接
-                    imgUrl: '' // 分享图标
+                    link: '', // 分享链接
+                    imgUrl: '', // 分享图标
+                    success: function () {
+                        // 用户确认分享后执行的回调函数
+                    },
+                    cancel: function () {
+                        // 用户取消分享后执行的回调函数
+                    }
                 });
             });
             $rootScope.isWxOpen = true;
