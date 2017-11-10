253ecb32e378b4da6d88693a42da49d3dbba9631
wangjie <1248136872@qq.com>
2017-11-10 14:47:27 +0800

    up

--- web/admin/lhzz_line/lib/js/login.js
+++ web/admin/lhzz_line/lib/js/login.js
@@ -1,101 +1,40 @@
-// var role="site/all-roles";
-// var logout="site/admin-login";
-// app.controller("admin_login",function($scope,$http){
-//   var num= /^[0-9]*$/;
-//   //获取角色列表
-//   $http.post(url+role)
-//     .success(function(data){
-//       $scope.allrole=data.data.roles;
-//     })
-//   $scope.name1=function(){
-//     $scope.name=$("input[name='name1']").val();
-//     $scope.psw=$("input[name='psw']").val();
-//     console.log("name==="+ $scope.name.length)
-//     if(!num.test( $scope.name)||$scope.name.length<4){
-//       $(".login").css({"background":"rgba(236, 184, 75, 0.44)"})
-//       $(".warm").text("请输入正确的纯数字的手机号/魔方号!");
-//     }
-//     else {
-//       $(".warm").text("");
-//       if($scope.psw.length>25||$scope.psw.length<6){
-//         $(".login").css({"background":"rgba(236, 184, 75, 1)"})
-//       }
-//     }
-//   };
-//   $scope.psw1=function(){
-//     $scope.psw=$("input[name='psw']").val();
-//     $scope.name=$("input[name='name1']").val();
-//     console.log("psw==="+ $scope.psw)
-//     console.log("psw22222==="+ $("input[name='psw']").val())
-//     console.log("psw_length==="+ $scope.psw.length);
-//     if($scope.psw.length>25||$scope.psw.length<4){
-//       $(".warm").text("请输入6-25位的密码！");
-//       $(".login").css({"background":"rgba(236, 184, 75, 0.44)"})
-//     }
-//     else{
-//       $(".warm").text("");
-//       if(num.test( $scope.name)){
-//         $(".login").css({"background":"rgba(236, 184, 75, 1)"})
-//       }
-//     }
-//   };
-//
-//   $scope.$on('ngRepeatFinished', function (data) { //接收广播，一旦repeat结束就会执行
-//     $scope.login=function(){
-//       $scope.name=$("input[name='name1']").val();
-//       $scope.psw=$("input[name='psw']").val();
-//       $scope.myrole=$('#select1').val();
-//       if(num.test( $scope.name)&&$scope.name.length>=4&&($scope.psw.length<=25||$scope.psw.length>=4)){
-//         $.ajax({
-//           url: url+logout,
-//           type: 'POST',
-//           data:{"role_id":$scope.myrole,"username":$scope.name,"password":$scope.psw},
-//           dataType: "json",
-//           contentType:"application/x-www-form-urlencoded;charset=UTF-8",
-//           success: function (data) {
-//             $scope.loginout=data;
-//             if($scope.loginout.code==200){
-//               window.location.href=$scope.loginout.data.toUrl;
-//               $(".warm").text($scope.loginout.msg);
-//             }
-//             else{
-//               $(".warm").text($scope.loginout.msg);
-//             }
-//           }
-//         });
-//       }
-//     }
-//   });
-// });
-// app.controller('itemReaptCtrl', ['$scope', function ($scope) {
-//   $scope.$watch($scope.$last, function () {
-//     if($scope.$last){   //$scope.$last是来判断是否是最后一个ng-repeat对象， 如果是则$scope.$last的值为true ,反之则为false
-//       setTimeout(function(){$scope.$emit('ngRepeatFinished')},1); // 由于是向父控制器中发布广播，所有用$emit
-//     }
-//   })
-// }]);
-
-
-let app=angular.module("app",[]);
-app.controller("admin_login",function($scope,$http){
-    $scope.login=function () {
-      let url=baseUrl+"/site/admin-login";
-      let params={
-        role_id:1,
-        username:13551201821,
-        password:"demo123"
-      };
-      let config={
+;
+let app1= angular.module("app",[]);
+app1.controller('login_ctrl',function ($scope,$http,$document) {
+    let baseUrl = (function () {
+        return 'http://test.cdlhzz.cn'
+    })();
+    let config = {
         headers: {'Content-Type': 'application/x-www-form-urlencoded'},
         transformRequest: function (data) {
-          return $.param(data)
+            return $.param(data)
         }
-      };
-      $http.post(url,params,config).then(function (res) {
-        console.log(res);
-        window.location.href="http://cross.local.com:808/web/admin/lhzz_line"
-      },function (error) {
-        console.log(error)
-      })
-    }
-})
+    };
+    //Enter 键盘事件
+    $document.bind("keypress", function(event) {
+        $scope.$apply(function (){
+            if(event.keyCode == 13){
+                $scope.login();
+            }
+        })
+    });
+
+    $scope.error_flag=false;
+    $scope.login=function () {
+      $http.post(baseUrl+'/site/admin-login', {
+          role_id: 1,
+          username: $scope.my_username,
+          password: $scope.my_password
+      }, config).then(function (res) {
+          console.log(res);
+          if (res.data.code == 200) {
+              window.location.href="index.html"
+          } else {
+              $scope.error_flag = true;
+              $scope.error_txt='*用户名或密码不正确'
+          }
+      }, function (error) {
+          console.log(error)
+      });
+  }
+});
