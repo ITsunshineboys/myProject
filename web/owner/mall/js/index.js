917a411df843e43a7174ffa318a7cdc65e70ab35
Piz <q564146734@163.com>
2017-11-10 18:06:23 +0800

    WX分享

--- web/owner/mall/js/index.js
+++ web/owner/mall/js/index.js
@@ -1,4 +1,10 @@
 app.controller("indexCtrl", ["$rootScope", "$scope", "_ajax", function ($rootScope, $scope, _ajax) {
+    $rootScope.$on("$stateChangeStart", function () {
+        console.log('页面被切换了');
+        wx.ready(function () {
+            wxConfig()
+        });
+    });
     // 微信事宜
     $rootScope.isWxOpen = false;
     _ajax.get('/order/iswxlogin', "", function (res) {
@@ -11,74 +17,6 @@ app.controller("indexCtrl", ["$rootScope", "$scope", "_ajax", function ($rootSco
                 nonceStr: data.nonceStr, // 必填，生成签名的随机串
                 signature: data.signature,// 必填，签名，见附录1
                 jsApiList: ["onMenuShareTimeline", "onMenuShareAppMessage", "onMenuShareQQ", "onMenuShareWeibo", "onMenuShareQZone", "chooseWXPay"] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
-            });
-            wx.ready(function () {
-                // 分享到朋友圈
-                wx.onMenuShareTimeline({
-                    title: '', // 分享标题
-                    link: window.location.protocol + '//' + window.location.host + window.location.pathname + window.location.hash, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-                    imgUrl: '', // 分享图标
-                    success: function () {
-                        // 用户确认分享后执行的回调函数
-                    },
-                    cancel: function () {
-                        // 用户取消分享后执行的回调函数
-                    }
-                });
-                // 分享给朋友
-                wx.onMenuShareAppMessage({
-                    title: 'Demo', // 分享标题
-                    desc: '', // 分享描述
-                    link: window.location.protocol + '//' + window.location.host + window.location.pathname + window.location.hash, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-                    imgUrl: '', // 分享图标
-                    type: '', // 分享类型,music、video或link，不填默认为link
-                    dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
-                    success: function () {
-                        // 用户确认分享后执行的回调函数
-                    },
-                    cancel: function () {
-                        // 用户取消分享后执行的回调函数
-                    }
-                });
-                // 分享到QQ
-                wx.onMenuShareQQ({
-                    title: '', // 分享标题
-                    desc: '', // 分享描述
-                    link: window.location.protocol + '//' + window.location.host + window.location.pathname + window.location.hash, // 分享链接
-                    imgUrl: '', // 分享图标
-                    success: function () {
-                        // 用户确认分享后执行的回调函数
-                    },
-                    cancel: function () {
-                        // 用户取消分享后执行的回调函数
-                    }
-                });
-                // 腾讯微博
-                wx.onMenuShareWeibo({
-                    title: '', // 分享标题
-                    desc: '', // 分享描述
-                    link: window.location.protocol + '//' + window.location.host + window.location.pathname + window.location.hash, // 分享链接
-                    imgUrl: '', // 分享图标
-                    success: function () {
-                        // 用户确认分享后执行的回调函数
-                    },
-                    cancel: function () {
-                        // 用户取消分享后执行的回调函数
-                    }
-                });
-                // 分享到QQ空间
-                wx.onMenuShareQZone({
-                    title: '', // 分享标题
-                    desc: '', // 分享描述
-                    link: window.location.protocol + '//' + window.location.host + window.location.pathname + window.location.hash, // 分享链接
-                    imgUrl: '', // 分享图标
-                    success: function () {
-                        // 用户确认分享后执行的回调函数
-                    },
-                    cancel: function () {
-                        // 用户取消分享后执行的回调函数
-                    }
-                });
             });
             $rootScope.isWxOpen = true;
             if (sessionStorage.getItem("openId") === null) {
