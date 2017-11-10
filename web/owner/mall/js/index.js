bd7ab4e8439b0bbad955ea651da81b016bd3fcc7
Piz <q564146734@163.com>
2017-11-10 17:14:58 +0800

    WX分享

--- web/owner/mall/js/index.js
+++ web/owner/mall/js/index.js
@@ -12,7 +12,7 @@ app.controller("indexCtrl", ["$rootScope", "$scope", "_ajax", function ($rootSco
             };
             sessionStorage.setItem('wxConfig', JSON.stringify(wxConfig));
             wx.config({
-                debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+                debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
                 appId: data.appId, // 必填，公众号的唯一标识
                 timestamp: data.timestamp, // 必填，生成签名的时间戳
                 nonceStr: data.nonceStr, // 必填，生成签名的随机串
@@ -30,7 +30,7 @@ app.controller("indexCtrl", ["$rootScope", "$scope", "_ajax", function ($rootSco
                 wx.onMenuShareAppMessage({
                     title: 'Demo', // 分享标题
                     desc: "", // 分享描述
-                    link: location.protocol + '//' + location.host + location.pathname + location.hash, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+                    link: baseUrl + location.pathname + location.hash, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
                     imgUrl: '' // 分享图标
                 });
                 // 分享到QQ
