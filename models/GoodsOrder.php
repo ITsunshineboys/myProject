f17fb640f503cdabb8f628ee2322fb01ee6819a5
hyz123 <1248705443@qq.com>
2017-11-11 15:59:04 +0800

    up

--- models/GoodsOrder.php
+++ models/GoodsOrder.php
@@ -2330,8 +2330,7 @@ class GoodsOrder extends ActiveRecord
                 $GoodsOrder[$k]['comment_grade']='';
                 $GoodsOrder[$k]['handle']='';
                 $sup=Supplier::find()->where(['id'=>$GoodsOrder[$k]['supplier_id']])->one();
-                var_dump($sup->toArray());die;
-                $GoodsOrder[$k]['shop_name']=$sup->shop_name;
+                $GoodsOrder[$k]['shop_name']=$sup->toArray()['shop_name'];
                  if ($role=='user')
                 {
                     $GoodsOrder[$k]['uid']=$sup->uid;
