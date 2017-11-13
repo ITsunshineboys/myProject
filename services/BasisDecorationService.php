3f1f4c951c1f4f8bcf9f7d0731069325eca63da6
Wch <362224550@qq.com>
2017-11-13 16:21:41 +0800

    1

--- services/BasisDecorationService.php
+++ services/BasisDecorationService.php
@@ -188,7 +188,6 @@ class BasisDecorationService
             }
         }
         $ids = GoodsAttr::findByGoodsIdUnit($goods_id);
-        var_dump($ids);exit;
         if ($ids == null){
             $code = 1061;
             return Json::encode([
@@ -206,7 +205,7 @@ class BasisDecorationService
                     break;
             }
         }
-
+var_dump($goods_value);exit;
         //线路个数计算 ,线路费用计算
         $electricity['wire_quantity'] = ceil($points * $material / $goods_value);
         $electricity['wire_cost'] = $electricity['wire_quantity'] * $goods_price;
