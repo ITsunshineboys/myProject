c3aa0714d60ea673d6cb6221b389e2bdd715abc1
hj <1534120192@qq.com>
2017-11-13 17:39:20 +0800

    revert

--- web/index.php
+++ web/index.php
@@ -10,8 +10,8 @@ require(__DIR__ . '/../vendor/autoload.php');
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
 if (YII_DEBUG) {
-header('Access-Control-Allow-Origin: http://cross.local.com:808');
-header("Access-Control-Allow-Credentials: true");
+    header('Access-Control-Allow-Origin: http://cross.local.com:808');
+    header("Access-Control-Allow-Credentials: true");
 }
 
 (new yii\web\Application($config))->run();
