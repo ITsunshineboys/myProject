4fc6a18f8befb4b01abd012492fef3856da2b01a
Wch <362224550@qq.com>
2017-11-13 16:31:30 +0800

    1

--- controllers/OwnerController.php
+++ controllers/OwnerController.php
@@ -304,6 +304,7 @@ class OwnerController extends Controller
         //查询弱电所需要材料
         $goods_select ='goods.id,goods.category_id,goods.platform_price,goods.supplier_price,goods.purchase_price_decoration_company,goods_brand.name,gc.title,logistics_district.district_name,goods.series_id,goods.style_id,goods.subtitle,goods.profit_rate,gc.path,goods.cover_image,supplier.shop_name';
         $goods = Goods::priceDetail(self::WALL_SPACE, self::WEAK_MATERIAL,$goods_select);
+        var_dump($goods);exit;
         if ($goods == null){
             $code = 1061;
             return Json::encode([
