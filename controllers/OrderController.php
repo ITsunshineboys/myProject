123c7962c69dd8248e7a27a334aef79fb6d45579
hyz123 <1248705443@qq.com>
2017-11-15 16:52:46 +0800

    up

--- controllers/OrderController.php
+++ controllers/OrderController.php
@@ -798,6 +798,7 @@ class OrderController extends Controller
                 {
                     if (!$address)
                     {
+                        var_dump( var_dump($orders));exit;
                         $code=1000;
                         return Json::encode([
                             'code' => $code,
