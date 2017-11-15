e8c354c16500fe10877a7c9f146b4dd4496e52f4
yangr <yr@qq.com>
2017-11-15 10:55:25 +0800

    test

--- models/Effect.php
+++ models/Effect.php
@@ -266,10 +266,10 @@ class Effect extends ActiveRecord
 
         }
 
+        $material_grop=self::array_group_by($material,'first_cate_id');
 
+        $data['material']=$material_grop;
         return [$data,$material];
-
-
 
     }
 
