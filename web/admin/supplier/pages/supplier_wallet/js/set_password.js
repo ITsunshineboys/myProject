6ff6ec5628b93172786f6e5b6317cd795593df10
wangjie <1248136872@qq.com>
2017-11-15 16:22:25 +0800

    up

--- web/admin/supplier/pages/supplier_wallet/js/set_password.js
+++ web/admin/supplier/pages/supplier_wallet/js/set_password.js
@@ -1,6 +1,7 @@
 angular.module('set_password_module',[])
 .controller('set_password_ctrl',function ($scope,$http,$state,$stateParams,$timeout,$location,$anchorScroll,$window,_ajax) {
   $scope.myng=$scope;
+  let reg=/^\d{6}$/;
   //判断 first or unfirst
   _ajax.post('/withdrawals/check-isset-pay-pwd',{},function (res) {
       console.log(res);
@@ -13,7 +14,8 @@ angular.module('set_password_module',[])
       }else{
           $scope.show_first=true;
       }
-  })
+  });
+
   //获取验证码
   $scope.Countdown=60;
   $scope.show_send=true;//发送按钮、倒计时
@@ -28,47 +30,67 @@ angular.module('set_password_module',[])
         //在这里去手动触发脏检查
         if($scope.Countdown<0){
           $scope.show_send=true;
+          $scope.Countdown=60;
           clearInterval(count_down);
         }
       });
     },1000);
     _ajax.post('/withdrawals/send-pay-code',{},function (res) {
         console.log(res);
-    })
+    });
   };
   $scope.confirm_btn=function (valid,error) {
     if($scope.show_first==true){
-      if(valid && $scope.new_pw==$scope.again_pw && !$scope.pw_flag){
-        $('#save_modal').modal('show');
-        _ajax.post('/withdrawals/set-pay-pwd',{
-            key:$scope.pw_key,
-            pay_pwd_first:$scope.new_pw,
-            pay_pwd_secend:$scope.again_pw
-        },function (res) {
-
-        })
+      //判断两次密码是否一致
+      $scope.new_pw!=$scope.again_pw?$scope.pw_flag=true:$scope.pw_flag=false;
+      if(valid){
+        let new_pw_f=reg.test($scope.new_pw);
+        let again_pw_f=reg.test($scope.again_pw);
+        if(new_pw_f&&again_pw_f){
+            if($scope.new_pw==$scope.again_pw && !$scope.pw_flag){
+                _ajax.post('/withdrawals/set-pay-pwd',{
+                    key:$scope.pw_key,
+                    pay_pwd_first:$scope.new_pw,
+                    pay_pwd_secend:$scope.again_pw
+                },function (res) {
+                    if(res.code==200){
+                        $('#save_modal').modal('show');
+                    }
+                });
+            }else{
+                $scope.pw_two_txt='第二次与第一次密码不一致，请重新填写';
+                $scope.pw_two_flag=true;
+            }
+        }else{
+            $scope.pw_flag=true;
+            $scope.pw_two_txt='请输入6位整数数字';
+        }
       }else{
         $scope.submitted=true;
       }
-      //判断两次密码是否一致
-      $scope.new_pw!=$scope.again_pw?$scope.pw_flag=true:$scope.pw_flag=false;
     }else{
       if(valid){
-        _ajax.post('/withdrawals/set-pay-pwd',{
-            key:$scope.pw_key,
-            pay_pwd:$scope.new_pw,
-            sms_code:$scope.v_code
-        },function (res) {
-            console.log(res);
-            if(res.code == 200){
-                $('#save_modal').modal('show');
-                $scope.show_prompt=false;
-            }else if(res.code == 1002){
-                $scope.show_prompt=true;
-            }else if(res.code == 403){
-                $state.go('login')
-            }
-        })
+          let pw_reg=reg.test($scope.new_pw);
+          if(pw_reg){
+              _ajax.post('/withdrawals/set-pay-pwd',{
+                  key:$scope.pw_key,
+                  pay_pwd:$scope.new_pw,
+                  sms_code:$scope.v_code
+              },function (res) {
+                  console.log(res);
+                  if(res.code == 200){
+                      $('#save_modal').modal('show');
+                      $scope.v_code_flag=false;
+                  }else if(res.code == 1002){
+                      $scope.pw_flag=false;
+                      $scope.v_code_flag=true;
+                      $scope.unfirst_txt='验证码错误，请重新输入';
+                  }
+              })
+          }else{
+              $scope.pw_flag=true;
+              $scope.unfirst_txt='请输入6位整数数字';
+          }
       }else{
         $scope.submitted=true;
       }
