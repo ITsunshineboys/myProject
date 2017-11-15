73f9e454db6fb9f1f5d0f76a1b99f5f2b43b86ae
yangr <yr@qq.com>
2017-11-15 15:39:48 +0800

    test

--- models/SupplierCashManager.php
+++ models/SupplierCashManager.php
@@ -275,7 +275,7 @@ class SupplierCashManager extends ActiveRecord
 
         list($time_start, $time_end) = ModelService::timeDeal($time_type, $time_start, $time_end);
         if ($time_start && $time_end && $time_end >= $time_start) {
-
+            var_dump($time_start,$time_end);die;
             $query->andWhere(['between', 'g.paytime', $time_start, $time_end]);
         } elseif ($time_start) {
             $query->andWhere(['>=', 'g.paytime', $time_start]);
