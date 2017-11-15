a0df1d56f202ffc98359c432c879d59465b00d67
yangr <yr@qq.com>
2017-11-15 10:52:29 +0800

    test

--- models/Effect.php
+++ models/Effect.php
@@ -265,13 +265,9 @@ class Effect extends ActiveRecord
                 ->asArray()->one()['title'];
 
         }
-        var_dump([$data,$material]);die;
-        $material_grop=self::array_group_by($material,'first_cate_id');
-
-        $data['material']=$material_grop;
 
 
-        return $data;
+        return [$data,$material];
 
 
 
