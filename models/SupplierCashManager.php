ef007565721f9dabe54329f46291e489132a9cfb
yangr <yr@qq.com>
2017-11-16 11:58:08 +0800

    test

--- models/SupplierCashManager.php
+++ models/SupplierCashManager.php
@@ -344,8 +344,7 @@ class SupplierCashManager extends ActiveRecord
             }
             if($time_start==$time_end){
                 list($time_start,$time_end)=ModelService::timeDeal($time_start,$time_end);
-                $time_start = (int)strtotime($time_start);
-                $time_end = (int)strtotime($time_end);
+
             }
         } else {
             list($time_start, $time_end) = StringService::startEndDate($time_type);
