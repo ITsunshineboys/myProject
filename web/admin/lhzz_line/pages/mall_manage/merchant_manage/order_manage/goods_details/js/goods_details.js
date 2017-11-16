4ba97cc7435c4ea8439578ac5f208365e4045085
Piz <q564146734@163.com>
2017-11-16 18:10:04 +0800

    面包屑调整

--- web/admin/lhzz_line/pages/mall_manage/merchant_manage/order_manage/goods_details/js/goods_details.js
+++ web/admin/lhzz_line/pages/mall_manage/merchant_manage/order_manage/goods_details/js/goods_details.js
@@ -1,5 +1,21 @@
-app.controller('order_goods', ['$scope', '$stateParams', '_ajax', function ($scope, $stateParams, _ajax) {
-    $scope.shopID = sessionStorage.getItem('shopID');
+app.controller('order_goods', ['$rootScope', '$scope', '$stateParams', '_ajax', function ($rootScope, $scope, $stateParams, _ajax) {
+    console.log($rootScope);
+    $rootScope.crumbs = [{
+        name: '商城管理',
+        icon: 'icon-shangchengguanli',
+        link: 'merchant_index'
+    }, {
+        name: '商家管理',
+        link: 'store_mag'
+    }, {
+        name: '订单管理',
+        link: -1
+    }, {
+        name: '订单详情',
+        link: -1
+    }, {
+        name: '商品详情'
+    }];
     let params = {
         order_no: $stateParams.orderNo,
         sku: $stateParams.sku
