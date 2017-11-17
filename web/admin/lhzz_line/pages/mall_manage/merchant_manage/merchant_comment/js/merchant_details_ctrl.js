f6eb538b5160352f7626da25dfbace16b5cbc40d
tiger <tiger@163.com>
2017-11-17 17:26:05 +0800

    大后台商城管理-商家管理-属性管理 面包屑配置

--- web/admin/lhzz_line/pages/mall_manage/merchant_manage/merchant_comment/js/merchant_details_ctrl.js
+++ web/admin/lhzz_line/pages/mall_manage/merchant_manage/merchant_comment/js/merchant_details_ctrl.js
@@ -2,7 +2,22 @@
  * Created by Administrator on 2017/8/9 0009.
  */
 var merchant_details = angular.module("merchant_details", [])
-    .controller("merchant_details_ctrl", function ($scope, $http, $stateParams) {
+    .controller("merchant_details_ctrl", function ($rootScope,$scope, $http, $stateParams) {
+        $rootScope.crumbs = [{
+            name: '商城管理',
+            icon: 'icon-shangchengguanli',
+            link: 'merchant_index'
+        }, {
+            name: '商家管理',
+            link:'store_mag',
+        },{
+            name:'商家详情',
+            link: 'store_detail',
+            params:{authorize_flag:true,store:$stateParams.store}
+        },{
+            name:'品牌授权详情'
+        }];
+
         $scope.store =  $stateParams.store;
         $scope.authorizedata = $stateParams.itemdetail;
     })
