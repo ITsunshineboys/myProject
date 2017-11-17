ed5fd3e8df86f66acca57e96916e409687c170e5
hj <1534120192@qq.com>
2017-11-17 16:16:37 +0800

    auth

--- config/auth.php
+++ config/auth.php
@@ -10,6 +10,7 @@ return [
     // common
     'site/time-types' => [1, 2, 3, 4, 5, 6, 7],
     'site/upload' => [1, 2, 3, 4, 5, 6, 7],
+    'site/admin-logout' => [1, 2, 3, 4, 5, 6, 7],
 
     // recommend
     'mall/recommend-admin-index' => [1],
