cf70aabd0457b734a50269798dd69585a89e8289
Wch <362224550@qq.com>
2017-11-17 14:56:54 +0800

    11

--- models/LaborCost.php
+++ models/LaborCost.php
@@ -79,11 +79,9 @@ class LaborCost extends ActiveRecord
             ->select($select)
             ->where(['and',['city_code' => $arr],['worker_kind'=>$craft],['rank'=>$rank]])
             ->one();
+        
+        $labors['univalence'] = $labors['univalence'] / self::PRICE_CONVERT;
 
-        var_dump($labors);exit;
-        foreach ($labors as &$labor){
-            $labor['univalence'] = $labor['univalence'] / self::PRICE_CONVERT;
-        }
         return $labors;
     }
 
