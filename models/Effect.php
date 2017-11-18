7ed359963153f6bc8177c54cde702078d0851522
yangr <yr@qq.com>
2017-11-18 16:46:38 +0800

    11

--- models/Effect.php
+++ models/Effect.php
@@ -218,7 +218,7 @@ class Effect extends ActiveRecord
             ->leftJoin('effect_picture as ep','e.id=ep.effect_id')
             ->leftJoin('series As s','s.id = ep.series_id')
             ->leftJoin('style As t','t.id = ep.style_id')
-            ->where(['ea.effect_id'=>$effect_id])->one();
+            ->where(['ea.id'=>$effect_id])->one();
         if(!$array){
             $data['particulars_view']=null;
         }
@@ -282,7 +282,7 @@ class Effect extends ActiveRecord
             ->leftJoin('effect_picture as ep','ea.effect_id=ep.effect_id')
             ->leftJoin('series As s','s.id = ep.series_id')
             ->leftJoin('style As t','t.id = ep.style_id')
-            ->where(['ea.effect_id'=>$effect_id])->one();
+            ->where(['ea.id'=>$effect_id])->one();
         if(!$array){
             $data=null;
         }
