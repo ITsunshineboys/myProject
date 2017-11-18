d339f4c9f10b63fb4b0df042be5755eeb88b89c1
Wch <362224550@qq.com>
2017-11-18 19:15:45 +0800

    test

--- models/Points.php
+++ models/Points.php
@@ -43,7 +43,6 @@ class Points extends ActiveRecord
 
     public static function findByOne($select =[],$where = [])
     {
-        var_dump($where);exit;
         return self::find()
             ->asArray()
             ->select($select)
