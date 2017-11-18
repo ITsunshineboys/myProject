0b84ddcec9f391210da2638a0c53452d83e61b8f
hj <1534120192@qq.com>
2017-11-18 19:00:01 +0800

    modify update.sql

--- scripts/update.sql
+++ scripts/update.sql
@@ -1,4 +1,4 @@
 -- 上线后，数据表的更新放在这个文件 --
 
 ALTER TABLE engineering_standard_craft modify column id int(11) unsigned NOT NULL AUTO_INCREMENT;
-ALTER TABLE decoration_particulars add effect_id int(11) DEFAULT NULL COMMENT '装修列表'
+ALTER TABLE decoration_particulars add effect_id int(11) DEFAULT NULL COMMENT '装修列表' after id;
