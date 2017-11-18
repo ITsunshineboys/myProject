c45fec608212b9d5787788186dcbbc6d1e11f780
yangr <yr@qq.com>
2017-11-18 14:23:30 +0800

    11

--- controllers/SupplierCashController.php
+++ controllers/SupplierCashController.php
@@ -321,7 +321,7 @@ class SupplierCashController extends Controller
             }
 
         } else {
-            $where = " o.sku like '%{$search}%' or s.shop_name like '%{$search}%' or g.order_no like '%{$search}%'";
+            $where = " s.shop_no like '%{$search}%' or s.shop_name like '%{$search}%' or g.order_no like '%{$search}%'";
         }
 
         $page = (int)$request->get('page', 1);
