e3ae05f2f8e8236b6aa0c6a405ef3f567a3a7a05
Wch <362224550@qq.com>
2017-11-20 20:15:44 +0800

    1

--- services/BasisDecorationService.php
+++ services/BasisDecorationService.php
@@ -723,13 +723,14 @@ class BasisDecorationService
 
 //        个数：（腻子面积×【0.33kg】÷抓取的商品的规格重量）
         $putty_cost ['quantity'] = ceil($area * $craft['material'] / $goods_value_one);
+
+//        腻子费用：个数×商品价格
+        $putty_cost ['cost']  =  $putty_cost['quantity'] * $goods['platform_price'];
         var_dump($area);
         var_dump($craft['material']);
         var_dump($goods_value_one);
         var_dump($putty_cost);
         exit;
-//        腻子费用：个数×商品价格
-        $putty_cost ['cost']  =  $putty_cost['quantity'] * $goods['platform_price'];
         return $putty_cost;
     }
 
