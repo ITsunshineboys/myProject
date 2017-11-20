c86624040043a320a0cf54c0953372681ca22aaf
Wch <362224550@qq.com>
2017-11-20 10:41:15 +0800

    1

--- services/BasisDecorationService.php
+++ services/BasisDecorationService.php
@@ -1770,7 +1770,6 @@ class BasisDecorationService
 
         foreach ($material as &$one_material){
             if ($one_material == null){
-                var_dump($one_material);exit;
                 unset($one_material);
             }
         }
