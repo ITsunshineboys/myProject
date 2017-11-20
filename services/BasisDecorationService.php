9f9d72beecbd005b2d6b3611d5330f94d12a0773
Wch <362224550@qq.com>
2017-11-20 19:56:51 +0800

    1

--- services/BasisDecorationService.php
+++ services/BasisDecorationService.php
@@ -669,6 +669,7 @@ class BasisDecorationService
             $wall_space_area =  sqrt($ground_area / $bedroom) * $tall * $wall * $bedroom;
 //        卧室底漆面积=卧室地面积+卧室墙面积
             $total_area =    $ground_area + $wall_space_area;
+            echo 111;
         var_dump($total_area);
         var_dump($wall_space_area);
         var_dump($total_area);
