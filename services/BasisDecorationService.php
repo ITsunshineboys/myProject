aab5cae4377f6101617d3c10eb2be4a6e874db32
Wch <362224550@qq.com>
2017-11-20 19:48:07 +0800

    1

--- services/BasisDecorationService.php
+++ services/BasisDecorationService.php
@@ -675,7 +675,7 @@ class BasisDecorationService
 //        卧室底漆面积=卧室地面积+卧室墙面积
             $total_area =    $ground_area + $wall_space_area;
         } else {
-            $ground_area = $area * $house_area;
+            $ground_area = $area * ($house_area / 100);
             var_dump($ground_area);
             var_dump($area);
             var_dump($house_area);
