cfc506e77d4dfb512de05d68dd94b418c55bb763
yangr <yr@qq.com>
2017-11-20 17:02:34 +0800

    test

--- controllers/SupplierCashController.php
+++ controllers/SupplierCashController.php
@@ -512,11 +512,8 @@ class SupplierCashController extends Controller
     }
 
     public function actionTest(){
-        var_dump(EffectEarnest::find()->asArray()->where(['effect_id'=>331])->one());
-        var_dump(Effect::find()->asArray()->where(['id'=>331])->one());
-        var_dump(EffectPicture::find()->asArray()->where(['effect_id'=>331])->one());
-        var_dump(Style::find()->asArray()->where(['id'=>3])->one());
-        var_dump(Series::find()->asArray()->where(['id'=>3])->one());
+        var_dump(EffectEarnest::find()->asArray()->all());
+      
 
 
     }
