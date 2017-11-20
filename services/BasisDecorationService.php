4c7f55f6cd6b155a11d2fd592225adb11be3ae37
Wch <362224550@qq.com>
2017-11-20 21:40:30 +0800

    1

--- services/BasisDecorationService.php
+++ services/BasisDecorationService.php
@@ -742,6 +742,10 @@ class BasisDecorationService
         //        （卫生间地面积÷卫生间个数）开平方×【2.4m】×4 ×卫生间个数
         $sqrt= sqrt($ground_area / $quantity);
         $wall_area = $sqrt * $craft * $wall * $quantity;
+        var_dump($ground_area);
+        var_dump($craft);
+        var_dump($quantity);
+        var_dump($wall);exit;
 
         return $wall_area;
     }
