659810dd95e2f42f7795bdd524809b49206f3bc3
Wch <362224550@qq.com>
2017-11-20 20:29:22 +0800

    1

--- controllers/OwnerController.php
+++ controllers/OwnerController.php
@@ -540,7 +540,7 @@ class OwnerController extends Controller
 
         $toilet_area = BasisDecorationService::waterproofArea($_toilet_area,$_toilet_height, $post['area'], $post['toilet']);
         //总面积
-        $apartment_where = 'min_area <='.$post['area'] .' and >='.$post['area'];
+        $apartment_where = 'min_area <='.$post['area'] .' and max_area >='.$post['area'];
         $a = Apartment::find()->asArray()->where($apartment_where)->one();
         var_dump($a);exit;
         $total_area = $kitchen_area + $toilet_area;
