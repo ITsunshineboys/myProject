ac993d346d4033a41bd596a0aba8bbd7c9334442
Wch <362224550@qq.com>
2017-11-21 21:40:18 +0800

    1

--- controllers/OwnerController.php
+++ controllers/OwnerController.php
@@ -1292,7 +1292,6 @@ class OwnerController extends Controller
         if ($post['building_scrap'] == 'true') {
             $building_scrap = BasisDecorationService::haveBuildingScrap($post, $craft);
         } else {
-            echo 111;exit;
             $building_scrap = BasisDecorationService::nothingBuildingScrap($post, $craft);
         }
 
