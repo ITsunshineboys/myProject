831fd76b6ad34715abb0f5beffe2c4ebe04ed6c7
Wch <362224550@qq.com>
2017-11-21 20:46:13 +0800

    1

--- controllers/OwnerController.php
+++ controllers/OwnerController.php
@@ -589,7 +589,7 @@ class OwnerController extends Controller
             ->andWhere(['>=','max_area',$post['area']])
             ->andWhere(['project_name'=>self::OTHER_AREA['waterproof_area']])
             ->one();
-        $qita = empty($apartment['project_value'])?$apartment['project_value']:1;
+        $qita = !empty($apartment['project_value'])?$apartment['project_value']:1;
         var_dump($qita);exit;
         $total_area = $kitchen_area + $toilet_area + $qita;
 
