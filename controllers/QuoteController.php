818e472278357ca0097d1decf134cfcd1d5632b5
Wch <362224550@qq.com>
2017-11-22 14:09:59 +0800

    1

--- controllers/QuoteController.php
+++ controllers/QuoteController.php
@@ -1918,7 +1918,7 @@ class QuoteController extends Controller
     public function actionTest()
     {
         //engineering_standard_carpentry_coefficient
-       var_dump(EngineeringStandardCarpentryCoefficient::deleteAll([]));
+//       var_dump(EngineeringStandardCarpentryCoefficient::deleteAll([]));
        var_dump(EngineeringStandardCarpentryCoefficient::find()->asArray()->all());
     }
 }
