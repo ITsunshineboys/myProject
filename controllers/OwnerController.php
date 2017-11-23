5f9d3af95ad9bb95972c2593047b0035023881c4
Wch <362224550@qq.com>
2017-11-23 17:31:55 +0800

    1

--- controllers/OwnerController.php
+++ controllers/OwnerController.php
@@ -1491,7 +1491,7 @@ class OwnerController extends Controller
         $post = Yii::$app->request->get();
 
         // 有资料 计算公式
-        $materials = ['人造大理石'];
+        $materials = ['木地板','人造大理石','弯头','木门','浴霸','换气扇','吸顶灯','水龙头','床','床头柜','抽油烟机','灶具','立柜式空调','挂壁式空调','灯具','床垫','马桶','浴柜','花洒套装','淋浴隔断'];
         $goods = Goods::assortList($materials,$post['city']);
         $goods_price  = BasisDecorationService::priceConversion($goods);
         $material[]   = BasisDecorationService::formula($goods_price,$post);
