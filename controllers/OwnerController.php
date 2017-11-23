3809d21158d4f929a24902783b487b9debc2f1cf
Wch <362224550@qq.com>
2017-11-23 19:14:32 +0800

    1

--- controllers/OwnerController.php
+++ controllers/OwnerController.php
@@ -1751,8 +1751,8 @@ class OwnerController extends Controller
                         $ma = $detail['attribute'];
                     }
                 }
+                var_dump($stairs_price);exit;
                 foreach ($stairs_price as &$one_stairs_price) {
-
                     if ($one_stairs_price['value'] == $ma && $one_stairs_price['style_id'] == $effect_['style_id']) {
                         echo 111;exit;
                         $one_stairs_price['quantity'] = 1;
