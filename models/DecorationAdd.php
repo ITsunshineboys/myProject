c007ed185a3b6218826b954d0c9ebb812263efee
Wch <362224550@qq.com>
2017-11-23 10:46:55 +0800

    1

--- models/DecorationAdd.php
+++ models/DecorationAdd.php
@@ -99,6 +99,7 @@ class DecorationAdd extends ActiveRecord
             ->asArray()
             ->select($select)
             ->where($where)
+            ->leftJoin('decoration_message as d','d.decoration_add_id = decoration.id')
             ->groupBy('three_materials')
             ->all();
     }
