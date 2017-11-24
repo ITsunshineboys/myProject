5334b91555855837da2e0d7ab7050f2ba850f6e4
xiexueying <837241830@qq.com>
2017-11-24 15:36:41 +0800

    dah

--- web/line/js/controller.js
+++ web/line/js/controller.js
@@ -1144,6 +1144,9 @@ angular.module("all_controller", ['ngCookies'])
         $scope.show_harvest = false;
         $scope.show_address = true; //显示第一个
         $scope.mall_id = $stateParams.mall_id;
+        if($scope.mall_id == undefined){
+            $scope.mall_id = sessionStorage.getItem('mall_id')
+        }
         sessionStorage.setItem('mall_id',$stateParams.mall_id);
         $scope.shopNum = $stateParams.shopNum;
         $scope.leaveMessage = $stateParams.leaveMessage ; //买家留言
@@ -1372,10 +1375,7 @@ angular.module("all_controller", ['ngCookies'])
 
         // 点击去支付判断是否填写完整
         $scope.getModel = function () {
-            if($scope.mall_id == undefined){
-                $scope.mall_id = sessionStorage.getItem('mall_id')
-            }
-            alert(sessionStorage.getItem('address_id'));
+            alert($scope.mall_id);
             $scope.order_order = '';
             $scope.order_address_model = '';
             if( $scope.show_harvest == false && $scope.show_address == true){
