511d2f6da5dbab18969b22d3e3c3c5998afdf2bc
Wch <362224550@qq.com>
2017-11-25 18:52:07 +0800

    1

--- models/Goods.php
+++ models/Goods.php
@@ -963,7 +963,7 @@ class Goods extends ActiveRecord
     public static function seriesAndStyle($level, $title, $post)
     {
         if ($title) {
-            $select = "goods.id,goods.category_id,goods.platform_price,goods.supplier_price,goods.purchase_price_decoration_company,goods_brand.name,gc.title,logistics_district.district_name,goods.category_id,gc.path,goods.profit_rate,goods.subtitle,goods.series_id,goods.style_id,goods.cover_image,supplier.shop_name";
+            $select = "goods.id,goods.category_id,goods.platform_price,goods.supplier_price,goods.purchase_price_decoration_company,goods_brand.name,gc.title,logistics_district.district_name,goods.category_id,gc.path,goods.profit_rate,goods.subtitle,goods.series_id,goods.style_id,goods.cover_image,supplier.shop_name,goods.title as goods_name";
             $goods = self::find()
                 ->asArray()
                 ->select($select)
