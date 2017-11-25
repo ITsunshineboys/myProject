30582515d45d08b12056a0e6ce00891b88db2600
Wch <362224550@qq.com>
2017-11-25 15:40:01 +0800

    1

--- services/BasisDecorationService.php
+++ services/BasisDecorationService.php
@@ -1250,7 +1250,7 @@ class BasisDecorationService
         }else {
             foreach($goods as $v) {
                 $r[$v['title']][$v['profit_rate']] = $v;
-                $max = min($r[$v['title']][$v['profit_rate']],$v['profit_rate']);
+                $max = max($v['profit_rate'],$r[$v['title']][$v['profit_rate']]);
             }
             return $max;
         }
