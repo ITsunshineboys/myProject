2a991440f485a7b9d487a186ebd9bec9c4a9271c
hj <1534120192@qq.com>
2017-11-25 19:21:41 +0800

    test

--- services/FileService.php
+++ services/FileService.php
@@ -256,6 +256,9 @@ class FileService
      */
     public static function deleteFile($filepath)
     {
+        if (YII_DEBUG) {
+            StringService::writeLog('test', var_export(StringService::checkUri($filepath), true) . '|' . var_export(self::existUploadFile($filepath), true));
+        }
         if (false === StringService::checkUri($filepath) && self::existUploadFile($filepath)) {
             $file = Yii::getAlias('@webroot') . '/' . ltrim($filepath, '/');
             unlink($file);
