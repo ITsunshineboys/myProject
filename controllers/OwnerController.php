8be03d552b918c3267840231ee42a5f67fb2eeb2
Wch <362224550@qq.com>
2017-11-25 18:28:46 +0800

    1

--- controllers/OwnerController.php
+++ controllers/OwnerController.php
@@ -798,7 +798,7 @@ class OwnerController extends Controller
         }
         //卧室底漆面积
         $bedroom_primer_area = BasisDecorationService::paintedArea($post['area'],$bedroom_area['project_value'],$post['bedroom'],self::WALL_HIGH,self::WALL);
-        var_dump($bedroom_primer_area);exit;
+
 
 
         //客餐厅底漆面积
