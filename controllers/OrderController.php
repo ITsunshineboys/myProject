44d6f454626c4aa0277f04c2e0667427e540918a
hyz <1248705443@qq.com>
2017-11-27 11:33:40 +0800

    up

--- controllers/OrderController.php
+++ controllers/OrderController.php
@@ -4336,7 +4336,8 @@ class OrderController extends Controller
          $baseUrl = urlencode($url);
          $tools = new PayService();
          $url = $tools->__CreateOauthUrlForCode1($baseUrl);
-         header($url);
+         var_dump($url);exit;
+//         header($url);
 //         $res=Wxpay::curl($url,false,0);
 //         var_dump(json_decode($res,true));exit;
      }
