729f309239e682c0baddd2c71efe97f27d82c1f0
Wch <362224550@qq.com>
2017-11-27 14:54:29 +0800

    1

--- controllers/OwnerController.php
+++ controllers/OwnerController.php
@@ -1734,7 +1734,6 @@ class OwnerController extends Controller
             }
             $select = "goods.id,goods.category_id,goods.platform_price,goods.supplier_price,goods.purchase_price_decoration_company,goods_brand.name,gc.title,logistics_district.district_name,goods.category_id,gc.path,goods.profit_rate,goods.subtitle,goods.series_id,goods.style_id,goods.cover_image,supplier.shop_name,goods.title as goods_name,goods.sku";
             $goods  = Goods::findBySkuAll($sku, $select);
-            var_dump($goods);exit;
             if ($goods == null) {
                 return Json::encode([
                     'code' => 200,
