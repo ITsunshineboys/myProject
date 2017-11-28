26e81bd6935cfe80b0090ec6e09cf9ad73162094
hyz <1248705443@qq.com>
2017-11-28 15:11:16 +0800

    up

--- controllers/OrderController.php
+++ controllers/OrderController.php
@@ -1179,6 +1179,7 @@ class OrderController extends Controller
         $receive_details['district']=$address['district'];
         $receive_details['region']=$address['region'];
         $receive_details['invoice_header']=$invoice['invoice_header'];
+        $order_information['invoice_type']=$invoice['invoice_type'];
         $receive_details['invoice_header_type']=$invoice['invoice_header_type'];
         $receive_details['invoice_content']=$invoice['invoice_content'];
         $receive_details['invoicer_card'] = $invoice['invoicer_card'];
@@ -1189,6 +1190,17 @@ class OrderController extends Controller
                 break;
             case 2:
                 $receive_details['invoice_header_type']='公司';
+                break;
+        }
+        switch ($order_information['invoice_type']){
+            case 1:
+                $order_information['invoice_type']='普通发票';
+                break;
+            case 2:
+                $order_information['invoice_type']='电子发票';
+                break;
+            case 3:
+                $order_information['invoice_type']='普通增值税发票';
                 break;
         }
         $goods_data=array();
@@ -1454,23 +1466,28 @@ class OrderController extends Controller
             $receive_details['district']=LogisticsDistrict::getdistrict($order_information['district_code']);
             $receive_details['region']=$order_information['region'];
             $receive_details['invoice_header']=$order_information['invoice_header'];
-            //此处有更改  输出发票抬头类型为发票类型
             $receive_details['invoice_type']=$order_information['invoice_type'];
+            $receive_details['invoice_header_type']=$order_information['invoice_header_type'];
             $receive_details['invoice_content']=$order_information['invoice_content'];
             $receive_details['invoicer_card'] = $order_information['invoicer_card'];
             $receive_details['buyer_message'] = $order_information['buyer_message'];
+            switch ($receive_details['invoice_header_type']){
+                case 1:
+                    $receive_details['invoice_header_type']='个人';
+                    break;
+                case 2:
+                    $receive_details['invoice_header_type']='公司';
+                    break;
+            }
             switch ($order_information['invoice_type']){
                 case 1:
                     $order_information['invoice_type']='普通发票';
-                    $receive_details['invoice_header_type']='普通发票';
                     break;
                 case 2:
                     $order_information['invoice_type']='电子发票';
-                    $receive_details['invoice_header_type']='电子发票';
                     break;
                 case 3:
                     $order_information['invoice_type']='普通增值税发票';
-                    $receive_details['invoice_header_type']='普通增值税发票';
                     break;
             }
             $goods_data=array();
