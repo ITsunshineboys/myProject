d81b4e14f8a5e00f7cac24017b65b3e63738e961
wangjie <1248136872@qq.com>
2017-11-28 11:45:03 +0800

    up

--- web/admin/supplier/pages/commodity_manage/js/system_offline_ctrl.js
+++ web/admin/supplier/pages/commodity_manage/js/system_offline_ctrl.js
@@ -14,9 +14,7 @@ system_offline.controller("system_offline",function ($rootScope,$scope,$http,$st
 	$scope.detail_arr=[];//详情数组
   	$scope.logistics_templates_supplier=[];//物流模板类型数组
 	$scope.detail_txt=$scope.detail_item.description;//详情描述
-    console.log($scope.detail_txt)
-	console.log($scope.detail_item.after_sale_services_desc)
-	$scope.detail_item_sale_ser=$scope.detail_item.after_sale_services_desc
+	$scope.detail_item_sale_ser=$scope.detail_item.after_sale_services_desc;
   	$scope.sale_services_flag=false;
 	for(let [key,value] of $scope.detail_item_sale_ser.entries()){
 		if(value==('上门维修'||'上门退货'||'上门换货'||'退货'||'换货')){
@@ -28,23 +26,15 @@ system_offline.controller("system_offline",function ($rootScope,$scope,$http,$st
       console.log(res);
       $scope.detail_arr=res.data.goods_view;
   })
+	/*-----------------------------商品属性---------------------------------*/
   _ajax.get('/mall/goods-attrs-admin',{goods_id:+$scope.detail_item.id},function (res) {
       $scope.detail_attrs=res.data.goods_attrs_admin;
       console.log(res);
   })
-
   /*------------------------------物流模板-------------------------------------*/
-  _ajax.get('/mall/logistics-templates-supplier',{},function (res) {
-      $scope.logistics_templates_supplier=res.data.logistics_templates_supplier;
-      for(let [key,value] of $scope.logistics_templates_supplier.entries()){
-          if($scope.detail_item.logistics_template_id==value.id){
-              $scope.logistics_name=value.name;
-          	}
-      }
-  });
-  /*-------------物流模板的内容------------------*/
  _ajax.get('/mall/logistics-template-view',{id:+$scope.detail_item.logistics_template_id},function (res) {
      console.log(res);
+	   $scope.logistics_name=res.data.logistics_template.name;//模板名称
      $scope.logistics_delivery_method=res.data.logistics_template.delivery_method;//快递方式
      $scope.logistics_district_names_arr=res.data.logistics_template.district_names;//地区
      $scope.delivery_cost_default=res.data.logistics_template.delivery_cost_default;//默认运费
