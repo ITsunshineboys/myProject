ca083d7ee4ab68387c774d2bbeddf0f920240dd6
Wch <362224550@qq.com>
2017-11-29 16:13:25 +0800

    1

--- controllers/OwnerController.php
+++ controllers/OwnerController.php
@@ -304,6 +304,7 @@ class OwnerController extends Controller
             }
 
             if ($one_points['title'] == self::ROOM_DETAIL['secondary_bedroom'] && $post['bedroom'] > 1){
+                echo 111;exit;
                 $secondary_bedroom = $one_points['count'] * ($post['bedroom'] -1) ;
             }else{
                 $secondary_bedroom = 0;
