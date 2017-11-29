7646542b9e354e42b2cc1a4b62797b1ac3ec546e
Wch <362224550@qq.com>
2017-11-29 21:10:04 +0800

    1

--- controllers/OwnerController.php
+++ controllers/OwnerController.php
@@ -924,6 +924,13 @@ class OwnerController extends Controller
             ->andWhere(['points_id'=>$points['id']])
             ->one();
         $putty_area = $putty_bedroom_area[0] + $putty_drawing_room_area[0] + $putty_area['project_value'];
+        var_dump( $concave_length['project_value']);
+        var_dump( $drawing_room_perimeter);
+        var_dump( $bedroom_primer_perimeter);
+        var_dump( $concave_line_length);
+        var_dump( $putty_bedroom_area[0]);
+        var_dump( $putty_drawing_room_area[0]);
+        var_dump( $putty_area['project_value']);exit;
 //        腻子天数 腻子面积÷【每天做腻子面积】
         $putty_day = $putty_area / $putty;
 
