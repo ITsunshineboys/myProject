a741054b259f4e427726144c925c5a758b5bb78c
Wch <362224550@qq.com>
2017-11-29 15:08:33 +0800

    1

--- controllers/OwnerController.php
+++ controllers/OwnerController.php
@@ -289,6 +289,7 @@ class OwnerController extends Controller
         $post = \Yii::$app->request->get();
         //      点位 和 材料查询
         $points = Points::findByOne('id,title',self::PROJECT_DETAILS['weak_current']);
+        var_dump($points);exit;
         $weak_where = 'pid = '.$points['id'];
         $weak_points = Points::findByPid('title,count',$weak_where);
         var_dump($weak_points);exit;
