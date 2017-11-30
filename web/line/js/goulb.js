64ac90319d3d67597f5b76552ef49c2eb495eec5
afang <1570948949@qq.com>
2017-11-30 10:57:50 +0800

    H5

--- web/line/js/goulb.js
+++ web/line/js/goulb.js
@@ -1,5 +1,5 @@
-// const baseUrl = '';
-const baseUrl = 'https://ac.cdlhzz.cn';
+const baseUrl = '';
+// const baseUrl = 'https://ac.cdlhzz.cn';
 document.documentElement.style.fontSize = document.documentElement.clientWidth /32 + "px";
 window.addEventListener("resize",function () {
     document.documentElement.style.fontSize = document.documentElement.clientWidth /32+ "px";
