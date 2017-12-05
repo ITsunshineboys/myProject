20da32acff28e5e7ec3faa4483bcba74f159fcbb
hyz <1248705443@qq.com>
2017-12-05 11:59:52 +0800

    up

--- vendor/wxpay/lib/WxPayDataBase.php
+++ vendor/wxpay/lib/WxPayDataBase.php
@@ -137,19 +137,15 @@ class WxPayDataBase
         $xml = "<xml>";
         foreach ($this->values as $key=>$val)
         {
-            if (is_numeric($val)){
-                $xml.="<".$key.">".$val."</".$key.">";
-            }else{
-                $xml.="<".$key."><![CDATA[".$val."]]></".$key.">";
-            }
+            $xml.="<".$key.">".$val."</".$key.">";
         }
         $xml.="</xml>";
+        var_dump($xml);die;
         //签名步骤一：按字典序排序参数
         ksort($this->values);
         $string = $this->ToUrlParams();
         //签名步骤二：在string后加入KEY
         $string = $string . "&key=".WxPayConfig::APP_KEY;
-        echo $string;exit;
         //签名步骤三：MD5加密
         $string = md5($string);
         //签名步骤四：所有字符转为大写
