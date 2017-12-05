93eccfc91f3e4d374ede1e7e15c7db691b90e908
hyz <1248705443@qq.com>
2017-12-05 11:29:29 +0800

    up

--- vendor/wxpay/lib/WxPayApi.php
+++ vendor/wxpay/lib/WxPayApi.php
@@ -84,6 +84,7 @@ class WxPayApi
      */
     public static function AppUnifiedOrder($inputObj, $timeOut = 6)
     {
+        var_dump($inputObj);exit;
         $url = "https://api.mch.weixin.qq.com/pay/unifiedorder";
         //检测必填参数
         if(!$inputObj->IsOut_trade_noSet()) {
