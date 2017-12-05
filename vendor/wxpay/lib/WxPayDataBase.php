4e5e1916364b929fcea6d08d8ba4390fa2cb1785
hyz <1248705443@qq.com>
2017-12-05 11:58:30 +0800

    up

--- vendor/wxpay/lib/WxPayDataBase.php
+++ vendor/wxpay/lib/WxPayDataBase.php
@@ -144,12 +144,12 @@ class WxPayDataBase
             }
         }
         $xml.="</xml>";
-        var_dump($xml);exit;
         //签名步骤一：按字典序排序参数
         ksort($this->values);
         $string = $this->ToUrlParams();
         //签名步骤二：在string后加入KEY
         $string = $string . "&key=".WxPayConfig::APP_KEY;
+        echo $string;exit;
         //签名步骤三：MD5加密
         $string = md5($string);
         //签名步骤四：所有字符转为大写
