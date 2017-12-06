88e2ec2b132ca212aac6356aa1ff24b5640304bf
Wch <362224550@qq.com>
2017-12-06 18:40:14 +0800

    1

--- models/Goods.php
+++ models/Goods.php
@@ -473,7 +473,7 @@ class Goods extends ActiveRecord
      */
     public static function priceDetail($level, $title, $city = self::DEFAULT_CITY)
     {
-        $select = 'goods.id,goods.category_id,goods.platform_price,goods.supplier_price,goods.purchase_price_decoration_company,goods_brand.name,gc.title,logistics_district.district_name,goods.series_id,goods.style_id,goods.subtitle,goods.profit_rate,gc.path,goods.cover_image,supplier.shop_name,goods.title as goods_name';
+        $select = 'goods.supplier_id,goods.id,goods.category_id,goods.platform_price,goods.supplier_price,goods.purchase_price_decoration_company,goods_brand.name,gc.title,logistics_district.district_name,goods.series_id,goods.style_id,goods.subtitle,goods.profit_rate,gc.path,goods.cover_image,supplier.shop_name,goods.title as goods_name';
 
         $all = self::find()
             ->asArray()
@@ -486,7 +486,6 @@ class Goods extends ActiveRecord
             ->where(['and', ['logistics_district.district_code' => $city], ['gc.level' => $level], ['in', 'gc.id', $title], ['goods.status' => self::STATUS_ONLINE]])
             ->all();
 
-        var_dump($all);die;
         foreach ($all as &$one_goods) {
             $one_goods['platform_price'] =  $one_goods['platform_price'] / 100;
             $one_goods['supplier_price'] =  $one_goods['supplier_price'] / 100;
