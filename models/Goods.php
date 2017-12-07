a94b113e1b9de3b8356284d913bbe3d74cfea0c0
Wch <362224550@qq.com>
2017-12-07 15:11:44 +0800

    test

--- models/Goods.php
+++ models/Goods.php
@@ -473,8 +473,8 @@ class Goods extends ActiveRecord
      */
     public static function priceDetail($level, $title, $city = self::DEFAULT_CITY)
     {
-        //TODO 缺少market_price
-        $select = 'goods.market_price,goods.supplier_id,goods.id,goods.category_id,goods.platform_price,goods.supplier_price,goods.purchase_price_decoration_company,goods_brand.name,gc.title,logistics_district.district_name,goods.series_id,goods.style_id,goods.subtitle,goods.profit_rate,gc.path,goods.cover_image,supplier.shop_name,goods.title as goods_name';
+        //TODO 缺少market_price sku left_number
+        $select = 'goods.left_number,goods.sku,goods.market_price,goods.supplier_id,goods.id,goods.category_id,goods.platform_price,goods.supplier_price,goods.purchase_price_decoration_company,goods_brand.name,gc.title,logistics_district.district_name,goods.series_id,goods.style_id,goods.subtitle,goods.profit_rate,gc.path,goods.cover_image,supplier.shop_name,goods.title as goods_name';
         //TODO 修改
         if(is_array($title)){
             $where=['and', ['logistics_district.district_code' => $city], ['gc.level' => $level], ['in', 'gc.id', $title], ['goods.status' => self::STATUS_ONLINE]];
