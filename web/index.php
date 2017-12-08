803051fce71bad7e42b1088ea576beeab08faf84
hyz <1248705443@qq.com>
2017-12-08 16:21:37 +0800

    up

--- web/index.php
+++ web/index.php
@@ -10,18 +10,17 @@ require(__DIR__ . '/../vendor/autoload.php');
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
 if (YII_DEBUG) {
-//    $arr=[
-//       0=> 'Access-Control-Allow-Origin: http://cross.local.com:808',
-//        1=>  'Access-Control-Allow-Origin: http://localhost:808',
-//    ];
-    $host='http://'.$_SERVER['SERVER_NAME'];
-    if (array_key_exists($host,$arr))
-    {
-        header('Access-Control-Allow-Origin:'.$host.':808');
-        header("Access-Control-Allow-Credentials: true");
+
+    $origin = isset($_SERVER['HTTP_ORIGIN'])? $_SERVER['HTTP_ORIGIN'] : '';
+
+    $allow_origin = array(
+        'http://cross.local.com:808',
+        'http://localhost:808'
+    );
+
+    if(in_array($origin, $allow_origin)){
+        header('Access-Control-Allow-Origin:'.$origin);
     }
-//    header('Access-Control-Allow-Origin: http://cross.local.com:808');
-//    header("Access-Control-Allow-Credentials: true");
 }
 
 (new yii\web\Application($config))->run();
