751c3dc6b0c1ba00f81e2ce96ebce65cef3e70e7
hyz <1248705443@qq.com>
2017-12-08 16:16:41 +0800

    up

--- web/index.php
+++ web/index.php
@@ -10,14 +10,14 @@ require(__DIR__ . '/../vendor/autoload.php');
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
 if (YII_DEBUG) {
-    $arr=[
-        'Access-Control-Allow-Origin: http://cross.local.com:808',
-        'Access-Control-Allow-Origin: http://localhost:808',
-    ];
+//    $arr=[
+//       0=> 'Access-Control-Allow-Origin: http://cross.local.com:808',
+//        1=>  'Access-Control-Allow-Origin: http://localhost:808',
+//    ];
     $host='http://'.$_SERVER['SERVER_NAME'];
     if (array_key_exists($host,$arr))
     {
-        header('Access-Control-Allow-Origin:'.$host);
+        header('Access-Control-Allow-Origin:'.$host.':808');
         header("Access-Control-Allow-Credentials: true");
     }
 //    header('Access-Control-Allow-Origin: http://cross.local.com:808');
