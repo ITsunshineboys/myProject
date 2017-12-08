6800cd6421f79ca24bc6683c34dd7de35310b44d
hyz <1248705443@qq.com>
2017-12-08 16:40:03 +0800

    up

--- web/index.php
+++ web/index.php
@@ -3,16 +3,26 @@
 $config = require(__DIR__ . '/../config/web.php');
 
 // comment out the following two lines when deployed to production
-define('YII_DEBUG', $config['params']['online']['env'] == 'dev');
-define('YII_ENV', $config['params']['online']['env']);
+//define('YII_DEBUG', $config['params']['online']['env'] == 'dev');
+//define('YII_ENV', $config['params']['online']['env']);
 
 require(__DIR__ . '/../vendor/autoload.php');
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
 if (YII_DEBUG) {
 
-    header('Access-Control-Allow-Origin: http://cross.local.com:808');
-    header("Access-Control-Allow-Credentials: true");
+    $origin = isset($_SERVER['HTTP_ORIGIN'])? $_SERVER['HTTP_ORIGIN'] : 'http://cross.local.com:808';
+
+    $allow_origin = array(
+        'http://cross.local.com:808',
+        'http://localhost:808'
+    );
+
+    if(in_array($origin, $allow_origin)){
+        header('Access-Control-Allow-Origin:'.$origin);
+        header("Access-Control-Allow-Credentials: true");
+    }
 }
+
 
 (new yii\web\Application($config))->run();
