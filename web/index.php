0712bd3191796f1a2995d40b9f3225c9270445bd
hyz <1248705443@qq.com>
2017-12-08 16:14:36 +0800

    up

--- web/index.php
+++ web/index.php
@@ -10,8 +10,18 @@ require(__DIR__ . '/../vendor/autoload.php');
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
 if (YII_DEBUG) {
-    header('Access-Control-Allow-Origin: http://cross.local.com:808');
-    header("Access-Control-Allow-Credentials: true");
+    $arr=[
+        'Access-Control-Allow-Origin: http://cross.local.com:808',
+        'Access-Control-Allow-Origin: http://localhost:808',
+    ];
+    $host='http://'.$_SERVER['SERVER_NAME'];
+    if (array_key_exists($host,$arr))
+    {
+        header('Access-Control-Allow-Origin:'.$host);
+        header("Access-Control-Allow-Credentials: true");
+    }
+//    header('Access-Control-Allow-Origin: http://cross.local.com:808');
+//    header("Access-Control-Allow-Credentials: true");
 }
 
 (new yii\web\Application($config))->run();
