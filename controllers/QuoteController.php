66b7d9cebf62f5df2719b5e02e8a992b717a0ca2
Wch <362224550@qq.com>
2017-12-13 17:33:37 +0800

    1

--- controllers/QuoteController.php
+++ controllers/QuoteController.php
@@ -2108,8 +2108,8 @@ class QuoteController extends Controller
     public function actionTest()
     {
         //engineering_standard_carpentry_craft
-        $a = EngineeringStandardCarpentryCraft::findOne(['id'=>3]);
-        $a->title = '石膏板抓取规格';
+        $a = EngineeringStandardCarpentryCraft::findOne(['id'=>4]);
+        $a->title = '细木工板抓取长度';
         $a->save();
         var_dump($a);exit;
 
