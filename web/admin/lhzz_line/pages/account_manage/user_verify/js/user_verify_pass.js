34b1830c4c4f733338a2a021f975a89d347d32e9
Clarissahu <hulingfangzi@163.com>
2017-12-13 18:55:10 +0800

    account

--- web/admin/lhzz_line/pages/account_manage/user_verify/js/user_verify_pass.js
+++ web/admin/lhzz_line/pages/account_manage/user_verify/js/user_verify_pass.js
@@ -1,6 +1,98 @@
 /**
  * Created by Administrator on 2017/12/13/013.
  */
-app.controller('account_user_verify_pass', ['$rootScope', '$scope', '$state', '$stateParams', function ($rootScope, $scope, $state, $stateParams) {
+app.controller('account_user_verify_pass', ['$rootScope', '$scope', '$state', '$stateParams', '_ajax', function ($rootScope, $scope, $state, $stateParams, _ajax) {
+    /*请求参数*/
+    $scope.params = {
+        time_type: 'all',               // 时间类型
+        start_time: '',                 // 自定义开始时间
+        end_time: '',                   // 自定义结束时间
+        page: 1,                        // 当前页数
+        keyword: '',                    // 关键字查询
+        status: 2,                      // 审核状态
+        sort: 2                         //排序规则 默认按审核时间降序排列
+    }
 
+    /*分页配置*/
+    $scope.pageConfig = {
+        showJump: true,
+        itemsPerPage: 12,
+        currentPage: 1,
+        onChange: function () {
+            tableList();
+        }
+    }
+
+    // 时间筛选器
+    $scope.$watch('params.time_type', function (value, oldValue) {
+        if (value == oldValue) {
+            return
+        }
+        if (value == 'all' && $scope.params.keyword != '') {
+            return
+        }
+        if (value != 'custom') {
+            $scope.keyword = '';                // 关键字查询
+            $scope.params.start_time = '';     // 自定义开始时间
+            $scope.params.end_time = '';       // 自定义结束时间
+            $scope.params.sort = 2;            //审核时间排序
+            $scope.pageConfig.currentPage = 1;
+            tableList();
+        }
+    });
+
+    //自定义时间筛选
+    // 开始时间
+    $scope.$watch('params.start_time', function (value, oldValue) {
+        if (value == oldValue) {
+            return
+        }
+        if ($scope.params.end_time != '') {
+            $scope.keyword = '';        // 关键字查询
+            $scope.params.sort = 2;     //审核时间排序
+            $scope.pageConfig.currentPage = 1;
+            tableList();
+        }
+    });
+
+    // 结束时间
+    $scope.$watch('params.end_time', function (value, oldValue) {
+        if (value == oldValue) {
+            return
+        }
+        if ($scope.params.start_time != '') {
+            $scope.keyword = '';        // 关键字查询
+            $scope.params.sort = 2;     //审核时间排序
+            $scope.pageConfig.currentPage = 1;
+            tableList();
+        }
+    });
+
+    // 查询
+    $scope.search = function () {
+        $scope.params.time_type = 'all';   // 时间类型
+        $scope.params.start_time = '';     // 自定义开始时间
+        $scope.params.end_time = '';       // 自定义结束时间
+        $scope.params.sort = 2;            //审核时间排序
+        $scope.pageConfig.currentPage = 1;
+        tableList();
+    };
+
+    // 审核时间排序
+    $scope.sortTime = function () {
+        $scope.params.sort = $scope.params.sort == 2 ? 1 : 2;
+        $scope.pageConfig.currentPage = 1;
+        tableList();
+    };
+
+
+    /*列表数据获取*/
+    function tableList() {
+        $scope.params.keyword = $scope.keyword;
+        $scope.params.page = $scope.pageConfig.currentPage;
+        _ajax.get('/supplieraccount/owner-audit-list', $scope.params, function (res) {
+            $scope.pageConfig.totalItems = res.data.count;
+            $scope.listdata = res.data.list;
+        })
+    }
 }]);
--- web/admin/lhzz_line/pages/mall_manage/merchant_settle/js/settle_verify_wait.js
+++ web/admin/lhzz_line/pages/mall_manage/merchant_settle/js/settle_verify_wait.js
@@ -1,8 +1,7 @@
 /**
  * Created by Administrator on 2017/12/12/012.
  */
-app.controller('settle_verify_wait', ['$rootScope', '$scope', '$state', '$stateParams', function ($rootScope, $scope, $state, $stateParams) {
-    $scope.id = $stateParams.id;    // 商家ID
+app.controller('settle_verify_wait', ['$rootScope', '$scope', '$state', '$stateParams', '_ajax', function ($rootScope, $scope, $state, $stateParams, _ajax) {
     $rootScope.crumbs = [{
         name: '商城管理',
         icon: 'icon-shangchengguanli',
@@ -13,11 +12,141 @@ app.controller('settle_verify_wait', ['$rootScope', '$scope', '$state', '$stateP
 
 
     $scope.params = {
-
-
+        time_type: 'all',               // 时间类型
+        start_time: '',                 // 自定义开始时间
+        end_time: '',                   // 自定义结束时间
+        page: 1,                        // 当前页数
+        keyword: '',                    // 关键字查询
+        review_status: 0,               // 审核状态
+        'sort[]': 'review_apply_time: 3' //排序规则 默认按申请时间降序排列
     }
 
-    $scope.storetype_arr = [{storetype: "全部", id: -1}, {storetype: "旗舰店", id: 0}, {
-        storetype: "专卖店", id: 1}, {storetype: "专营店", id: 2},{storetype: "自营店", id: 3}] //店铺类型
+    /*分页配置*/
+    $scope.pageConfig = {
+        showJump: true,
+        itemsPerPage: 12,
+        currentPage: 1,
+        onChange: function () {
+            tableList();
+        }
+    }
+
+    /*排序按钮样式控制*/
+    $scope.sortStyleFunc = () => {
+        return $scope.params['sort[]'].split(':')[1]
+    }
+
+    // 时间筛选器
+    $scope.$watch('params.time_type', function (value, oldValue) {
+        if (value == oldValue) {
+            return
+        }
+        if (value == 'all' && $scope.params.keyword != '') {
+            return
+        }
+        if (value != 'custom') {
+            $scope.keyword = '';                // 关键字查询
+            $scope.params.start_time = '';     // 自定义开始时间
+            $scope.params.end_time = '';       // 自定义结束时间
+            $scope.params['sort[]'] = 'review_apply_time:3'; //申请时间排序
+            $scope.pageConfig.currentPage = 1;
+            tableList();
+        }
+    });
+
+    //自定义时间筛选
+    // 开始时间
+    $scope.$watch('params.start_time', function (value, oldValue) {
+        if (value == oldValue) {
+            return
+        }
+        if ($scope.params.end_time != '') {
+            $scope.keyword = '';        // 关键字查询
+            $scope.params['sort[]'] = 'review_apply_time:3'; //申请时间排序
+            $scope.pageConfig.currentPage = 1;
+            tableList()
+        }
+    });
+
+    // 结束时间
+    $scope.$watch('params.end_time', function (value, oldValue) {
+        if (value == oldValue) {
+            return
+        }
+        if ($scope.params.start_time != '') {
+            $scope.keyword = '';        // 关键字查询
+            $scope.params['sort[]'] = 'review_apply_time:3'; //申请时间排序
+            $scope.pageConfig.currentPage = 1;
+            tableList()
+        }
+    });
+
+    // 查询
+    $scope.search = function () {
+        $scope.params.time_type = 'all';   // 时间类型
+        $scope.params.start_time = '';     // 自定义开始时间
+        $scope.params.end_time = '';       // 自定义结束时间
+        $scope.params['sort[]'] = 'review_apply_time:3'; //申请时间排序
+        $scope.pageConfig.currentPage = 1;
+        tableList()
+    };
+
+    //时间排序
+    $scope.sortTime = function () {
+        $scope.params['sort[]'] = $scope.params['sort[]'] == 'review_apply_time:3' ? 'review_apply_time:4' : 'review_apply_time:3';
+        $scope.pageConfig.currentPage = 1;
+        tableList();
+    };
+
+
+
+    /*列表数据获取*/
+    function tableList() {
+        $scope.params.keyword = $scope.keyword;
+        $scope.params.page = $scope.pageConfig.currentPage;
+        // _ajax.get('/supplier/supplier-be-audited-list',$scope.params,function (res) {
+            // $scope.pageConfig.totalItems = res.data.list.count;
+            // $scope.listdata = res.data.list.list;
+            let data = {
+                "data": {
+                    "list": {
+                        "list": [
+                            {
+                                "review_apply_time": "1970-01-01 08:00",
+                                "review_status": "0",
+                                "mobile": "15208280987",
+                                "shop_name": "何友志",
+                                "type_shop": "旗舰店",
+                                "category_id": "12",
+                                "category": "辅材-板材类-澳松板"
+                            },
+                            {
+                                "review_apply_time": "1970-01-01 08:00",
+                                "review_status": "2",
+                                "mobile": "18208142446",
+                                "shop_name": "邵恒",
+                                "type_shop": "旗舰店",
+                                "category_id": "10",
+                                "category": "辅材-板材类-龙骨"
+                            }
+                        ],
+                        "total_page": 1,
+                        "count": "2",
+                        "page": 1
+                    }
+                }
+            }
+
+           $scope.listdata = data.data.list.list
+
+
+            console.log(res);
+        // })
+    }
+
+
+
+    // $scope.storetype_arr = [{storetype: "全部", id: -1}, {storetype: "旗舰店", id: 0}, {
+    //     storetype: "专卖店", id: 1}, {storetype: "专营店", id: 2},{storetype: "自营店", id: 3}] //店铺类型
 
 }]);
