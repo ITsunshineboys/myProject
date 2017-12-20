344e3d1b817848b20ff1fc50e2ced36099dfa371
Piz <q564146734@163.com>
2017-12-20 17:35:44 +0800

    更改代码

--- web/offline-mall/src/App.vue
+++ web/offline-mall/src/App.vue
@@ -1,28 +1,12 @@
 <template>
   <div id="app">
     <router-view></router-view>
-    <button @click="show">点击</button>
   </div>
 </template>
 
 <script>
   export default {
-    name: 'app',
-    data () {
-      return {
-        id: 5
-      }
-    },
-    methods: {
-      show () {
-        let params = {
-          id: 60
-        }
-        this.axios.post('/effect/effect-view', params, function (res) {
-          console.log(res)
-        })
-      }
-    }
+    name: 'app'
   }
 </script>
 
