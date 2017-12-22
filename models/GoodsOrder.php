352afdc713a6e52b543b8c25a3c7df458a6109d1
hyz <1248705443@qq.com>
2017-12-22 17:11:07 +0800

    up

--- models/GoodsOrder.php
+++ models/GoodsOrder.php
@@ -1448,7 +1448,13 @@ class GoodsOrder extends ActiveRecord
         $res['handle_time']=date('Y-m-d H:i',$res['creat_time']);
         unset($res['creat_time']);
         $res['refund_result']=self::Getplatformresult($res['refund_result']);
-        $res['refund_type']=self::Getplatformtype($res['refund_type']);
+        if (empty($res['refund_type']))
+        {
+            $res['refund_type']=self::Getplatformtype($res['refund_type']);
+        }else
+        {
+            $res['refund_type']='';
+        }
         $res['refund_time']=date('Y-m-d H:i',$res['refund_time']);
         return $res;
     }
