0021d8ba1580fd21c19f0ad256e53be6298aa56d
wangjie <1248136872@qq.com>
2017-12-23 15:08:28 +0800

    h5

--- web/offline-mall/src/views/address/index.vue
+++ web/offline-mall/src/views/address/index.vue
@@ -2,22 +2,21 @@
     <div>
       <x-header :left-options="{backText: ''}">编辑收货地址</x-header>
       <div class="bg-white address-box">
-        <x-input class="consignee-box" on-change="consignee" title="收货人" label-width="5rem" :placeholder="'请输入姓名'" v-model="consignee"></x-input>
-        <x-input class="phone-box" title="联系电话" :placeholder="'请输入联系电话'" mask="999 9999 9999" v-model="contactNumber" :max="13" is-type="china-mobile"></x-input>
+        <x-input class="consignee-box" v-model="consignee" @on-change="consigneeChange" title="收货人" label-width="5rem" :placeholder="'请输入姓名'"></x-input>
+        <x-input class="phone-box"  ref="phone_ref" @on-change="phoneChange" title="联系电话" name="mobile"  v-model="phoneNumber"  placeholder="请输入联系电话" keyboard="number" is-type="china-mobile" :max="11"></x-input>
         <group class="choose-address-box" label-width="5rem" label-align="left">
           <x-address title="地址选择" v-model="addressValue" raw-value :list="addressData" value-text-align="right"></x-address>
         </group>
         <group class="address-detail-box" label-width="5rem">
-          <x-textarea  :title="'详细地址'" :placeholder="'请输入详细地址'" :max=30 :show-counter="false" :rows="1" autosize></x-textarea>
+          <x-textarea  :title="'详细地址'" @on-change="addressChange" :placeholder="'请输入详细地址'" v-model="detailAddress" :max=30 :show-counter="false" :rows="1" autosize></x-textarea>
         </group>
       </div>
-      <x-button class="save-btn" type="primary" >保存</x-button>
+      <x-button class="save-btn" :class="{'save-btn-true':requiredStatus && phoneStatus && addressStatus}" type="primary" :text="'保存'" @click.native="btnClick" :disabled="!requiredStatus || !phoneStatus || !addressStatus"></x-button>
     </div>
 </template>
 
 <script>
-  import {XHeader, Group, XInput, XAddress, XTextarea, ChinaAddressData, XButton} from 'vux'
-//  import { XAddress, ChinaAddressData } from 'vux'
+  import {XHeader, Group, XInput, XAddress, XTextarea, ChinaAddressV4Data, XButton} from 'vux'
   export default {
     name: 'Address',
     components: {
@@ -30,10 +29,42 @@
     },
     data () {
       return {
+        requiredStatus: false,
+        phoneStatus: false,
+        addressStatus: false,
         consignee: '',
-        contactNumber: '',
-        addressData: ChinaAddressData,
-        addressValue: ['四川省', '成都市', '锦江区']
+        phoneNumber: '',
+        addressValue: ['四川省', '成都市', '锦江区'],
+        detailAddress: '',
+        addressData: ChinaAddressV4Data
+      }
+    },
+    methods: {
+      btnClick () {
+        this.axios.post('/order/add-line-receive-address', {
+          consignee: this.consignee,
+          mobile: this.phoneNumber,
+          district_code: this.addressValue[2],
+          region: this.detailAddress
+        }, function (res) {
+          console.log(res)
+        })
+//        console.log(this.addressValue[2])
+      },
+      consigneeChange (value) {   // 收货人
+        let that = this
+        value === '' ? that.requiredStatus = false : that.requiredStatus = true
+      },
+      phoneChange () {
+        let that = this
+        that.phoneStatus = this.$refs.phone_ref.valid
+        if (that.phoneStatus === true) {
+          that.phoneNumber === '' ? that.phoneStatus = false : that.phoneStatus = true
+        }
+      },
+      addressChange () {
+        let that = this
+        that.detailAddress === '' ? that.addressStatus = false : that.addressStatus = true
       }
     }
   }
@@ -80,6 +111,8 @@
     color:rgba(255,255,255,0.25)!important;
     background:rgba(34,34,34,0.3)!important;
   }
-
-  /*.address-detail-box */
+  .save-btn-true{
+    color:rgba(255,255,255,1)!important;
+    background:rgba(34,34,34,1)!important;
+  }
 </style>
