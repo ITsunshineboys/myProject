8c7996da755a4ffe974363a4653148b5e6faf2f0
Piz <q564146734@163.com>
2017-12-26 14:07:02 +0800

    分类列表

---
+++ web/offline-mall/src/views/class/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="class-list">
+    <x-header :left-options="{backText: ''}">
+      <search placeholder="请输入想要购买的商品"></search>
+      <span class="iconfont icon-more" slot="right" @click="showMore"></span>
+    </x-header>
+
+    <div v-show="showText">
+      Hello World!
+    </div>
+  </div>
+</template>
+
+<script>
+  import {XHeader, Search} from 'vux'
+  export default {
+    components: {
+      XHeader,
+      Search
+    },
+    data () {
+      return {
+        showText: false
+      }
+    },
+    methods: {
+      showMore () {
+        this.showText = !this.showText
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
