617028c3db50b2d62646ec945d4ec12ab0fb7a57
Clarissahu <hulingfangzi@163.com>
2017-12-26 13:59:18 +0800

    add good

--- web/offline-mall/src/router/index.js
+++ web/offline-mall/src/router/index.js
@@ -2,12 +2,18 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Order from '@/views/order/index'
 import Address from '@/views/address/index'
+import GoodDetail from '@/views/good_detail/index'
 import ClassList from '@/views/class/index'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    {
+      path: '/good-detail',
+      name: 'GoodDetail',
+      component: GoodDetail
+    },
     {
       path: '/order',
       name: 'Order',
