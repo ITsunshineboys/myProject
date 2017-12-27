084764621c19c34792e88a51de7468d72753cc62
hj <hj@hj.com>
2017-12-27 16:22:17 +0800

    modify lhzz permission model

--- models/LhzzPermission.php
+++ models/LhzzPermission.php
@@ -12,11 +12,55 @@ use yii\db\ActiveRecord;
 
 class LhzzPermission extends ActiveRecord
 {
+    const MODULES = [
+        'site' => '通用',
+        'distribution' => '分销',
+        'effect' => '样板间',
+        'mall' => '商城',
+        'order' => '订单',
+        'quote' => '智能报价',
+        'supplier-cash' => '提现',
+        'supplieraccount' => '账户',
+        'withdrawals' => '钱包',
+    ];
+
     /**
      * @return string 返回该AR类关联的数据表名
      */
     public static function tableName()
     {
         return 'lhzz_permission';
+    }
+
+    /**
+     * Get all permissions group by module
+     *
+     * @return array
+     */
+    public static function allGroupByModule()
+    {
+        $perms = self::find()
+            ->select(['id', 'controller', 'desc'])
+            ->orderBy('controller')
+            ->asArray()
+            ->all();
+
+        $tmp = [];
+        foreach ($perms as $perm) {
+            $tmp[$perm['controller']][] = [
+                'id' => $perm['id'],
+                'name' => $perm['desc'],
+            ];
+        }
+
+        $all = [];
+        foreach ($tmp as $k => $v) {
+            $all[] = [
+                'module' => isset(self::MODULES[$k]) ? self::MODULES[$k] : $k,
+                'permissions' => $v,
+            ];
+        }
+
+        return $all;
     }
 }
