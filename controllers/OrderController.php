58ac3c528e197a32f961bb681b88164d677f2731
hyz <1248705443@qq.com>
2017-12-27 11:33:50 +0800

    up

--- controllers/OrderController.php
+++ controllers/OrderController.php
@@ -3386,160 +3386,160 @@ class OrderController extends Controller
             ]);
         }
     }
-    /**
-     * 添加测试数据
-     * @return string
-     */
-    public function actionAddTestOrderData(){
-        $request=Yii::$app->request;
-        $order_no=GoodsOrder::SetOrderNo();
-        $goods_id=$request->post('goods_id');
-        $goods_num=$request->post('goods_num');
-        $mobile=$request->post('mobile');
-        $address_id=$request->post('address_id');
-        $invoice_id=$request->post('invoice_id');
-        $goods=Goods::find()
-            ->where(['id'=>$goods_id])
-            ->one();
-        if (!$goods){
-            $c=1000;
-            return Json::encode([
-                'code' =>  $c,
-                'msg'  =>'商品不存在'
-            ]);
-        }
-        $supplier=Supplier::findOne($goods->supplier_id);
-        if (!$supplier)
-        {
-            $c=1000;
-            return Json::encode([
-                'code' =>  $c,
-                'msg'  =>'商家不存在'
-            ]);
-        }
-        $LogisticsTemplate=LogisticsTemplate::findOne($goods->logistics_template_id);
-        $Goods[]=[
-            'goods_id'=>$goods_id,
-            'goods_num'=>$goods_num
-        ];
-        if ($LogisticsTemplate->delivery_method==0)
-        {
-            $freight=GoodsOrder::CalculationFreightTest($Goods);
-        }
-        else
-        {
-            $freight=0;
-        }
-        $address=UserAddress::find()
-            ->where(['id'=>$address_id])
-            ->one();
-        $invoice=Invoice::find()
-            ->where(['id'=>$invoice_id])
-            ->one();
-        if (!$address  || !$invoice)
-        {
-            $c=1000;
-            return Json::encode([
-                'code' =>  $c,
-                'msg'  =>'收货地址ID 或 发票ID 错误'
-            ]);
-        }
-        $user=User::find()->where(['mobile'=>$mobile])->one();
-        $amount_order=$goods->platform_price*$goods_num+$freight;
-        $tran = Yii::$app->db->beginTransaction();
-        $time=time();
-        try{
-            $GoodsOrder= new GoodsOrder();
-            $GoodsOrder->order_no=$order_no;
-            $GoodsOrder->amount_order=$amount_order;
-            $GoodsOrder->pay_status=0;
-            $GoodsOrder->create_time=$time;
-            $GoodsOrder->paytime=$time;
-            $GoodsOrder->pay_name='支付宝支付';
-            $GoodsOrder->order_refer=2;
-            $GoodsOrder->return_insurance=0;
-            $GoodsOrder->role_id=7;
-            $GoodsOrder->supplier_id=$goods->supplier_id;
-            $GoodsOrder->user_id=$user->id;
-            $GoodsOrder->buyer_message='请发快递给我';
-            $GoodsOrder->consignee=$address->consignee;
-            $GoodsOrder->district_code=$address->district;
-            $GoodsOrder->region=$address->region;
-            $GoodsOrder->consignee_mobile=$address->mobile;
-            $GoodsOrder->invoice_type=$invoice->invoice_type;
-            $GoodsOrder->invoice_header_type=$invoice->invoice_header_type;
-            $GoodsOrder->invoicer_card=$invoice->invoicer_card;
-            $GoodsOrder->invoice_header=$invoice->invoice_header;
-            $GoodsOrder->invoice_content=$invoice->invoice_content;
-            $res1=$GoodsOrder->save(false);
-            $OrderGoods=new  OrderGoods();
-            $OrderGoods->order_no=$order_no;
-            $OrderGoods->goods_id=$goods->id;
-            $OrderGoods->goods_number=$goods_num;
-            $OrderGoods->goods_attr_id=12;
-            $OrderGoods->create_time=$time;
-            $OrderGoods->goods_name=$goods->title;
-            $OrderGoods->goods_price=$goods->platform_price;
-            $OrderGoods->sku=$goods->sku;
-            $OrderGoods->market_price=$goods->market_price;
-            $OrderGoods->supplier_price=$goods->supplier_price;
-            $OrderGoods->shipping_type=$LogisticsTemplate->delivery_method;
-            $OrderGoods->order_status=0;
-            $OrderGoods->shipping_status=0;
-            $OrderGoods->customer_service=0;
-            $OrderGoods->is_unusual=0;
-            $OrderGoods->freight=$freight;
-            $OrderGoods->cover_image=$goods->cover_image;
-            $OrderGoods->after_sale_services=$goods->after_sale_services;
-            $OrderGoods->subtitle=$goods->subtitle;
-            $OrderGoods->category_id=$goods->category_id;
-            $res2= $OrderGoods->save(false);
-            if (!$res1  || !$res2)
-            {
-                $tran->rollBack();
-                $code=500;
-                return Json::encode([
-                    'code' => $code,
-                    'msg'  => Yii::$app->params['errorCodes'][$code]
-                ]);
-            }
-            $goodsAttr=GoodsAttr::find()
-                ->where(['goods_id'=>$goods->id])
-                ->all();
-            foreach ($goodsAttr as &$attrs)
-            {
-                $OrderAttr=new OrderGoodsAttr();
-                $OrderAttr->order_no=$order_no;
-                $OrderAttr->sku=$goods->sku;
-                $OrderAttr->name=$attrs->name;
-                $OrderAttr->value=$attrs->value;
-                $OrderAttr->unit=$attrs->unit;
-                $OrderAttr->addition_type=$attrs->addition_type;
-                $OrderAttr->goods_id=$attrs->goods_id;
-                if (!$OrderAttr->save(false))
-                {
-                    $tran->rollBack();
-                    $code=500;
-                    return Json::encode([
-                        'code' => $code,
-                        'msg'  => Yii::$app->params['errorCodes'][$code]
-                    ]);
-                }
-            }
-            $tran->commit();
-            return Json::encode([
-                'code' =>  200,
-                'msg'  => 'ok'
-            ]);
-        }catch (Exception $e){
-            $tran->rollBack();
-            $code=500;
-            return Json::encode([
-                'code' => $code,
-                'msg'  => Yii::$app->params['errorCodes'][$code]
-            ]);
-        }
-    }
+//    /**
+//     * 添加测试数据
+//     * @return string
+//     */
+//    public function actionAddTestOrderData(){
+//        $request=Yii::$app->request;
+//        $order_no=GoodsOrder::SetOrderNo();
+//        $goods_id=$request->post('goods_id');
+//        $goods_num=$request->post('goods_num');
+//        $mobile=$request->post('mobile');
+//        $address_id=$request->post('address_id');
+//        $invoice_id=$request->post('invoice_id');
+//        $goods=Goods::find()
+//            ->where(['id'=>$goods_id])
+//            ->one();
+//        if (!$goods){
+//            $c=1000;
+//            return Json::encode([
+//                'code' =>  $c,
+//                'msg'  =>'商品不存在'
+//            ]);
+//        }
+//        $supplier=Supplier::findOne($goods->supplier_id);
+//        if (!$supplier)
+//        {
+//            $c=1000;
+//            return Json::encode([
+//                'code' =>  $c,
+//                'msg'  =>'商家不存在'
+//            ]);
+//        }
+//        $LogisticsTemplate=LogisticsTemplate::findOne($goods->logistics_template_id);
+//        $Goods[]=[
+//            'goods_id'=>$goods_id,
+//            'goods_num'=>$goods_num
+//        ];
+//        if ($LogisticsTemplate->delivery_method==0)
+//        {
+//            $freight=GoodsOrder::CalculationFreightTest($Goods);
+//        }
+//        else
+//        {
+//            $freight=0;
+//        }
+//        $address=UserAddress::find()
+//            ->where(['id'=>$address_id])
+//            ->one();
+//        $invoice=Invoice::find()
+//            ->where(['id'=>$invoice_id])
+//            ->one();
+//        if (!$address  || !$invoice)
+//        {
+//            $c=1000;
+//            return Json::encode([
+//                'code' =>  $c,
+//                'msg'  =>'收货地址ID 或 发票ID 错误'
+//            ]);
+//        }
+//        $user=User::find()->where(['mobile'=>$mobile])->one();
+//        $amount_order=$goods->platform_price*$goods_num+$freight;
+//        $tran = Yii::$app->db->beginTransaction();
+//        $time=time();
+//        try{
+//            $GoodsOrder= new GoodsOrder();
+//            $GoodsOrder->order_no=$order_no;
+//            $GoodsOrder->amount_order=$amount_order;
+//            $GoodsOrder->pay_status=0;
+//            $GoodsOrder->create_time=$time;
+//            $GoodsOrder->paytime=$time;
+//            $GoodsOrder->pay_name='支付宝支付';
+//            $GoodsOrder->order_refer=2;
+//            $GoodsOrder->return_insurance=0;
+//            $GoodsOrder->role_id=7;
+//            $GoodsOrder->supplier_id=$goods->supplier_id;
+//            $GoodsOrder->user_id=$user->id;
+//            $GoodsOrder->buyer_message='请发快递给我';
+//            $GoodsOrder->consignee=$address->consignee;
+//            $GoodsOrder->district_code=$address->district;
+//            $GoodsOrder->region=$address->region;
+//            $GoodsOrder->consignee_mobile=$address->mobile;
+//            $GoodsOrder->invoice_type=$invoice->invoice_type;
+//            $GoodsOrder->invoice_header_type=$invoice->invoice_header_type;
+//            $GoodsOrder->invoicer_card=$invoice->invoicer_card;
+//            $GoodsOrder->invoice_header=$invoice->invoice_header;
+//            $GoodsOrder->invoice_content=$invoice->invoice_content;
+//            $res1=$GoodsOrder->save(false);
+//            $OrderGoods=new  OrderGoods();
+//            $OrderGoods->order_no=$order_no;
+//            $OrderGoods->goods_id=$goods->id;
+//            $OrderGoods->goods_number=$goods_num;
+//            $OrderGoods->goods_attr_id=12;
+//            $OrderGoods->create_time=$time;
+//            $OrderGoods->goods_name=$goods->title;
+//            $OrderGoods->goods_price=$goods->platform_price;
+//            $OrderGoods->sku=$goods->sku;
+//            $OrderGoods->market_price=$goods->market_price;
+//            $OrderGoods->supplier_price=$goods->supplier_price;
+//            $OrderGoods->shipping_type=$LogisticsTemplate->delivery_method;
+//            $OrderGoods->order_status=0;
+//            $OrderGoods->shipping_status=0;
+//            $OrderGoods->customer_service=0;
+//            $OrderGoods->is_unusual=0;
+//            $OrderGoods->freight=$freight;
+//            $OrderGoods->cover_image=$goods->cover_image;
+//            $OrderGoods->after_sale_services=$goods->after_sale_services;
+//            $OrderGoods->subtitle=$goods->subtitle;
+//            $OrderGoods->category_id=$goods->category_id;
+//            $res2= $OrderGoods->save(false);
+//            if (!$res1  || !$res2)
+//            {
+//                $tran->rollBack();
+//                $code=500;
+//                return Json::encode([
+//                    'code' => $code,
+//                    'msg'  => Yii::$app->params['errorCodes'][$code]
+//                ]);
+//            }
+//            $goodsAttr=GoodsAttr::find()
+//                ->where(['goods_id'=>$goods->id])
+//                ->all();
+//            foreach ($goodsAttr as &$attrs)
+//            {
+//                $OrderAttr=new OrderGoodsAttr();
+//                $OrderAttr->order_no=$order_no;
+//                $OrderAttr->sku=$goods->sku;
+//                $OrderAttr->name=$attrs->name;
+//                $OrderAttr->value=$attrs->value;
+//                $OrderAttr->unit=$attrs->unit;
+//                $OrderAttr->addition_type=$attrs->addition_type;
+//                $OrderAttr->goods_id=$attrs->goods_id;
+//                if (!$OrderAttr->save(false))
+//                {
+//                    $tran->rollBack();
+//                    $code=500;
+//                    return Json::encode([
+//                        'code' => $code,
+//                        'msg'  => Yii::$app->params['errorCodes'][$code]
+//                    ]);
+//                }
+//            }
+//            $tran->commit();
+//            return Json::encode([
+//                'code' =>  200,
+//                'msg'  => 'ok'
+//            ]);
+//        }catch (Exception $e){
+//            $tran->rollBack();
+//            $code=500;
+//            return Json::encode([
+//                'code' => $code,
+//                'msg'  => Yii::$app->params['errorCodes'][$code]
+//            ]);
+//        }
+//    }
      /**
      * 用户确认收货
      * @return string
