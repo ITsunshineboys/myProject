482d4254e9d875107f21cc9d15f10c88a7e3172b
afang <15608062620@163.com>
2018-01-03 14:46:31 +0800

    H5

--- web/owner/mall/app.js
+++ web/owner/mall/app.js
@@ -1,62 +1,60 @@
-let app = angular.module("app", ["ui.router", "all_controller","ngAnimate",'angularCSS'])
+var app = angular.module("app", ["ui.router","ngAnimate",'angularCSS'])
     .config(["$stateProvider", "$urlRouterProvider", function ($stateProvider, $urlRouterProvider) {
         $urlRouterProvider.otherwise("/");
         $stateProvider
             .state("home", {
                 url: "/",
-                templateUrl: "intelligent.html",
-                css:['css/intelligent_index.css','zui-1.7.0-dist/dist/css/zui.min.css']
+                templateUrl: "home.html",
+                css:['css/intelligent_index.css','zui-1.7.0-dist/dist/css/zui.min.css'],
+                controller:'home_ctrl'
             })
             .state('nodata',{
                 url:'/nodata',
                 templateUrl:'nodata.html',
-                css:'css/all.css'
-            })
-            .state('nodata.house_list',{
-                url:'/nodata_house_list',
-                templateUrl:'nodata_house.html',
                 css:['css/inteligent_nodata.css','zui-1.7.0-dist/dist/css/zui.min.css','css/all.css'],
+                controller:'nodata_ctrl'
             })
-            .state('nodata.cell_search',{
-                url:'/cell_search',
-                templateUrl:'cell_search.html',
+            .state('search',{
+                url:'/search',
+                templateUrl:'search.html',
                 css:'css/cell_search.css',
-                params: {
-                    code: '',
-                    toponymy: '',
-                    street: ''
-                }
+                controller:'search_ctrl'
             })
-            .state('nodata.basics_decoration',{ // 基础装修
-                url:'/basics_decoration',
-                templateUrl:'all_basics.html',
-                css:['css/basics.css','zui-1.7.0-dist/dist/css/zui.min.css','css/all.css'],
-            })
-            .state('nodata.main_material',{ // 主要材料
-                url:'/main_material',
-                templateUrl:'main_goods.html',
-                css:['css/main.css','zui-1.7.0-dist/dist/css/zui.min.css','css/all.css']
-            })
-            .state('nodata.other_material',{    // 其他
-                url:'/other_material?index',
-                templateUrl:'other_goods.html',
-                css:['css/other.css','css/all.css']
-            })
-            .state('nodata.product_detail',{
-                url:'/product_detail?num',
-                templateUrl:'cur_product_detail.html',
-                css:['css/product_details.css','css/all.css'],
-            })
-            .state('nodata.all_goods',{
-                url:'/all_goods',
-                templateUrl:'get_all_goods.html',
-                css:['css/commodify.css','css/all.css']
-            })
-            .state('nodata.second_level',{
-                url:'/second_level',
-                templateUrl:'second_level.html',
-                css:['css/movefurniture.css','css/all.css']
-            })
+            // .state('nodata.house_list',{
+            //     url:'/nodata_house_list',
+            //     templateUrl:'nodata.html',
+            //     css:['css/inteligent_nodata.css','zui-1.7.0-dist/dist/css/zui.min.css','css/all.css'],
+            // })
+            // .state('nodata.basics_decoration',{ // 基础装修
+            //     url:'/basics_decoration',
+            //     templateUrl:'all_basics.html',
+            //     css:['css/basics.css','zui-1.7.0-dist/dist/css/zui.min.css','css/all.css'],
+            // })
+            // .state('nodata.main_material',{ // 主要材料
+            //     url:'/main_material',
+            //     templateUrl:'main_goods.html',
+            //     css:['css/main.css','zui-1.7.0-dist/dist/css/zui.min.css','css/all.css']
+            // })
+            // .state('nodata.other_material',{    // 其他
+            //     url:'/other_material?index',
+            //     templateUrl:'other_goods.html',
+            //     css:['css/other.css','css/all.css']
+            // })
+            // .state('nodata.product_detail',{
+            //     url:'/product_detail?num',
+            //     templateUrl:'cur_product_detail.html',
+            //     css:['css/product_details.css','css/all.css'],
+            // })
+            // .state('nodata.all_goods',{
+            //     url:'/all_goods',
+            //     templateUrl:'get_all_goods.html',
+            //     css:['css/commodify.css','css/all.css']
+            // })
+            // .state('nodata.second_level',{
+            //     url:'/second_level',
+            //     templateUrl:'second_level.html',
+            //     css:['css/movefurniture.css','css/all.css']
+            // })
             .state('modelRoom',{ // 样板间
                 url:'/quotation?roomCode&toponymy&street',
                 templateUrl:'model_room.html',
@@ -76,13 +74,15 @@ let app = angular.module("app", ["ui.router", "all_controller","ngAnimate",'angu
             })
 
     }])
-    .service('_ajax', function ($http, $state) {
+    .service('_ajax', function ($q,$http, $state) {
         this.get = function (url, params, callback) {
+            let deferred = $q.defer()
             $http({
                 method: 'GET',
                 url: baseUrl + url,
                 params: params
             }).then(function (response) {
+                deferred.resolve(response)
                 let res = response.data;
                 if (res.code === 403) {
                     $state.go('login')
@@ -97,6 +97,7 @@ let app = angular.module("app", ["ui.router", "all_controller","ngAnimate",'angu
                 console.log(response);
                 alert(response.statusText)
             })
+            return deferred.promise
         };
         this.post = function (url, params, callback) {
             $http({
@@ -118,6 +119,7 @@ let app = angular.module("app", ["ui.router", "all_controller","ngAnimate",'angu
                 } else {
                     alert(res.msg)
                 }
+                deferred.resolve(response)
             }, function (response) {
                 console.log(response);
                 alert(response.statusText)
--- web/owner/mall/js/search.js
+++ web/owner/mall/js/search.js
@@ -1,13 +1,24 @@
-/**
- * Created by xl on 2017/6/17 0017.
- */
-$(".search").keyup(function () {
-    var input_value =$(".search").val();
-    if(input_value ==""){
-        $(".tab_none").show();
-        $(".tab_hidden").hide();
-    }else{
-        $(".tab_none").hide();
-        $(".tab_hidden").show();
+app.controller('search_ctrl',function ($scope,_ajax,$state) {
+    //初始化数据
+    $scope.toponymy = {
+        name:'',
+        address:''
     }
-});
+    if(sessionStorage.getItem('toponymy')!=null){
+        $scope.toponymy = JSON.parse(sessionStorage.getItem('toponymy'))
+    }
+    //返回上一页
+    $scope.goPrev = function(){
+        history.go(-1)
+    }
+    //获取小区信息
+    $scope.$watch('toponymy.name',function (newVal,oldVal) {
+        if(newVal!=''){
+            _ajax.get('/owner/search',{
+                str:newVal
+            },function (res) {
+                console.log(res);
+            })
+        }
+    })
+})
