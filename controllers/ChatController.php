f0e9c11eb9050747354bac5356c290709d62c492
yr <790733192@qq.com>
2018-01-04 15:42:19 +0800

    edit

--- controllers/ChatController.php
+++ controllers/ChatController.php
@@ -43,182 +43,182 @@ class ChatController extends Controller
         return [$u_id, $role_id];
     }
 
-    /**
-     *第一次登陆app时创建环信用户和本地环信关联数据
-     * @return array|string
-     */
-    public function actionNew()
-    {
-        $user = self::getUser();
-        if (!is_array($user)) {
-            return $user;
-        }
-        list($u_id, $role_id) = $user;
-        $username=trim(\Yii::$app->request->post('moblie',''));
-        $password=trim(\Yii::$app->request->post('password',''));
-        if (UserChat::find()->where(['u_id' => $u_id, 'role_id' => $role_id])->one()) {
-            $code = 1000;
-            return Json::encode([
-                'code' => $code,
-                'msg' => '已有聊天号，无须新建'
-            ]);
-        }
-        $user_chat = UserChat::newChatUser($username,$password,$u_id,$role_id);
-        if (!$user_chat) {
-            $code = 500;
-            return Json::encode([
-                'code' => $code,
-                'msg' => \Yii::$app->params['errorCodes'][$code]
-            ]);
-        }
-
-        list($chat, $hx) = $user_chat;
-        if (array_key_exists('error', $hx)) {
-            return Json::encode([
-                'code' => 1000,
-                'msg' => $hx['error']
-            ]);
-        }
-        return Json::encode([
-            'code' => 200,
-            'msg' => 'ok',
-            'data' => $chat,
-            'data_online' => $hx
-        ]);
-    }
-
-    //封禁用户
-    public function actionUserBan()
-    {
-        //改变本地状态
-        $user = self::getUser();
-        if (!is_array($user)) {
-            return $user;
-        }
-        list($u_id, $role_id) = $user;
-        $chat = UserChat::find()->where(['u_id' => $u_id, 'role_id' => $role_id])->one();
-        $chat->status = 0;
-        $chat->update();
-        //改变环信里的状态
-        $username = $chat->chat_username;
-        $chat_online = new ChatService();
-        $hx = $chat_online->deactiveUser($username);
-
-        if (array_key_exists('error', $hx)) {
-            return Json::encode([
-                'code' => 1000,
-                'msg' => $hx['error']
-            ]);
-        }
-
-        return Json::encode([
-            'code' => 200,
-            'msg' => 'ok',
-            'data' => $chat,
-            'data_online' => $hx
-        ]);
-
-    }
-
-    //解禁用户
-    public function actionUserUnBan()
-    {
-        //改变本地状态
-        $user = self::getUser();
-        if (!is_array($user)) {
-            return $user;
-        }
-        list($u_id, $role_id) = $user;
-        $chat = UserChat::find()->where(['u_id' => $u_id, 'role_id' => $role_id])->one();
-        $chat->status = 1;
-        $chat->update();
-        //改变环信里的状态
-        $username = $chat->chat_username;
-        $chat_online = new ChatService();
-        $hx = $chat_online->activeUser($username);
-
-        if (array_key_exists('error', $hx)) {
-            return Json::encode([
-                'code' => 1000,
-                'msg' => $hx['error']
-            ]);
-        }
-
-        return Json::encode([
-            'code' => 200,
-            'msg' => 'ok',
-            'data' => $chat,
-            'data_online' => $hx
-        ]);
-
-    }
-    /**
-     * 得到登陆用户的环信号
-     * @return array|string
-     */
-    public function actionGetUsername()
-    {
-        $user = self::getUser();
-        if (!is_array($user)) {
-            return $user;
-        }
-        list($u_id, $role_id) = $user;
-        if ($user_chat = UserChat::find()->where(['u_id' => $u_id, 'role_id' => $role_id])->one()) {
-            $username = $user_chat->chat_username;
-            return Json::encode([
-                'code' => 200,
-                'msg' => 'ok',
-                'username' => $username
-            ]);
-        }
-        return Json::encode([
-            'code' => 1051,
-            'msg' => '没有聊天号'
-        ]);
-    }
-
-    /**
-     * 得到未读消息数
-     * @return string
-     */
-    public function actionOfflineMsgCount()
-    {
-        $user = $this->actionGetUsername();
-        $username = Json::decode($user)['username'];
-        $chat_online = new ChatService();
-        $msg_count = $chat_online->getOfflineMessages($username);
-
-        if (array_key_exists('error', $msg_count)) {
-            return Json::encode([
-                'code' => 1000,
-                'msg' => $msg_count['error']
-            ]);
-        }
-        return Json::encode([
-            'code' => 200,
-            'msg' => 'ok',
-            'msg_count' => $msg_count['count']
-        ]);
-    }
-    /**
-     * 环信用户修改昵称
-     * @return array|string
-     */
-    public function actionEditNickname(){
-        $user = self::getUser();
-        if (!is_array($user)) {
-            return $user;
-        }
-        list($u_id, $role_id) = $user;
-        $nickname=trim(\Yii::$app->request->post('nickname'));
-
-        $code=UserChat::editnickname($nickname,$u_id,$role_id);
-        return Json::encode([
-            'code'=>$code,
-            'msg'=>$code==200?'ok':\Yii::$app->params['errorCodes'][$code]
-        ]);
-
-    }
+//    /**
+//     *第一次登陆app时创建环信用户和本地环信关联数据
+//     * @return array|string
+//     */
+//    public function actionNew()
+//    {
+//        $user = self::getUser();
+//        if (!is_array($user)) {
+//            return $user;
+//        }
+//        list($u_id, $role_id) = $user;
+//        $username=trim(\Yii::$app->request->post('moblie',''));
+//        $password=trim(\Yii::$app->request->post('password',''));
+//        if (UserChat::find()->where(['u_id' => $u_id, 'role_id' => $role_id])->one()) {
+//            $code = 1000;
+//            return Json::encode([
+//                'code' => $code,
+//                'msg' => '已有聊天号，无须新建'
+//            ]);
+//        }
+//        $user_chat = UserChat::newChatUser($username,$password,$u_id,$role_id);
+//        if (!$user_chat) {
+//            $code = 500;
+//            return Json::encode([
+//                'code' => $code,
+//                'msg' => \Yii::$app->params['errorCodes'][$code]
+//            ]);
+//        }
+//
+//        list($chat, $hx) = $user_chat;
+//        if (array_key_exists('error', $hx)) {
+//            return Json::encode([
+//                'code' => 1000,
+//                'msg' => $hx['error']
+//            ]);
+//        }
+//        return Json::encode([
+//            'code' => 200,
+//            'msg' => 'ok',
+//            'data' => $chat,
+//            'data_online' => $hx
+//        ]);
+//    }
+//
+//    //封禁用户
+//    public function actionUserBan()
+//    {
+//        //改变本地状态
+//        $user = self::getUser();
+//        if (!is_array($user)) {
+//            return $user;
+//        }
+//        list($u_id, $role_id) = $user;
+//        $chat = UserChat::find()->where(['u_id' => $u_id, 'role_id' => $role_id])->one();
+//        $chat->status = 0;
+//        $chat->update();
+//        //改变环信里的状态
+//        $username = $chat->chat_username;
+//        $chat_online = new ChatService();
+//        $hx = $chat_online->deactiveUser($username);
+//
+//        if (array_key_exists('error', $hx)) {
+//            return Json::encode([
+//                'code' => 1000,
+//                'msg' => $hx['error']
+//            ]);
+//        }
+//
+//        return Json::encode([
+//            'code' => 200,
+//            'msg' => 'ok',
+//            'data' => $chat,
+//            'data_online' => $hx
+//        ]);
+//
+//    }
+//
+//    //解禁用户
+//    public function actionUserUnBan()
+//    {
+//        //改变本地状态
+//        $user = self::getUser();
+//        if (!is_array($user)) {
+//            return $user;
+//        }
+//        list($u_id, $role_id) = $user;
+//        $chat = UserChat::find()->where(['u_id' => $u_id, 'role_id' => $role_id])->one();
+//        $chat->status = 1;
+//        $chat->update();
+//        //改变环信里的状态
+//        $username = $chat->chat_username;
+//        $chat_online = new ChatService();
+//        $hx = $chat_online->activeUser($username);
+//
+//        if (array_key_exists('error', $hx)) {
+//            return Json::encode([
+//                'code' => 1000,
+//                'msg' => $hx['error']
+//            ]);
+//        }
+//
+//        return Json::encode([
+//            'code' => 200,
+//            'msg' => 'ok',
+//            'data' => $chat,
+//            'data_online' => $hx
+//        ]);
+//
+//    }
+//    /**
+//     * 得到登陆用户的环信号
+//     * @return array|string
+//     */
+//    public function actionGetUsername()
+//    {
+//        $user = self::getUser();
+//        if (!is_array($user)) {
+//            return $user;
+//        }
+//        list($u_id, $role_id) = $user;
+//        if ($user_chat = UserChat::find()->where(['u_id' => $u_id, 'role_id' => $role_id])->one()) {
+//            $username = $user_chat->chat_username;
+//            return Json::encode([
+//                'code' => 200,
+//                'msg' => 'ok',
+//                'username' => $username
+//            ]);
+//        }
+//        return Json::encode([
+//            'code' => 1051,
+//            'msg' => '没有聊天号'
+//        ]);
+//    }
+//
+//    /**
+//     * 得到未读消息数
+//     * @return string
+//     */
+//    public function actionOfflineMsgCount()
+//    {
+//        $user = $this->actionGetUsername();
+//        $username = Json::decode($user)['username'];
+//        $chat_online = new ChatService();
+//        $msg_count = $chat_online->getOfflineMessages($username);
+//
+//        if (array_key_exists('error', $msg_count)) {
+//            return Json::encode([
+//                'code' => 1000,
+//                'msg' => $msg_count['error']
+//            ]);
+//        }
+//        return Json::encode([
+//            'code' => 200,
+//            'msg' => 'ok',
+//            'msg_count' => $msg_count['count']
+//        ]);
+//    }
+//    /**
+//     * 环信用户修改昵称
+//     * @return array|string
+//     */
+//    public function actionEditNickname(){
+//        $user = self::getUser();
+//        if (!is_array($user)) {
+//            return $user;
+//        }
+//        list($u_id, $role_id) = $user;
+//        $nickname=trim(\Yii::$app->request->post('nickname'));
+//
+//        $code=UserChat::editnickname($nickname,$u_id,$role_id);
+//        return Json::encode([
+//            'code'=>$code,
+//            'msg'=>$code==200?'ok':\Yii::$app->params['errorCodes'][$code]
+//        ]);
+//
+//    }
     /**
      * 发送文本消息给指定用户
      * @return array|string
@@ -252,7 +252,7 @@ class ChatController extends Controller
             ]);
         }
         $chat_limt = UserChat::chatlimt($u_id);
-        if($chat_limt>=10){
+        if($chat_limt>=100){
             $code =1082;
             return Json::encode([
                 'code'=>$code,
