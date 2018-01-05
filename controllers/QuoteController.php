80904d0be683eecc3a45f56651096aa74adcf338
yr <790733192@qq.com>
2018-01-05 16:43:02 +0800

    edit

--- controllers/QuoteController.php
+++ controllers/QuoteController.php
@@ -1357,6 +1357,20 @@ class QuoteController extends Controller
 //                        }
                     }
                 }
+                $toponymy_edit=EffectToponymy::find()->where(['id'=>$request['effect_id']])->one();
+                $toponymy_edit->province_code=$request['province_code'];
+                $toponymy_edit->city_code=$request['city_code'];
+                $toponymy_edit->district_code=$request['district_code'];
+                $toponymy_edit->street=$request['address'];
+                $toponymy_edit->toponymy=$request['house_name'];
+                if(!$toponymy_edit->save(false)){
+                    $transaction->rollBack();
+                    $code = 500;
+                    return json_encode([
+                        'code' => $code,
+                        'msg' => \Yii::$app->params['errorCodes'][$code]
+                    ]);
+                }
             }
             $transaction->commit();
         }catch (\Exception $e) {
