b6eb17495f0741fac52229bde209facb8a87c626
hyz <1248705443@qq.com>
2018-01-09 17:24:39 +0800

    up

--- models/GoodsOrder.php
+++ models/GoodsOrder.php
@@ -2924,7 +2924,7 @@ class GoodsOrder extends ActiveRecord
                 $list[]=
                 [
                     'goods_price'=>$arr[$k]['goods_price'],
-                    'send_time'=>$send_time,
+                    'send_time'=>$send_time==0?'':$send_time,
                     'complete_time'=>$complete_time,
                     'automatic_receive_time'=>$automatic_receive_time,
                     'sku'=> $arr[$k]['sku'],
