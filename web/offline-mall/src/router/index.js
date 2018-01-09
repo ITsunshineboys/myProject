2fa2c39aaf68692f61b9cf1b7e355e9cf464cd19
Mr.Piz <q564146734@163.com>
2018-01-09 18:02:16 +0800

    店铺简介 店铺管理

--- web/offline-mall/src/router/index.js
+++ web/offline-mall/src/router/index.js
@@ -1,18 +1,19 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '@/views/home/index'                  // 首页
-import Search from '@/views/search/index'              // 搜索
-import City from '@/views/city/index'                  // 城市选择
-import Order from '@/views/order/index'                // 订单主页
-import Address from '@/views/address/index'            // 收货地址
-import Invoice from '@/views/invoice/index'            // 发票
-import PaySuccess from '@/views/pay_success/index'     // 支付成功
-import GoodDetail from '@/views/good_detail/index'     // 商品详情
-import AllComment from '@/views/comment/index'         // 全部评价
-import ClassList from '@/views/class/index'            // 分类列表
-import GoodsList from '@/views/goods_list/index'       // 商品列表
-import StoreHome from '@/views/store_management/index' // 店铺首页
-import Agreement from '@/views/agreement/index'        // 商城购买协议
+import Home from '@/views/home/index'                       // 首页
+import Search from '@/views/search/index'                   // 搜索
+import City from '@/views/city/index'                       // 城市选择
+import Order from '@/views/order/index'                     // 订单主页
+import Address from '@/views/address/index'                 // 收货地址
+import Invoice from '@/views/invoice/index'                 // 发票
+import PaySuccess from '@/views/pay_success/index'          // 支付成功
+import GoodDetail from '@/views/good_detail/index'          // 商品详情
+import AllComment from '@/views/comment/index'              // 全部评价
+import ClassList from '@/views/class/index'                 // 分类列表
+import GoodsList from '@/views/goods_list/index'            // 商品列表
+import StoreHome from '@/views/store_management/index'      // 店铺首页
+import ShopIntro from '@/views/store_management/shop_intro' // 店铺介绍
+import Agreement from '@/views/agreement/index'             // 商城购买协议
 
 Vue.use(Router)
 
@@ -66,6 +67,10 @@ export default new Router({
       path: '/store',
       name: 'Store',
       component: StoreHome
+    }, {
+      path: '/shop-intro',
+      name: 'ShopIntro',
+      component: ShopIntro
     }, {
       path: '/agreement',
       name: 'Agreement',
