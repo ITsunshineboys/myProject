add7d0dc30a5d77cf8328c788ec4c75932ffcbaa
Wch <362224550@qq.com>
2018-01-10 14:31:32 +0800

    1

--- models/Goods.php
+++ models/Goods.php
@@ -510,7 +510,8 @@ class Goods extends ActiveRecord
                 $where_ = "id in (".$one_goods['series_id'].")";
                     $series = Series::find()->select('id,series')->where($where_)->all();
                     foreach ($series as $one_series){
-                        $one_goods['series_id'] = $one_series['series'];
+                        $one_goods['series_name'] = $one_series['series'];
+                        unset($one_goods['series_id']);
                     }
             }
 
@@ -522,7 +523,8 @@ class Goods extends ActiveRecord
                 $where_ = "id in (".$one_goods['style_id'].")";
                 $series = Style::find()->select('id,style')->where($where_)->all();
                 foreach ($series as $one_series){
-                    $one_goods['style_id'] = $one_series['style'];
+                    $one_goods['style_name'] = $one_series['style'];
+                    unset($one_goods['style_id']);
                 }
             }
         }
