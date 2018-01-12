855b88b33db53474b53062b5b15f5813097f52ff
yr <790733192@qq.com>
2018-01-12 16:47:33 +0800

    edit

--- models/Goods.php
+++ models/Goods.php
@@ -452,7 +452,7 @@ class Goods extends ActiveRecord
         if (!$sku) {
             return false;
         }
-        $select = "goods.id,goods.category_id,goods.platform_price,goods.supplier_price,goods.purchase_price_decoration_company,goods_brand.name,gc.title,logistics_district.district_name,goods.category_id,gc.path,goods.profit_rate,goods.subtitle,goods.series_id,goods.style_id,goods.cover_image,supplier.shop_name,goods.title as goods_name,goods.sku";
+        $select = "goods.id,goods.category_id,goods.platform_price,goods.supplier_price,goods.purchase_price_decoration_company,goods_brand.name,gc.title,logistics_district.district_name,goods.category_id,gc.path,goods.profit_rate,goods.subtitle,goods.series_id,goods.style_id,goods.cover_image,supplier.shop_name,goods.title as goods_name,goods.sku,goods.status";
         $rows = self::find()
             ->asArray()
             ->select($select)
