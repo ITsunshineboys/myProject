5294c125c26483aa251e7cfb002c571a45c76879
Wch <362224550@qq.com>
2018-01-17 15:58:39 +0800

    up

--- services/BasisDecorationService.php
+++ services/BasisDecorationService.php
@@ -1560,9 +1560,9 @@ class BasisDecorationService
 
 
         foreach ($hallLamp as &$lamp){
-            $lamp['quantity'] = (int)ceil($get['hall']);
-            $lamp['cost'] = round(self::algorithm(1,$lamp['quantity'],$lamp[0]['platform_price']),2);
-            $lamp['procurement'] = round(self::algorithm(1,$lamp['quantity'],$lamp[0]['purchase_price_decoration_company']),2);
+            $lamp[0]['quantity'] = (int)ceil($get['hall']);
+            $lamp[0]['cost'] = round(self::algorithm(1,$lamp['quantity'],$lamp[0]['platform_price']),2);
+            $lamp[0]['procurement'] = round(self::algorithm(1,$lamp['quantity'],$lamp[0]['purchase_price_decoration_company']),2);
             unset($lamp[1]);
         }
 
