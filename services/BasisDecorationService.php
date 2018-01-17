deeb697f452ac2602c0980ba63ded7c5df3e4a87
Wch <362224550@qq.com>
2018-01-17 14:55:56 +0800

    up

--- services/BasisDecorationService.php
+++ services/BasisDecorationService.php
@@ -1543,6 +1543,7 @@ class BasisDecorationService
 
     public static function lamp($goods,$get)
     {
+        echo 111;die;
         foreach ($goods as $oneGoods){
             if(strpos($oneGoods[1]['value'],'客厅') !== false) {
                 $hallLamp[] = $oneGoods;
