c083064b31b48bf134195abc881fc85b8165172f
Wch <362224550@qq.com>
2018-01-17 10:58:39 +0800

    111

--- controllers/OwnerController.php
+++ controllers/OwnerController.php
@@ -1438,6 +1438,7 @@ class OwnerController extends Controller
         // 有资料 计算公式
         $goods = Goods::assortList(self::MATERIALS,$get['city']);
         $material[]= BasisDecorationService::formula($goods,$get);
+        var_dump($material);die;
 
 
         //无计算公式
