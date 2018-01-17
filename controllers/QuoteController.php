34e43272377f67ac9c1ca5ee355f8b775ff70c48
yr <790733192@qq.com>
2018-01-17 16:19:50 +0800

    test

--- controllers/QuoteController.php
+++ controllers/QuoteController.php
@@ -3107,7 +3107,7 @@ class QuoteController extends Controller
      */
     public function actionTest()
     {
-       echo EffectPicture::find()->where(['effect_id'=>252])->all();
+       var_dump(EffectPicture::find()->where(['effect_id'=>252])->all());
 
     }
 }
