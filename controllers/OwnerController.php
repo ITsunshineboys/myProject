ddefd7c3fcc38d5ccb95ea541ab653cbcb494518
Wch <362224550@qq.com>
2018-01-18 12:45:10 +0800

    up

--- controllers/OwnerController.php
+++ controllers/OwnerController.php
@@ -1443,7 +1443,7 @@ class OwnerController extends Controller
                 $lamp [] = $attr;
             }
         }
-
+var_dump($lamp);die;
         $material[] = BasisDecorationService::lamp($lamp,$get);
         $material[]= BasisDecorationService::formula($goods,$get);
 
