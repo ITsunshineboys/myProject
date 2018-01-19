8a5318bcbed5ca8afafc8c006b03358e0e2d71b4
Wch <362224550@qq.com>
2018-01-19 16:33:49 +0800

    up

--- controllers/OwnerController.php
+++ controllers/OwnerController.php
@@ -1833,7 +1833,7 @@ class OwnerController extends Controller
      */
     public function actionTest()
     {
-        $goods = EngineeringStandardCarpentryCoefficient::find()->asArray()->all();
+        $goods = WorkerType::craft(OwnerController::CRAFT_NAME['tiler'],$get['city']);
         var_dump($goods);
     }
 
