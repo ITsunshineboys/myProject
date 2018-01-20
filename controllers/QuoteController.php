0d1b55184fca2908183629f3d4193318adffc87d
Wch <362224550@qq.com>
2018-01-20 11:56:11 +0800

    up

--- controllers/QuoteController.php
+++ controllers/QuoteController.php
@@ -3009,6 +3009,7 @@ class QuoteController extends Controller
         }elseif (isset($post['area'])){
             //下半部分
             foreach ($post['area'] as $one_else){
+                var_dump($one_else);die;
                 if (isset($one_else['id'])){
                     Apartment::findByUpdate($one_else['value'],$one_else['id']);
                 }
