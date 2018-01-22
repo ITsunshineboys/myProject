1d46821feacc0e997a08a475d0b0b102a0917a33
wangjie <1248136872@qq.com>
2018-01-22 15:55:30 +0800

    express

--- web/admin/supplier/pages/order_manage/js/express_ctrl.js
+++ web/admin/supplier/pages/order_manage/js/express_ctrl.js
@@ -1,42 +1,34 @@
-/**
- * Created by Administrator on 2017/9/26/026.
- */
 let express = angular.module("expressModule", []);
-express.controller("express_ctrl", function ($rootScope,$scope, _ajax, $stateParams,$state) {
-    $scope.order_no = $stateParams.order_no; //订单号
-    $scope.sku = $stateParams.sku; //商品编号
-    $scope.tabflag = $stateParams.tabflag;
-    let statename = $stateParams.statename;
-	console.log($stateParams.statename);
+express.controller("express_ctrl", function ($rootScope, $scope, _ajax, $stateParams, $state) {
+	$scope.order_no = $stateParams.order_no; //订单号
+	$scope.sku = $stateParams.sku; //商品编号
+	$scope.tabflag = $stateParams.tabflag;
+	let statename = $stateParams.statename;
 
-    /*获取物流信息*/
-    expressDetail();
-    function expressDetail() {
-        let data = {
-            order_no: $scope.order_no,
-            sku: $scope.sku
-        };
-        _ajax.post("/order/getexpress", data, function (res) {
-            $scope.order_info = res.data;
-            $scope.shipping_way = res.data.shipping_type; //判断是送货上门or快递
-        });
-    }
+	/*获取物流信息*/
+	_ajax.post("/order/getexpress", {
+		order_no: $scope.order_no,
+		sku: $scope.sku
+	}, function (res) {
+		$scope.order_info = res.data;
+		!!$scope.order_info.waybillname ? $scope.order_info.waybillname = $scope.order_info.waybillname : $scope.order_info.waybillname = '送货上门'
+	});
 
-    /*返回上一个页面*/
-    $scope.backPage = function () {
-	    $state.go(statename,{order_no:$scope.order_no,sku:$scope.sku,tabflag:$scope.tabflag})
-    };
+	/*返回上一个页面*/
+	$scope.backPage = function () {
+		$state.go(statename, {order_no: $scope.order_no, sku: $scope.sku, tabflag: $scope.tabflag})
+	};
 
-    /*面包屑参数*/
-    $rootScope.crumbs = [{
-        name: '订单管理',
-        icon: 'icon-dingdanguanli',
-        link: 'order_manage',
-        params:{tabflag:$scope.tabflag}
-    },{
-        name: '订单详情',
-        link:  $scope.backPage,
-    },{
-        name: '物流详情'
-    }];
+	/*面包屑参数*/
+	$rootScope.crumbs = [{
+		name: '订单管理',
+		icon: 'icon-dingdanguanli',
+		link: 'order_manage',
+		params: {tabflag: $scope.tabflag}
+	}, {
+		name: '订单详情',
+		link: $scope.backPage,
+	}, {
+		name: '物流详情'
+	}];
 });
