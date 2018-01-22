51284a59149ddd3b84b6f6ad32a731d11da498bc
hyz <1248705443@qq.com>
2018-01-22 15:02:23 +0800

    test

--- models/OrderRefund.php
+++ models/OrderRefund.php
@@ -45,15 +45,19 @@ class OrderRefund extends ActiveRecord
         return $data?$data:[];
     }
 
+    /**
+     * @param $order_refund
+     * @return mixed
+     */
     public  static  function  SetRefundParameter($order_refund)
     {
         foreach ($order_refund as $k =>$v)
         {
-
             $order_refund[$k]['create_time']=date('Y-m-d H:i',$order_refund[$k]['create_time']);
             $order_refund[$k]['handle_time']=date('Y-m-d H:i',$order_refund[$k]['handle_time']);
             $order_refund[$k]['refund_time']=date('Y-m-d H:i',$order_refund[$k]['refund_time']);
-            if ($order_refund[$k]['handle']==0){
+            if ($order_refund[$k]['handle']==0)
+            {
                 unset( $order_refund[$k]['handle_time']);
                 unset( $order_refund[$k]['refund_time']);
             }
@@ -84,8 +88,6 @@ class OrderRefund extends ActiveRecord
             $code=1000;
            return $code;
         }
-
-
         switch ($GoodsOrder->order_refer)
         {
             case self::REFUND_TYPE_LINE:
