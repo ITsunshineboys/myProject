6d36eeff74a7794b44e2d0628c3728a4fe5fa246
yr <790733192@qq.com>
2018-01-24 18:16:49 +0800

    test

--- models/Effect.php
+++ models/Effect.php
@@ -263,6 +263,7 @@ class Effect extends ActiveRecord
         }
 
         foreach ($material as &$value){
+
             $goods_cate_id=Goods::find()->select('brand_id,category_id')->where(['id'=>$value['goods_id']])->asArray()->one();
             $value['price']= sprintf('%.2f',(float)$value['price']*0.01);
             $value['cate_level3']=GoodsCategory::find()->select('title')
@@ -281,7 +282,6 @@ class Effect extends ActiveRecord
 
 
         }
-
 
         $material_grop=array_values(self::array_group_by($material,'first_cate_id'));
 
