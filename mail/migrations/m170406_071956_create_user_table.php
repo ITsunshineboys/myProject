380e22a366ecc3f6ab11f07debd6b252810c138f
Mr.Piz <q564146734@163.com>
2018-01-24 17:32:55 +0800

    APP版本H5部署

---
+++ mail/migrations/m170406_071956_create_user_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `user`.
+ */
+class m170406_071956_create_user_table extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function up()
+    {
+        $this->createTable('user', [
+            'id' => $this->primaryKey(),
+            'username' => $this->string(),
+            'password' => $this->string(),
+            'authKey' => $this->string(),
+            'accessToken' => $this->string(),
+        ]);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function down()
+    {
+        $this->dropTable('user');
+    }
+}
