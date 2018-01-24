e1510705f826fc51ec6e0bd8f4f8f48a1ae83d13
Clarissahu <hulingfangzi@163.com>
2018-01-24 15:47:23 +0800

    商品

--- web/offline-mall/src/App.vue
+++ web/offline-mall/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <transition  enter-active-class="animated fadeIn">
+    <transition enter-active-class="animated fadeIn">
       <keep-alive :include="/Address|Invoice|Order/">
         <router-view></router-view>
       </keep-alive>
@@ -16,6 +16,7 @@
 
 <style lang="less">
   @import '~vux/src/styles/reset.less';
+
   body {
     background-color: #f6f6f6;
   }
