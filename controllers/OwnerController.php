8fa1ac5a5246727385f8c88988d4196daf8a9954
Wch <362224550@qq.com>
2018-02-01 18:11:56 +0800

    up

--- controllers/OwnerController.php
+++ controllers/OwnerController.php
@@ -1109,10 +1109,10 @@ class OwnerController extends Controller
         }
         $series = Series::find()->select('id,series')->where(['id'=>$get['series']])->one();
         $style = Style::find()->select('id,style')->where(['id'=>$get['style']])->one();
-var_dump($brick);die;
+
         foreach ($brick as $one_brick){
             foreach ($one_brick['attr'] as $attr_){
-
+var_dump($attr_);
                 if ($attr_['value'] == '卫生间'
                     && strpos($one_brick['series_name'],$series->series) !== false
                     && strpos($one_brick['style_name'],$style->style) !== false
