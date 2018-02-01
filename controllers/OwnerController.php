c87945b1dab4af07b1932f66b1f760a1d79a00bd
Wch <362224550@qq.com>
2018-02-01 14:44:23 +0800

    up

--- controllers/OwnerController.php
+++ controllers/OwnerController.php
@@ -1491,7 +1491,7 @@ class OwnerController extends Controller
 //            }
 //            $style = BasisDecorationService::style($condition_stairs);
 //            $material[][]= BasisDecorationService::profitMargin($style);
-            $material[] = ['a'=>1,'b'=>2];
+            $material[][] = ['a'=>1,'b'=>2];
         }
 
 
