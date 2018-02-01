d1bf0df0b024a18f7735e0ca9e1349f5a357cb84
hyz <1248705443>
2018-02-01 10:05:47 +0800

    up

--- controllers/OrderController.php
+++ controllers/OrderController.php
@@ -5691,7 +5691,14 @@ class OrderController extends Controller
     public  function  actionVerificationWxPaySuccess()
     {
         $goodsOrder=GoodsOrder::FindByOrderNo(\Yii::$app->request->get('order_no'),'pay_status');
-        $code=$goodsOrder->pay_status==GoodsOrder::PAY_STATUS_PAID?200:1098;
+        if (!$goodsOrder)
+        {
+            $code=1000;
+        }else
+        {
+            $code=$goodsOrder->pay_status==GoodsOrder::PAY_STATUS_PAID?200:1098;
+        }
+
         return Json::encode([
             'code' => $code,
             'msg' => $code==200?'ok':\Yii::$app->params['errorCodes'][$code]
