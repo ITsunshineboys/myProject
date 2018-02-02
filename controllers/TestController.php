e3d72a208f0563cfe265c9a83cb8f121ceaf61c6
hyz <1248705443>
2018-02-02 17:20:00 +0800

    up

--- controllers/TestController.php
+++ controllers/TestController.php
@@ -539,7 +539,7 @@ class TestController extends Controller
     public  function  actionTest()
     {
        $http_type = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') || (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https')) ? 'https://' : 'http://';
-        echo  $http_type . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+        echo  $http_type .   $_SERVER["SERVER_NAME"] . $_SERVER["REQUEST_URI"];
     }
 
 }
