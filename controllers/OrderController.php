168f7d3e415f10b3b4be85f84ecfeec942e6b3f4
hyz <1248705443>
2018-02-02 14:52:43 +0800

    test

--- controllers/OrderController.php
+++ controllers/OrderController.php
@@ -4954,7 +4954,7 @@ class OrderController extends Controller
             if ($user)
             {
                 $code=ShippingCart::MergeShippingCartNoLogin($session_id,$user);
-                echo $code;
+                echo $code;die;
                 $data=ShippingCart::ShippingList($user);
             }else
             {
