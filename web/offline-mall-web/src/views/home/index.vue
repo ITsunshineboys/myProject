79ff361de336bdcac9faab91c8540f933cacd1f7
wangjie <1248136872@qq.com>
2018-02-02 14:53:34 +0800

    h5

--- web/offline-mall-web/src/views/home/index.vue
+++ web/offline-mall-web/src/views/home/index.vue
@@ -120,7 +120,15 @@
       this.axios.get('/order/iswxlogin', {}, (res) => {
         res.code === 200 ? sessionStorage.setItem('wxCodeFlag', true) : sessionStorage.getItem('wxCodeFlag', false)
         this.wxData = res.data
-//        if (sessionStorage.getItem('wxCodeFlag') && !sessionStorage.getItem('wxStatus')) {
+        if (sessionStorage.getItem('wxCodeFlag') && !sessionStorage.getItem('wxStatus')) {
+          wx.config({
+            debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+            appId: this.wxData.appId, // 必填，公众号的唯一标识
+            timestamp: this.wxData.timestamp, // 必填，生成签名的时间戳
+            nonceStr: this.wxData.nonceStr, // 必填，生成签名的随机串
+            signature: this.wxData.signature, // 必填，签名，见附录1
+            jsApiList: ['onMenuShareAppMessage', 'onMenuShareTimeline'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+          })
 //          this.axios.post('/order/find-open-id', {
 //            url: location.href
 //          }, (res) => {
@@ -138,15 +146,7 @@
 //            sessionStorage.setItem('openID', res.data)
 //            console.log(res)
 //          })
-//        }
-        wx.config({
-          debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-          appId: this.wxData.appId, // 必填，公众号的唯一标识
-          timestamp: this.wxData.timestamp, // 必填，生成签名的时间戳
-          nonceStr: this.wxData.nonceStr, // 必填，生成签名的随机串
-          signature: this.wxData.signature, // 必填，签名，见附录1
-          jsApiList: ['onMenuShareAppMessage', 'onMenuShareTimeline'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
-        })
+        }
       })
       wx.ready(function () {
         wx.onMenuShareAppMessage({
