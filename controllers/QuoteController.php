57ac661b01d7886ac7279fccbac02fa90ae94738
yr <790733192@.com>
2018-02-02 11:04:01 +0800

    test

--- controllers/QuoteController.php
+++ controllers/QuoteController.php
@@ -3109,7 +3109,7 @@ class QuoteController extends Controller
     {
 
 
-        var_dump(GoodsAttr::find()->asArray()->where(['category_id'=>38])->all());
+        var_dump(GoodsAttr::find()->asArray()->where(['goods_id'=>557])->all());
 
 
     }
