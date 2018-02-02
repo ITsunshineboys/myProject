01d6761a5933daf039b73a4f8efffc8c1d89ef54
yr <790733192@.com>
2018-02-02 11:18:49 +0800

    test

--- controllers/QuoteController.php
+++ controllers/QuoteController.php
@@ -3109,7 +3109,7 @@ class QuoteController extends Controller
     {
 
 
-        var_dump(GoodsAttr::find()->asArray()->where(['category'=>38])->all());
+        var_dump(GoodsAttr::find()->asArray()->where(['category_id'=>38])->all());
         var_dump(GoodsCategory::find()->asArray()->where(['id'=>38])->one());
         var_dump(Goods::find()->asArray()->where(['id'=>49])->one());
 
