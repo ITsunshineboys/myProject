e39a61328b15d6a1947b49e15da48d3ebb962222
wangjie <1248136872@qq.com>
2018-02-09 10:52:48 +0800

    shop

--- web/admin/supplier/pages/commodity_manage/js/commodity_manage_ctrl.js
+++ web/admin/supplier/pages/commodity_manage/js/commodity_manage_ctrl.js
@@ -265,44 +265,54 @@ let commodity_manage = angular.module("commodity_manage", [])
         });
         //获取二级
         _ajax.get('/mall/categories',{pid:1},function (res) {
-            $scope.second = res.data.categories;
-            $scope.twoColor = $scope.second[0];
+	        if (res.data.categories.length>0) {
+		        $scope.second = res.data.categories;
+		        $scope.twoColor = $scope.second[0];
+		        //获取三级
+		        _ajax.get('/mall/categories',{pid:2},function (res) {
+			        $scope.three = res.data.categories;
+			        for (let [key, value] of $scope.three.entries()) {
+				        if ($scope.item_check.length == 0) {
+					        value['complete'] = false
+				        } else {
+					        for (let [key1, value1] of $scope.item_check.entries()) {
+						        if (value.id == value1.id) {
+							        value.complete = true
+						        }
+					        }
+				        }
+			        }
+		        })
+	        }else {
+		        $scope.second = []
+		        $scope.three = []
+	        }
         });
-        //获取三级
-        _ajax.get('/mall/categories',{pid:2},function (res) {
-            $scope.three = res.data.categories;
-            for (let [key, value] of $scope.three.entries()) {
-                if ($scope.item_check.length == 0) {
-                    value['complete'] = false
-                } else {
-                    for (let [key1, value1] of $scope.item_check.entries()) {
-                        if (value.id == value1.id) {
-                            value.complete = true
-                        }
-                    }
-                }
-            }
-        })
         //点击一级 获取相对应的二级
         $scope.getMore = function (n) {
             $scope.oneColor = n;
             _ajax.get('/mall/categories',{pid:+n.id},function (res) {
-                $scope.second = res.data.categories;
-                $scope.twoColor = $scope.second[0];
-                _ajax.get('/mall/categories',{pid:+ $scope.second[0].id},function (res) {
-                    $scope.three = res.data.categories;
-                    for (let [key, value] of $scope.three.entries()) {
-                        if ($scope.item_check.length == 0) {
-                            value['complete'] = false
-                        } else {
-                            for (let [key1, value1] of $scope.item_check.entries()) {
-                                if (value.id == value1.id) {
-                                    value.complete = true
-                                }
-                            }
-                        }
-                    }
-                })
+	            if (res.data.categories.length>0) {
+		            $scope.second = res.data.categories;
+		            $scope.twoColor = $scope.second[0];
+		            _ajax.get('/mall/categories',{pid:+ $scope.second[0].id},function (res) {
+			            $scope.three = res.data.categories;
+			            for (let [key, value] of $scope.three.entries()) {
+				            if ($scope.item_check.length == 0) {
+					            value['complete'] = false
+				            } else {
+					            for (let [key1, value1] of $scope.item_check.entries()) {
+						            if (value.id == value1.id) {
+							            value.complete = true
+						            }
+					            }
+				            }
+			            }
+		            })
+	            }else {
+		            $scope.second = []
+		            $scope.three = []
+	            }
             })
             $scope.threeColor='';
         };
@@ -311,18 +321,22 @@ let commodity_manage = angular.module("commodity_manage", [])
             $scope.id = n;
             $scope.twoColor = n;
             _ajax.get('/mall/categories',{pid:+n.id},function (res) {
-                $scope.three = res.data.categories;
-                for (let [key, value] of $scope.three.entries()) {
-                    if ($scope.item_check.length == 0) {
-                        value['complete'] = false
-                    } else {
-                        for (let [key1, value1] of $scope.item_check.entries()) {
-                            if (value.id == value1.id) {
-                                value.complete = true
-                            }
-                        }
-                    }
-                }
+            	if (res.data.categories.length > 0) {
+		            $scope.three = res.data.categories;
+		            for (let [key, value] of $scope.three.entries()) {
+			            if ($scope.item_check.length == 0) {
+				            value['complete'] = false
+			            } else {
+				            for (let [key1, value1] of $scope.item_check.entries()) {
+					            if (value.id == value1.id) {
+						            value.complete = true
+					            }
+				            }
+			            }
+		            }
+	            }else{
+		            $scope.three = [];
+	            }
             })
             $scope.threeColor='';
         };
