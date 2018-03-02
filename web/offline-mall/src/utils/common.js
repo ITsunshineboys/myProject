9d6cb9ee1678e3a433571a2f2956e4744b0f39c4
Clarissahu <hulingfangzi@163.com>
2018-03-02 16:38:02 +0800

    optimize

--- web/offline-mall/src/utils/common.js
+++ web/offline-mall/src/utils/common.js
@@ -1,15 +1,16 @@
 /**
  * Created by Administrator on 2017/12/19/019.
  */
-var ModalHelper = (function (bodyCls) { // eslint-disable-line
-  var scrollTop // 在闭包中定义一个用来保存滚动位置的变量
+// 滚动穿透
+var ModalHelper = (function (bodyCls) {
+  var scrollTop   // 在闭包中定义一个用来保存滚动位置的变量
   return {
-    afterOpen: function () { // 弹出之后记录保存滚动位置，并且给body添加.modal-open
+    afterOpen: function () {  // 弹出之后记录保存滚动位置，并且给body添加.modal-open
       scrollTop = document.documentElement.scrollTop || document.body.scrollTop
       document.body.setAttribute('class', bodyCls)
       document.body.style.top = -scrollTop + 'px'
     },
-    beforeClose: function () { // 关闭时将.modal-open移除并还原之前保存滚动位置
+    beforeClose: function () {  // 关闭时将.modal-open移除并还原之前保存滚动位置
       document.body.setAttribute('class', '')
       document.documentElement.scrollTop = scrollTop
       document.body.scrollTop = scrollTop
